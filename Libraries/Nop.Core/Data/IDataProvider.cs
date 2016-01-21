221edca5f5fb694d15265d7bccbd343e30bccfb5
GrandNode <support@grandnode.com>
2016-01-21 23:07:47 +0100

    Commit

--- Libraries/Nop.Core/Data/IDataProvider.cs
+++ Libraries/Nop.Core/Data/IDataProvider.cs
@@ -9,20 +9,8 @@ namespace Nop.Core.Data
     public interface IDataProvider
     {
         /// <summary>
-        /// Initialize connection factory
-        /// </summary>
-        //void InitConnectionFactory();
-
-        /// <summary>
-        /// Set database initializer
-        /// </summary>
-        //void SetDatabaseInitializer();
-
-        /// <summary>
         /// Initialize database
         /// </summary>
-        void InitDatabase();
-
-        
+        void InitDatabase();        
     }
 }
