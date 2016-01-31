9f60f6df0d92e0547664e1a45f6c3f0ef01eec97
GrandNode <support@grandnode.com>
2016-01-31 18:51:01 +0100

    Simplified code for creation on PagedList class object.

--- Libraries/Nop.Core/PagedList.cs
+++ Libraries/Nop.Core/PagedList.cs
@@ -21,39 +21,21 @@ namespace Nop.Core
         /// <param name="pageSize">Page size</param>
         public PagedList(IEnumerable<T> source, int pageIndex, int pageSize)
         {
-            int total = source.Count();
-            this.TotalCount = total;
-            this.TotalPages = total / pageSize;
-
-            if (total % pageSize > 0)
-                TotalPages++;
-
-            this.PageSize = pageSize;
-            this.PageIndex = pageIndex;
-            this.AddRange(source.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+            Init(source, pageIndex, pageSize);
         }
         public PagedList()
         {
         }
         public PagedList(IMongoQueryable<T> source, int pageIndex, int pageSize)
         {
-            int total = source.Count();
-            this.TotalCount = total;
-            this.TotalPages = total / pageSize;
-
-            if (total % pageSize > 0)
-                TotalPages++;
-
-            this.PageSize = pageSize;
-            this.PageIndex = pageIndex;
-            this.AddRange(source.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+            Init(source, pageIndex, pageSize);
         }
 
         public PagedList(IMongoQueryable<T> source, int pageIndex, int pageSize, bool product)
         {
             var products = source.Skip(pageIndex * pageSize).Take(pageSize+1).ToList();
             int count = products.Count();
-            int total = count + (pageIndex * pageSize); //source.Count();
+            int total = count + (pageIndex * pageSize); 
             this.TotalCount = total;
             this.TotalPages = total / pageSize;
 
@@ -88,15 +70,7 @@ namespace Nop.Core
         /// <param name="pageSize">Page size</param>
         public PagedList(IList<T> source, int pageIndex, int pageSize)
         {
-            TotalCount = source.Count();
-            TotalPages = TotalCount / pageSize;
-
-            if (TotalCount % pageSize > 0)
-                TotalPages++;
-
-            this.PageSize = pageSize;
-            this.PageIndex = pageIndex;
-            this.AddRange(source.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+            Init(source, pageIndex, pageSize);
         }
 
         public PagedList(IMongoCollection<T> source, FilterDefinition<T> filterdefinition, SortDefinition<T> sortdefinition, int pageIndex, int pageSize)
@@ -137,15 +111,27 @@ namespace Nop.Core
         /// <param name="totalCount">Total count</param>
         public PagedList(IEnumerable<T> source, int pageIndex, int pageSize, int totalCount)
         {
-            TotalCount = totalCount;
+            Init(source, pageIndex, pageSize, totalCount);
+        }
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// <param name="source">source</param>
+        /// <param name="pageIndex">Page index</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="totalCount">Total count</param>
+        private void Init(IEnumerable<T> source, int pageIndex, int pageSize, int? totalCount = null)
+        {
+            TotalCount = totalCount ?? source.Count();
             TotalPages = TotalCount / pageSize;
 
             if (TotalCount % pageSize > 0)
                 TotalPages++;
 
-            this.PageSize = pageSize;
-            this.PageIndex = pageIndex;
-            this.AddRange(source);
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            AddRange(source.Skip(pageIndex * pageSize).Take(pageSize));
         }
 
         public int PageIndex { get; private set; }
