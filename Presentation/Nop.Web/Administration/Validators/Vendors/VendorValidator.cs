1e35e4f7ad0abdd2a939781db7bc89829e8cbfe7
GrandNode <support@grandnode.com>
2016-02-07 21:24:19 +0100

    Avoid duplication when entering page size values - nopCommerce #1107

--- Presentation/Nop.Web/Administration/Validators/Vendors/VendorValidator.cs
+++ Presentation/Nop.Web/Administration/Validators/Vendors/VendorValidator.cs
@@ -10,9 +10,9 @@ namespace Nop.Admin.Validators.Vendors
         public VendorValidator(ILocalizationService localizationService)
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Vendors.Fields.Name.Required"));
-
             RuleFor(x => x.Email).NotEmpty().WithMessage(localizationService.GetResource("Admin.Vendors.Fields.Email.Required"));
             RuleFor(x => x.Email).EmailAddress().WithMessage(localizationService.GetResource("Admin.Common.WrongEmail"));
+            RuleFor(x => x.PageSizeOptions).Must(ValidatorUtilities.PageSizeOptionsValidator).WithMessage(localizationService.GetResource("Admin.Vendors.Fields.PageSizeOptions.ShouldHaveUniqueItems"));
         }
     }
 }
