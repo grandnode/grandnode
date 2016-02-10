6dc274727a2a787a8585629d1ef44c8f769a6019
GrandNode <support@grandnode.com>
2016-02-10 13:41:13 +0100

    Refactoring

---
+++ Plugins/Nop.Plugin.Widgets.Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.Test
+{
+    public class Class1
+    {
+    }
+}
