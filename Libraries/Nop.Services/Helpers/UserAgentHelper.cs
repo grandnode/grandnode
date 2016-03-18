07570882a02972b7b49798865b3f79fab7b58c24
GrandNode <support@grandnode.com>
2016-03-18 12:27:46 +0100

    Move back to browscap.org to detect search crawlers

--- Libraries/Nop.Services/Helpers/UserAgentHelper.cs
+++ Libraries/Nop.Services/Helpers/UserAgentHelper.cs
@@ -5,7 +5,6 @@ using System.Web;
 using Nop.Core;
 using Nop.Core.Configuration;
 using Nop.Core.Infrastructure;
-using UserAgentStringLibrary;
 
 namespace Nop.Services.Helpers
 {
@@ -30,19 +29,20 @@ namespace Nop.Services.Helpers
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
-        protected virtual UasParser GetUasParser()
+        protected virtual BrowscapXmlHelper GetBrowscapXmlHelperr()
         {
-            if (Singleton<UasParser>.Instance == null)
-            {
-                //no database created
-                if (String.IsNullOrEmpty(_config.UserAgentStringsPath))
-                    return null;
+            if (Singleton<BrowscapXmlHelper>.Instance != null)
+                return Singleton<BrowscapXmlHelper>.Instance;
 
-                var filePath = CommonHelper.MapPath(_config.UserAgentStringsPath);
-                var uasParser = new UasParser(filePath);
-                Singleton<UasParser>.Instance = uasParser;
-            }
-            return Singleton<UasParser>.Instance;
+            //no database created
+            if (String.IsNullOrEmpty(_config.UserAgentStringsPath))
+                return null;
+
+            var filePath = CommonHelper.MapPath(_config.UserAgentStringsPath);
+            var bowscapXmlHelper = new BrowscapXmlHelper(filePath);
+
+            Singleton<BrowscapXmlHelper>.Instance = bowscapXmlHelper;
+            return Singleton<BrowscapXmlHelper>.Instance;
         }
 
         /// <summary>
@@ -56,24 +56,23 @@ namespace Nop.Services.Helpers
 
             //we put required logic in try-catch block
             //more info: http://www.nopcommerce.com/boards/t/17711/unhandled-exception-request-is-not-available-in-this-context.aspx
-            bool result = false;
             try
             {
-                var uasParser = GetUasParser();
+                var bowscapXmlHelperr = GetBrowscapXmlHelperr();
 
                 //we cannot load parser
-                if (uasParser == null)
+                if (bowscapXmlHelperr == null)
                     return false;
 
                 var userAgent = _httpContext.Request.UserAgent;
-                result = uasParser.IsBot(userAgent);
-                //result = context.Request.Browser.Crawler;
+                return bowscapXmlHelperr.IsCrawler(userAgent);
             }
             catch (Exception exc)
             {
                 Debug.WriteLine(exc);
             }
-            return result;
+
+            return false;
         }
 
     }
