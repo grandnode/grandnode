6a33c9f026c6981af58a4b2d2c3f791deb4e0bf6
GrandNode <support@grandnode.com>
2016-12-12 20:07:26 +0100

    Fixed Autofac warnings. 'ContainerBuilder.Update(IContainer)'

--- Libraries/Grand.Core/Infrastructure/GrandEngine.cs
+++ Libraries/Grand.Core/Infrastructure/GrandEngine.cs
@@ -47,9 +47,6 @@ namespace Grand.Core.Infrastructure
         protected virtual void RegisterDependencies(GrandConfig config)
         {
             var builder = new ContainerBuilder();
-            var container = builder.Build();
-            this._containerManager = new ContainerManager(container);
-
             //we create new instance of ContainerBuilder
             //because Build() or Update() method can only be called once on a ContainerBuilder.
 
@@ -59,19 +56,20 @@ namespace Grand.Core.Infrastructure
             builder.RegisterInstance(config).As<GrandConfig>().SingleInstance();
             builder.RegisterInstance(this).As<IEngine>().SingleInstance();
             builder.RegisterInstance(typeFinder).As<ITypeFinder>().SingleInstance();
-            builder.Update(container);
 
             //register dependencies provided by other assemblies
-            builder = new ContainerBuilder();
             var drTypes = typeFinder.FindClassesOfType<IDependencyRegistrar>();
             var drInstances = new List<IDependencyRegistrar>();
             foreach (var drType in drTypes)
                 drInstances.Add((IDependencyRegistrar)Activator.CreateInstance(drType));
+            
             //sort
             drInstances = drInstances.AsQueryable().OrderBy(t => t.Order).ToList();
             foreach (var dependencyRegistrar in drInstances)
                 dependencyRegistrar.Register(builder, typeFinder, config);
-            builder.Update(container);
+
+            var container = builder.Build();
+            this._containerManager = new ContainerManager(container);
 
             //set dependency resolver
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
