13122d79a3340378a34bede9632d7efc92773dfd
GrandNode <support@grandnode.com>
2016-12-16 14:43:08 +0100

    Minor Changes - event publisher in a separate thread

--- Libraries/Grand.Services/Events/EventPublisherExtensions.cs
+++ Libraries/Grand.Services/Events/EventPublisherExtensions.cs
@@ -7,17 +7,23 @@ namespace Grand.Services.Events
     {
         public static void EntityInserted<T>(this IEventPublisher eventPublisher, T entity) where T : ParentEntity
         {
-            eventPublisher.Publish(new EntityInserted<T>(entity));
+            System.Threading.Tasks.Task.Run(() => {
+                eventPublisher.Publish(new EntityInserted<T>(entity));
+            });
         }
 
         public static void EntityUpdated<T>(this IEventPublisher eventPublisher, T entity) where T : ParentEntity
         {
-            eventPublisher.Publish(new EntityUpdated<T>(entity));
+            System.Threading.Tasks.Task.Run(() => {
+                eventPublisher.Publish(new EntityUpdated<T>(entity));
+            });
         }
 
         public static void EntityDeleted<T>(this IEventPublisher eventPublisher, T entity) where T : ParentEntity
         {
-            eventPublisher.Publish(new EntityDeleted<T>(entity));
+            System.Threading.Tasks.Task.Run(() => {
+                eventPublisher.Publish(new EntityDeleted<T>(entity));
+            });
         }
 
     }
