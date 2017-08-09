6433cc37cd17de7433914260bcc73be8b8633957
GrandNode <support@grandnode.com>
2017-08-09 11:46:51 +0200

    Tag helper - Captcha

---
+++ Grand.Framework/TagHelpers/GenerateCaptchaTagHelper.cs
@@ -0,0 +1,62 @@
+﻿using Grand.Framework.Security.Captcha;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grand.Framework.TagHelpers
+{
+    [HtmlTargetElement("captcha", TagStructure = TagStructure.WithoutEndTag)]
+    public class GenerateCaptchaTagHelper : TagHelper
+    {
+        private readonly IHtmlHelper _htmlHelper;
+        private readonly CaptchaSettings _captchaSettings;
+
+        /// <summary>
+        /// ViewContext
+        /// </summary>
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public GenerateCaptchaTagHelper(IHtmlHelper htmlHelper, CaptchaSettings captchaSettings)
+        {
+            _htmlHelper = htmlHelper;
+            _captchaSettings = captchaSettings;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            //contextualize IHtmlHelper
+            var viewContextAware = _htmlHelper as IViewContextAware;
+            viewContextAware?.Contextualize(ViewContext);
+
+            //generate captcha control
+            var captchaControl = new GRecaptchaControl(_captchaSettings.ReCaptchaVersion)
+            {
+                Theme = _captchaSettings.ReCaptchaTheme,
+                Id = "recaptcha",
+                PublicKey = _captchaSettings.ReCaptchaPublicKey,
+                Language = _captchaSettings.ReCaptchaLanguage
+            };
+            var captchaControlHtml = captchaControl.RenderControl();
+
+            //tag details
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.SetHtmlContent(captchaControlHtml);
+        }
+    }
+}
