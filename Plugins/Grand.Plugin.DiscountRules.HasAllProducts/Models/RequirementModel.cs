da89ea418509aad5e1f13f182977a1fb86ed5b35
GrandNode <support@grandnode.com>
2017-08-16 09:03:42 +0200

    Added updated plugin DiscountRequirement.HasAllProducts

---
+++ Plugins/Grand.Plugin.DiscountRules.HasAllProducts/Models/RequirementModel.cs
@@ -0,0 +1,61 @@
+﻿using Grand.Framework.Mvc.ModelBinding;
+using Grand.Framework.Mvc.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Grand.Plugin.DiscountRules.HasAllProducts.Models
+{
+    public class RequirementModel
+    {
+        [GrandResourceDisplayName("Plugins.DiscountRules.HasAllProducts.Fields.Products")]
+        public string Products { get; set; }
+
+        public string DiscountId { get; set; }
+
+        public string RequirementId { get; set; }
+
+        #region Nested classes
+
+        public partial class AddProductModel : BaseGrandModel
+        {
+            public AddProductModel()
+            {
+                AvailableCategories = new List<SelectListItem>();
+                AvailableManufacturers = new List<SelectListItem>();
+                AvailableStores = new List<SelectListItem>();
+                AvailableVendors = new List<SelectListItem>();
+                AvailableProductTypes = new List<SelectListItem>();
+            }
+
+            [GrandResourceDisplayName("Admin.Catalog.Products.List.SearchProductName")]
+            public string SearchProductName { get; set; }
+            [GrandResourceDisplayName("Admin.Catalog.Products.List.SearchCategory")]
+            public string SearchCategoryId { get; set; }
+            [GrandResourceDisplayName("Admin.Catalog.Products.List.SearchManufacturer")]
+            public string SearchManufacturerId { get; set; }
+            [GrandResourceDisplayName("Admin.Catalog.Products.List.SearchStore")]
+            public string SearchStoreId { get; set; }
+            [GrandResourceDisplayName("Admin.Catalog.Products.List.SearchVendor")]
+            public string SearchVendorId { get; set; }
+            [GrandResourceDisplayName("Admin.Catalog.Products.List.SearchProductType")]
+            public int SearchProductTypeId { get; set; }
+
+            public IList<SelectListItem> AvailableCategories { get; set; }
+            public IList<SelectListItem> AvailableManufacturers { get; set; }
+            public IList<SelectListItem> AvailableStores { get; set; }
+            public IList<SelectListItem> AvailableVendors { get; set; }
+            public IList<SelectListItem> AvailableProductTypes { get; set; }
+
+            //vendor
+            public bool IsLoggedInAsVendor { get; set; }
+        }
+
+        public partial class ProductModel : BaseGrandEntityModel
+        {
+            public string Name { get; set; }
+
+            public bool Published { get; set; }
+        }
+        #endregion
+    }
+}
---
+++ Plugins/Grand.Plugin.DiscountRules.HasAllProducts/RouteProvider.cs
@@ -0,0 +1,36 @@
+﻿using Grand.Framework.Mvc.Routing;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Grand.Plugin.DiscountRules.HasAllProducts
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasAllProducts.Configure",
+                 "Admin/DiscountRulesHasAllProducts/Configure",
+                 new { controller = "DiscountRulesHasAllProducts", action = "Configure" }
+            );
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasAllProducts.ProductAddPopup",
+                 "Admin/DiscountRulesHasAllProducts/ProductAddPopup",
+                 new { controller = "DiscountRulesHasAllProducts", action = "ProductAddPopup" }
+            );
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasAllProducts.ProductAddPopupList",
+                 "Admin/DiscountRulesHasAllProducts/ProductAddPopupList",
+                 new { controller = "DiscountRulesHasAllProducts", action = "ProductAddPopupList" }
+            );
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasAllProducts.LoadProductFriendlyNames",
+                 "Admin/DiscountRulesHasAllProducts/LoadProductFriendlyNames",
+                 new { controller = "DiscountRulesHasAllProducts", action = "LoadProductFriendlyNames" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
