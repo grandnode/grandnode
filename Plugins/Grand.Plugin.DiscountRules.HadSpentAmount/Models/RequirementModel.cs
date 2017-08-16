b0cb5110731cb7d75191871611f265da111e878f
GrandNode <support@grandnode.com>
2017-08-16 09:30:37 +0200

    Added updated plugin DiscountRules.HadSpentAmount

---
+++ Plugins/Grand.Plugin.DiscountRules.HadSpentAmount/Models/RequirementModel.cs
@@ -0,0 +1,15 @@
+﻿using Grand.Framework;
+using Grand.Framework.Mvc.ModelBinding;
+
+namespace Grand.Plugin.DiscountRules.HadSpentAmount.Models
+{
+    public class RequirementModel
+    {
+        [GrandResourceDisplayName("Plugins.DiscountRules.HadSpentAmount.Fields.Amount")]
+        public decimal SpentAmount { get; set; }
+
+        public string DiscountId { get; set; }
+
+        public string RequirementId { get; set; }
+    }
+}
