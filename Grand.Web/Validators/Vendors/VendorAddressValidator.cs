85e7a9c549b1b8d51b30e2621949f6541367b806
GrandNode <support@grandnode.com>
2017-11-28 12:17:24 +0100

    Minor changes - resource string (account.vendorInfo)

--- Grand.Web/Validators/Vendors/VendorAddressValidator.cs
+++ Grand.Web/Validators/Vendors/VendorAddressValidator.cs
@@ -18,10 +18,10 @@ namespace Grand.Web.Validators.Common
             {
                 RuleFor(x => x.CountryId)
                     .NotNull()
-                    .WithMessage(localizationService.GetResource("Vendor.Account.Fields.Country.Required"));
+                    .WithMessage(localizationService.GetResource("Account.VendorInfo.Country.Required"));
                 RuleFor(x => x.CountryId)
                     .NotEqual("")
-                    .WithMessage(localizationService.GetResource("Vendor.Account.Fields.Country.Required"));
+                    .WithMessage(localizationService.GetResource("Account.VendorInfo.Country.Required"));
             }
             if (addressSettings.CountryEnabled && addressSettings.StateProvinceEnabled)
             {
@@ -39,35 +39,35 @@ namespace Grand.Web.Validators.Common
                         }
                     }
                     return true;
-                }).WithMessage(localizationService.GetResource("Vendor.Account.Fields.StateProvince.Required"));
+                }).WithMessage(localizationService.GetResource("Account.VendorInfo.StateProvince.Required"));
             }
             if (addressSettings.CompanyRequired && addressSettings.CompanyEnabled)
             {
-                RuleFor(x => x.Company).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.Company.Required"));
+                RuleFor(x => x.Company).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.Company.Required"));
             }
             if (addressSettings.StreetAddressRequired && addressSettings.StreetAddressEnabled)
             {
-                RuleFor(x => x.Address1).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.StreetAddress.Required"));
+                RuleFor(x => x.Address1).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.Address1.Required"));
             }
             if (addressSettings.StreetAddress2Required && addressSettings.StreetAddress2Enabled)
             {
-                RuleFor(x => x.Address2).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.StreetAddress2.Required"));
+                RuleFor(x => x.Address2).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.Address2.Required"));
             }
             if (addressSettings.ZipPostalCodeRequired && addressSettings.ZipPostalCodeEnabled)
             {
-                RuleFor(x => x.ZipPostalCode).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.ZipPostalCode.Required"));
+                RuleFor(x => x.ZipPostalCode).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.ZipPostalCode.Required"));
             }
             if (addressSettings.CityRequired && addressSettings.CityEnabled)
             {
-                RuleFor(x => x.City).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.City.Required"));
+                RuleFor(x => x.City).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.City.Required"));
             }
             if (addressSettings.PhoneRequired && addressSettings.PhoneEnabled)
             {
-                RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.Phone.Required"));
+                RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.phonenumber.Required"));
             }
             if (addressSettings.FaxRequired && addressSettings.FaxEnabled)
             {
-                RuleFor(x => x.FaxNumber).NotEmpty().WithMessage(localizationService.GetResource("Vendor.Account.Fields.Fax.Required"));
+                RuleFor(x => x.FaxNumber).NotEmpty().WithMessage(localizationService.GetResource("Account.VendorInfo.faxnumber.Required"));
             }
         }
     }
