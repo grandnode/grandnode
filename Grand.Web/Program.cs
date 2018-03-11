e0087a8413bfe18e645f389c6d122a2dc611a49a
GrandNode <support@grandnode.com>
2018-03-11 14:11:50 +0100

    CaptureStartupErrors set on True (Linux systems)

--- Grand.Web/Program.cs
+++ Grand.Web/Program.cs
@@ -9,6 +9,7 @@ namespace Grand.Web
         {
             var host = WebHost.CreateDefaultBuilder(args)
                 .UseKestrel(options => options.AddServerHeader = false)
+                .CaptureStartupErrors(true)
                 .UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
                 .UseStartup<Startup>()
                 .Build();
