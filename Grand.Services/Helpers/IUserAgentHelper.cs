635321d34247193764cc4c2a3277f6df253cf145
GrandNode <support@grandnode.com>
2018-03-26 12:58:31 +0200

    Removed unnecessary code

--- Grand.Services/Helpers/IUserAgentHelper.cs
+++ Grand.Services/Helpers/IUserAgentHelper.cs
@@ -10,22 +10,6 @@ namespace Grand.Services.Helpers
         /// Get a value indicating whether the request is made by search engine (web crawler)
         /// </summary>
         /// <returns>Result</returns>
-        bool IsSearchEngine();
-        /// <summary>
-        /// Get a value indicating whether the request is made by web api
-        /// </summary>
-        /// <returns>Result</returns>
-        bool IsWebApi();
-
-        /// <summary>
-        /// Get a value indicating whether the request is made by mobile device
-        /// </summary>
-        /// <returns></returns>
-        bool IsMobileDevice();
-
-        /// Get a value indicating whether the request is made by IE8 browser
-        /// </summary>
-        /// <returns></returns>
-        bool IsIe8();
+        bool IsSearchEngine();        
     }
 }
