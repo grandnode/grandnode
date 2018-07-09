190ed4bfb345df68b0a5c00b356b811615699986
KrzysztofPajak <krzysiek@nop4you.com>
2018-07-09 13:27:16 +0200

    Revert "Minor improvements"
    This reverts commit 2f5a9589fda0626621268e7004a238d80a454d6e.

--- Grand.Services/Common/GenericAttributeExtensions.cs
+++ Grand.Services/Common/GenericAttributeExtensions.cs
@@ -6,18 +6,6 @@ namespace Grand.Services.Common
 {
     public static class GenericAttributeExtensions
     {
-        public static IGenericAttributeService GenericAttributeService
-        {
-            get
-            {
-                if(_genericAttributeService == null)
-                    _genericAttributeService = EngineContext.Current.Resolve<IGenericAttributeService>();
-                return _genericAttributeService;
-            }
-        }
-        
-        private static IGenericAttributeService _genericAttributeService { get; set; }
-
         /// <summary>
         /// Get an attribute of an entity
         /// </summary>
@@ -31,7 +19,7 @@ namespace Grand.Services.Common
             if (entity == null)
                 throw new ArgumentNullException("entity");
 
-            var genericAttributeService = GenericAttributeService; 
+            var genericAttributeService = EngineContext.Current.Resolve<IGenericAttributeService>();
             return genericAttributeService.GetAttributesForEntity<TPropType>(entity, key, storeId);
 
             
