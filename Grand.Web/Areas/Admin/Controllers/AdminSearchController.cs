8c70dda4ecbaa228f2bd89fe67b63ec71f04617f
jasieg <panjankonopka@gmail.com>
2018-08-27 15:07:40 +0200

    Admin search - further changes

--- Grand.Web/Areas/Admin/Controllers/AdminSearchController.cs
+++ Grand.Web/Areas/Admin/Controllers/AdminSearchController.cs
@@ -1,7 +1,10 @@
-﻿using Grand.Services.Blogs;
+﻿using Grand.Core.Domain.AdminSearch;
+using Grand.Services.Blogs;
 using Grand.Services.Catalog;
 using Grand.Services.Customers;
+using Grand.Services.Localization;
 using Grand.Services.News;
+using Grand.Services.Orders;
 using Grand.Services.Topics;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -20,9 +23,13 @@ namespace Grand.Web.Areas.Admin.Controllers
         private readonly INewsService _newsService;
         private readonly IBlogService _blogService;
         private readonly ICustomerService _customerService;
+        private readonly IOrderService _orderService;
+        private readonly AdminSearchSettings _adminSearchSettings;
+        private readonly ILocalizationService _localizationService;
 
         public AdminSearchController(IProductService productService, ICategoryService categoryService, IManufacturerService manufacturerService,
-            ITopicService topicService, INewsService newsService, IBlogService blogService, ICustomerService customerService)
+            ITopicService topicService, INewsService newsService, IBlogService blogService, ICustomerService customerService, IOrderService orderService,
+            AdminSearchSettings adminSearchSettings, ILocalizationService localizationService)
         {
             this._productService = productService;
             this._categoryService = categoryService;
@@ -31,82 +38,144 @@ namespace Grand.Web.Areas.Admin.Controllers
             this._newsService = newsService;
             this._blogService = blogService;
             this._customerService = customerService;
+            this._orderService = orderService;
+            this._adminSearchSettings = adminSearchSettings;
+            this._localizationService = localizationService;
         }
 
         [HttpPost]
         public IActionResult Search(string searchTerm)
         {
-            List<object> result = new List<object>();
+            if (string.IsNullOrEmpty(searchTerm))
+                return Json("error");
 
-            var products = _productService.SearchProducts(keywords: searchTerm);
-            foreach (var product in products)
+            //object = actual result, int = display order for sorting
+            List<Tuple<object, int>> result = new List<Tuple<object, int>>();
+
+            if (searchTerm.Length >= _adminSearchSettings.MinSearchTermLength)
             {
-                result.Add(new
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInProducts)
                 {
-                    title = product.Name,
-                    link = Url.Content("~/Admin/Product/Edit/") + product.Id,
-                    source = "Products"
-                });
+                    var products = _productService.SearchProducts(keywords: searchTerm);
+                    foreach (var product in products)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = product.Name,
+                            link = Url.Content("~/Admin/Product/Edit/") + product.Id,
+                            source = "Products"
+                        }, _adminSearchSettings.ProductsDisplayOrder));
+                    }
+                }
+
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInCategories)
+                {
+                    var categories = _categoryService.GetAllCategories(searchTerm);
+                    foreach (var category in categories)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = category.Name,
+                            link = Url.Content("~/Admin/Category/Edit/") + category.Id,
+                            source = "Categories"
+                        }, _adminSearchSettings.CategoriesDisplayOrder));
+                    }
+                }
+
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInManufacturers)
+                {
+                    var manufacturers = _manufacturerService.GetAllManufacturers(searchTerm);
+                    foreach (var manufacturer in manufacturers)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = manufacturer.Name,
+                            link = Url.Content("~/Admin/Manufacturer/Edit/") + manufacturer.Id,
+                            source = "Manufacturers"
+                        }, _adminSearchSettings.ManufacturersDisplayOrder));
+                    }
+                }
+
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInTopics)
+                {
+                    var topics = _topicService.GetAllTopics("", topicSystemName: searchTerm);
+                    foreach (var topic in topics)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = topic.SystemName,
+                            link = Url.Content("~/Admin/Topic/Edit/") + topic.Id,
+                            source = "Topics"
+                        }, _adminSearchSettings.TopicsDisplayOrder));
+                    }
+                }
+
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInNews)
+                {
+                    var news = _newsService.GetAllNews(newsTitle: searchTerm);
+                    foreach (var signleNews in news)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = signleNews.Title,
+                            link = Url.Content("~/Admin/News/Edit/") + signleNews.Id,
+                            source = "News"
+                        }, _adminSearchSettings.NewsDisplayOrder));
+                    }
+                }
+
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInBlogs)
+                {
+                    var blogPosts = _blogService.GetAllBlogPosts(blogPostName: searchTerm);
+                    foreach (var blogPost in blogPosts)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = blogPost.Title,
+                            link = Url.Content("~/Admin/Blog/Edit/") + blogPost.Id,
+                            source = "Blog posts"
+                        }, _adminSearchSettings.BlogsDisplayOrder));
+                    }
+                }
+
+                if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInCustomers)
+                {
+                    var customersByEmail = _customerService.GetAllCustomers(email: searchTerm);
+                    var customersByUsername = _customerService.GetAllCustomers(username: searchTerm);
+                    var combined = customersByEmail.Intersect(customersByUsername);
+
+                    foreach (var customer in combined)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = customer.Email,
+                            link = Url.Content("~/Admin/Customer/Edit/") + customer.Id,
+                            source = "Customers"
+                        }, _adminSearchSettings.CustomersDisplayOrder));
+                    }
+                }
             }
 
-            var categories = _categoryService.GetAllCategories(searchTerm);
-            foreach (var category in categories)
+            if (result.Count() < _adminSearchSettings.MaxSearchResultsCount && _adminSearchSettings.SearchInOrders)
             {
-                result.Add(new
+                int.TryParse(searchTerm, out int orderNumber);
+                if (orderNumber > 0)
                 {
-                    title = category.Name,
-                    link = Url.Content("~/Admin/Category/Edit/") + category.Id,
-                    source = "Categories"
-                });
+                    var order = _orderService.GetOrderByNumber(orderNumber);
+                    if (order != null)
+                    {
+                        result.Add(new Tuple<object, int>(new
+                        {
+                            title = order.OrderNumber,
+                            link = Url.Content("~/Admin/Order/Edit/") + order.Id,
+                            source = "Orders"
+                        }, _adminSearchSettings.OrdersDisplayOrder));
+                    }
+                }
             }
 
-            var manufacturers = _manufacturerService.GetAllManufacturers(searchTerm);
-            foreach (var manufacturer in manufacturers)
-            {
-                result.Add(new
-                {
-                    title = manufacturer.Name,
-                    link = Url.Content("~/Admin/Manufacturer/Edit/") + manufacturer.Id,
-                    source = "Manufacturers"
-                });
-            }
-
-            var topics = _topicService.GetAllTopics("", topicSystemName: searchTerm);
-            foreach (var topic in topics)
-            {
-                result.Add(new
-                {
-                    title = topic.SystemName,
-                    link = Url.Content("~/Admin/Topic/Edit/") + topic.Id,
-                    source = "Topics"
-                });
-            }
-
-            var news = _newsService.GetAllNews(newsTitle: searchTerm);
-            foreach (var signleNews in news)
-            {
-                result.Add(new
-                {
-                    title = signleNews.Title,
-                    link = Url.Content("~/Admin/News/Edit/") + signleNews.Id,
-                    source = "News"
-                });
-            }
-
-            var blogPosts = _blogService.GetAllBlogPosts(blogPostName: searchTerm);
-            foreach (var blogPost in blogPosts)
-            {
-                result.Add(new
-                {
-                    title = blogPost.Title,
-                    link = Url.Content("~/Admin/Blog/Edit/") + blogPost.Id,
-                    source = "Blog posts"
-                });
-            }
-
-            var customers = _customerService.getcus//name and username?
-
-            return Json(result);
+            result = result.OrderBy(x => x.Item2).ToList();
+            return Json(result.Select(x => x.Item1).ToList());
         }
     }
 }
---
+++ Grand.Web/wwwroot/Scripts/admin.search.js
@@ -0,0 +1,76 @@
+﻿var Admin = Admin || {};
+
+Admin.Search = (function () {
+    var itemTemplate = function (data) {
+        var result = '<div id="user-selection">' +
+            "<h5>" + data.title + "</h5>";
+
+        result = result + data.source
+
+        result = result + "</div>";
+        return result;
+    };
+
+    return {
+        init: function (url) {
+            var $input = $("#searchInput");
+            $input.blur(function (e) { e.preventDefault(); e.stopPropagation(); });
+            $input.typeahead({ minLength: 1, highlight: true, hint: false },
+                {
+                    name: "pages",
+                    displayKey: "name",
+                    templates: {
+                        suggestion: itemTemplate
+                    },
+                    source: function (q, sync, async) {
+
+                        var postData = {
+                            searchTerm: $("#searchInput").val(),
+                        };
+
+                        addAntiForgeryToken(postData);
+
+                        $.ajax({
+                            cache: false,
+                            type: "POST",
+                            url: url,
+                            dataType: 'json',
+                            data: postData,
+                            success: function (data) {
+                                async(data);
+                            },
+                            error: function () {
+                                alert('Error');
+                            }
+                        });
+                    },
+                    limit: 10
+                });
+
+            var navigateTo = function (item) {
+                $input.typeahead("val", "");
+                Admin.Navigation.open(item.link);
+            };
+
+            $input.on("typeahead:selected", function (e, item) {
+                navigateTo(item);
+            });
+        }
+    };
+})();
+
+var Admin = Admin || {};
+Admin.Navigation = (function () {
+    var events = {};
+    return {
+        open: function (url) {
+            if (events["open"]) {
+                var event = $.Event("open", { url: url });
+                events["open"].fire(event);
+                if (event.isDefaultPrevented())
+                    return;
+            }
+            window.location.href = url;
+        }
+    };
+})();
