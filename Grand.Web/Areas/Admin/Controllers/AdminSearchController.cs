eba0197d0274f7db696521d61728f78895f04925
KrzysztofPajak <kpajak@unit-soft.pl>
2018-09-05 12:29:03 +0200

    Admin panel - Added missing antiforgery token

--- Grand.Web/Areas/Admin/Controllers/AdminSearchController.cs
+++ Grand.Web/Areas/Admin/Controllers/AdminSearchController.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Grand.Web.Areas.Admin.Controllers
 {
