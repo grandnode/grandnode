5ca2e5515d0addf1e0c9c0a7ff2e01800c1cba4e
KrzysztofPajak <krzysiek@nop4you.com>
2018-11-06 14:32:31 +0100

    Added static class - localized extension  (for admin panel)

---
+++ Grand.Web/Areas/Admin/Helpers/LocalizedExtension.cs
@@ -0,0 +1,47 @@
+﻿using Grand.Core.Domain.Localization;
+using Grand.Framework.Localization;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Grand.Web.Areas.Admin.Helpers
+{
+    public static class LocalizedExtension
+    {
+        public static List<LocalizedProperty> ToLocalizedProperty<T>(this IList<T> list) where T : ILocalizedModelLocal
+        {
+            var local = new List<LocalizedProperty>();
+            foreach (var item in list)
+            {
+                Type[] interfaces = item.GetType().GetInterfaces();
+                PropertyInfo[] props = item.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+                foreach (var prop in props)
+                {
+                    bool insert = true;
+
+                    foreach (var i in interfaces)
+                    {
+                        if (i.HasProperty(prop.Name))
+                        {
+                            insert = false;
+                        }
+                    }
+
+                    if (insert && prop.GetValue(item) != null)
+                        local.Add(new LocalizedProperty()
+                        {
+                            LanguageId = item.LanguageId,
+                            LocaleKey = prop.Name,
+                            LocaleValue = prop.GetValue(item).ToString(),
+                        });
+                }
+            }
+            return local;
+        }
+
+        public static bool HasProperty(this Type obj, string propertyName)
+        {
+            return obj.GetProperty(propertyName) != null;
+        }
+    }
+}
