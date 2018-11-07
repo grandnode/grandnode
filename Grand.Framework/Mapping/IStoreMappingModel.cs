946bbcd8a18a608b57824270ddde072852a77175
KrzysztofPajak <krzysiek@nop4you.com>
2018-11-07 19:02:06 +0100

    Admin panel - implemented one file cshtml to support store mapping

--- Grand.Framework/Mapping/IStoreMappingModel.cs
+++ Grand.Framework/Mapping/IStoreMappingModel.cs
@@ -5,6 +5,7 @@ namespace Grand.Framework.Mapping
 {
     public interface IStoreMappingModel
     {
+        bool LimitedToStores { get; set; }
         List<StoreModel> AvailableStores { get; set; }
         string[] SelectedStoreIds { get; set; }
     }
