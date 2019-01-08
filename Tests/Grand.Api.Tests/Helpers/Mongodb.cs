1bf0172734ce328b841fe680db2656bf1e88cb2c
KrzysztofPajak <krzysiek@nop4you.com>
2019-01-08 15:45:54 +0100

    Add new project Grand.API.Tests

---
+++ Tests/Grand.Api.Tests/Helpers/Mongodb.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Conventions;
+
+namespace Grand.Api.Tests.Helpers
+{
+    public static class Mongodb
+    {
+        public static void IgnoreExtraElements()
+        {
+            var cp = new ConventionPack();
+            cp.Add(new IgnoreExtraElementsConvention(true));
+            ConventionRegistry.Register("ApplicationConventions", cp, t => true);
+        }
+    }
+}
