626e31356d58737eb97fed6cbf8e086345c7a603
KrzysztofPajak <krzysiek@nop4you.com>
2019-01-22 15:33:24 +0100

    Fix Permission authorization handler

--- Grand.Framework/Security/Authorization/PermissionAuthorizationHandler.cs
+++ Grand.Framework/Security/Authorization/PermissionAuthorizationHandler.cs
@@ -1,9 +1,11 @@
-﻿using Grand.Services.Security;
+﻿using Grand.Services.Customers;
+using Grand.Services.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Grand.Framework.Security.Authorization
@@ -11,31 +13,38 @@ namespace Grand.Framework.Security.Authorization
     public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
     {
         private readonly IPermissionService _permissionService;
+        private readonly ICustomerService _customerService;
         private readonly IHttpContextAccessor _contextAccessor;
-        public PermissionAuthorizationHandler(IPermissionService permissionService, IHttpContextAccessor contextAccessor)
+        public PermissionAuthorizationHandler(IPermissionService permissionService, ICustomerService customerService, IHttpContextAccessor contextAccessor)
         {
             this._permissionService = permissionService;
+            this._customerService = customerService;
             this._contextAccessor = contextAccessor;
         }
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
         {
-            if (!_permissionService.Authorize(requirement.Permission))
+            var email = context.User.FindFirst(c => c.Type == ClaimTypes.Email);
+            var customer = _customerService.GetCustomerByEmail(email.Value);
+            if (customer != null)
             {
-                var redirectContext = context.Resource as AuthorizationFilterContext;
-                var httpContext = _contextAccessor.HttpContext;
-                string authHeader = httpContext?.Request?.Headers["Authorization"];
-                var apirequest = authHeader != null && authHeader.Split(' ')[0] == JwtBearerDefaults.AuthenticationScheme;
-                if (apirequest)
+                if (!_permissionService.Authorize(requirement.Permission, customer))
                 {
-                    redirectContext.Result = new ForbidResult(JwtBearerDefaults.AuthenticationScheme);
+                    var redirectContext = context.Resource as AuthorizationFilterContext;
+                    var httpContext = _contextAccessor.HttpContext;
+                    string authHeader = httpContext?.Request?.Headers["Authorization"];
+                    var apirequest = authHeader != null && authHeader.Split(' ')[0] == JwtBearerDefaults.AuthenticationScheme;
+                    if (apirequest)
+                    {
+                        redirectContext.Result = new ForbidResult(JwtBearerDefaults.AuthenticationScheme);
+                    }
+                    else
+                    {
+                        redirectContext.Result = new RedirectToActionResult("AccessDenied", "Security", new { pageUrl = httpContext.Request.Path });
+                    }
                 }
-                else
-                {
-                    redirectContext.Result = new RedirectToActionResult("AccessDenied", "Security", new { pageUrl = httpContext.Request.Path });
-                }
+                context.Succeed(requirement);
             }
-            context.Succeed(requirement);
             return Task.CompletedTask;
         }
     }
