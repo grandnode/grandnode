8ddd5d0557c93c6b9e2e2bc161caa6f476f7fe5e
Prashant Khandelwal <prashant@midnightprogrammer.net>
2019-03-14 20:31:23 +0530

    #485: Fix problem with "Subscribed on" date on Newsletter Subscribers page.

--- Grand.Web/Areas/Admin/Controllers/NewsLetterSubscriptionController.cs
+++ Grand.Web/Areas/Admin/Controllers/NewsLetterSubscriptionController.cs
@@ -127,7 +127,7 @@ namespace Grand.Web.Areas.Admin.Controllers
 					var m = x.ToModel();
 				    var store = _storeService.GetStoreById(x.StoreId);
 				    m.StoreName = store != null ? store.Name : "Unknown store";
-					m.CreatedOn = _dateTimeHelper.ConvertToUserTime(x.CreatedOnUtc, DateTimeKind.Utc).ToLongTimeString();
+					m.CreatedOn = _dateTimeHelper.ConvertToUserTime(x.CreatedOnUtc, DateTimeKind.Utc).ToString();
                     m.Categories = GetCategoryNames(x.Categories.ToList());
                     return m;
 				}),
