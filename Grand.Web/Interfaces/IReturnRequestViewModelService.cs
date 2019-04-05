ac45ee2752f726343e39958999ba9db93ea09d60
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 15:57:47 +0200

    Extend the usage of async methods #438 - Web Return request View Model Service

--- Grand.Web/Interfaces/IReturnRequestViewModelService.cs
+++ Grand.Web/Interfaces/IReturnRequestViewModelService.cs
@@ -1,12 +1,13 @@
 ﻿using Grand.Core.Domain.Orders;
 using Grand.Web.Models.Order;
+using System.Threading.Tasks;
 
 namespace Grand.Web.Interfaces
 {
     public partial interface IReturnRequestViewModelService
     {
-        SubmitReturnRequestModel PrepareReturnRequest(SubmitReturnRequestModel model, Order order);
-        ReturnRequestDetailsModel PrepareReturnRequestDetails(ReturnRequest returnRequest, Order order);
-        CustomerReturnRequestsModel PrepareCustomerReturnRequests();
+        Task<SubmitReturnRequestModel> PrepareReturnRequest(SubmitReturnRequestModel model, Order order);
+        Task<ReturnRequestDetailsModel> PrepareReturnRequestDetails(ReturnRequest returnRequest, Order order);
+        Task<CustomerReturnRequestsModel> PrepareCustomerReturnRequests();
     }
 }
