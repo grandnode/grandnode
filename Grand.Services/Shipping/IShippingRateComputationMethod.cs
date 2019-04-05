8dca7b5ba0fcf011168cebb31e3d5aab239e93ca
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 15:15:37 +0200

    Extend the usage of async methods #438 - shipping rate

--- Grand.Services/Shipping/IShippingRateComputationMethod.cs
+++ Grand.Services/Shipping/IShippingRateComputationMethod.cs
@@ -3,6 +3,7 @@ using Grand.Core.Plugins;
 using Grand.Services.Shipping.Tracking;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Shipping
 {
@@ -21,21 +22,21 @@ namespace Grand.Services.Shipping
         /// </summary>
         /// <param name="getShippingOptionRequest">A request for getting shipping options</param>
         /// <returns>Represents a response of getting shipping rate options</returns>
-        GetShippingOptionResponse GetShippingOptions(GetShippingOptionRequest getShippingOptionRequest);
+        Task<GetShippingOptionResponse> GetShippingOptions(GetShippingOptionRequest getShippingOptionRequest);
 
         /// <summary>
         /// Returns a value indicating whether shipping methods should be hidden during checkout
         /// </summary>
         /// <param name="cart">Shoping cart</param>
         /// <returns>true - hide; false - display.</returns>
-        bool HideShipmentMethods(IList<ShoppingCartItem> cart);
+        Task<bool> HideShipmentMethods(IList<ShoppingCartItem> cart);
 
         /// <summary>
         /// Gets fixed shipping rate (if shipping rate computation method allows it and the rate can be calculated before checkout).
         /// </summary>
         /// <param name="getShippingOptionRequest">A request for getting shipping options</param>
         /// <returns>Fixed shipping rate; or null in case there's no fixed shipping rate</returns>
-        decimal? GetFixedRate(GetShippingOptionRequest getShippingOptionRequest);
+        Task<decimal?> GetFixedRate(GetShippingOptionRequest getShippingOptionRequest);
 
         /// <summary>
         /// Gets a shipment tracker
@@ -43,7 +44,7 @@ namespace Grand.Services.Shipping
         IShipmentTracker ShipmentTracker { get; }
 
 
-        IList<string> ValidateShippingForm(IFormCollection form);
+        Task<IList<string>> ValidateShippingForm(IFormCollection form);
 
         /// <summary>
         /// Gets a view component for displaying plugin in public store ("shipping" checkout step)
