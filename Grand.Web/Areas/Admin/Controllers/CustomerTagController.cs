290cc32a1e2ca73c3f43dc7122dc9a55eb9d7b6a
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 15:31:45 +0200

    Extend the usage of async methods #438 - admin customer tag controller

--- Grand.Web/Areas/Admin/Controllers/CustomerTagController.cs
+++ Grand.Web/Areas/Admin/Controllers/CustomerTagController.cs
@@ -13,6 +13,8 @@ using Grand.Web.Areas.Admin.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Grand.Web.Areas.Admin.Controllers
 {
@@ -46,30 +48,33 @@ namespace Grand.Web.Areas.Admin.Controllers
         public IActionResult List() => View();
 
         [HttpPost]
-        public IActionResult List(DataSourceRequest command)
+        public async Task<IActionResult> List(DataSourceRequest command)
         {
-            var customertags = _customerTagService.GetAllCustomerTags();
+            var customertags = await _customerTagService.GetAllCustomerTags();
+            var items = new List<(string Id, string Name, int Count)>();
+            foreach (var item in customertags)
+            {
+                items.Add((item.Id, item.Name, await _customerTagService.GetCustomerCount(item.Id)));
+            }
             var gridModel = new DataSourceResult
             {
-                Data = customertags.Select(x => new { Id = x.Id, Name = x.Name, Count = _customerTagService.GetCustomerCount(x.Id) }),
+                Data = items,
                 Total = customertags.Count()
             };
             return Json(gridModel);
         }
 
         [HttpGet]
-        public IActionResult Search(string term)
+        public async Task<IActionResult> Search(string term)
         {
-            var customertags = _customerTagService.GetCustomerTagsByName(term).Select(x => x.Name);
+            var customertags = (await _customerTagService.GetCustomerTagsByName(term)).Select(x => x.Name);
             return Json(customertags);
         }
 
-
-
         [HttpPost]
-        public IActionResult Customers(string customerTagId, DataSourceRequest command)
+        public async Task<IActionResult> Customers(string customerTagId, DataSourceRequest command)
         {
-            var customers = _customerTagService.GetCustomersByTag(customerTagId, command.Page - 1, command.PageSize);
+            var customers = await _customerTagService.GetCustomersByTag(customerTagId, command.Page - 1, command.PageSize);
             var gridModel = new DataSourceResult
             {
                 Data = customers.Select(x => _customerTagViewModelService.PrepareCustomerModelForList(x)),
@@ -85,11 +90,11 @@ namespace Grand.Web.Areas.Admin.Controllers
         }
 
         [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
-        public IActionResult Create(CustomerTagModel model, bool continueEditing)
+        public async Task<IActionResult> Create(CustomerTagModel model, bool continueEditing)
         {
             if (ModelState.IsValid)
             {
-                var customertag = _customerTagViewModelService.InsertCustomerTagModel(model);
+                var customertag = await _customerTagViewModelService.InsertCustomerTagModel(model);
                 SuccessNotification(_localizationService.GetResource("Admin.Customers.CustomerTags.Added"));
                 return continueEditing ? RedirectToAction("Edit", new { id = customertag.Id }) : RedirectToAction("List");
             }
@@ -98,9 +103,9 @@ namespace Grand.Web.Areas.Admin.Controllers
             return View(model);
         }
 
-        public IActionResult Edit(string id)
+        public async Task<IActionResult> Edit(string id)
         {
-            var customerTag = _customerTagService.GetCustomerTagById(id);
+            var customerTag = await _customerTagService.GetCustomerTagById(id);
             if (customerTag == null)
                 //No customer role found with the specified id
                 return RedirectToAction("List");
@@ -110,9 +115,9 @@ namespace Grand.Web.Areas.Admin.Controllers
         }
 
         [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
-        public IActionResult Edit(CustomerTagModel model, bool continueEditing)
+        public async Task<IActionResult> Edit(CustomerTagModel model, bool continueEditing)
         {
-            var customertag = _customerTagService.GetCustomerTagById(model.Id);
+            var customertag = await _customerTagService.GetCustomerTagById(model.Id);
             if (customertag == null)
                 //No customer role found with the specified id
                 return RedirectToAction("List");
@@ -121,7 +126,7 @@ namespace Grand.Web.Areas.Admin.Controllers
             {
                 if (ModelState.IsValid)
                 {
-                    customertag = _customerTagViewModelService.UpdateCustomerTagModel(customertag, model);
+                    customertag = await _customerTagViewModelService.UpdateCustomerTagModel(customertag, model);
                     SuccessNotification(_localizationService.GetResource("Admin.Customers.CustomerTags.Updated"));
                     return continueEditing ? RedirectToAction("Edit", new { id = customertag.Id }) : RedirectToAction("List");
                 }
@@ -137,23 +142,23 @@ namespace Grand.Web.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult CustomerDelete(string Id, string customerTagId)
+        public async Task<IActionResult> CustomerDelete(string Id, string customerTagId)
         {
-            var customertag = _customerTagService.GetCustomerTagById(customerTagId);
+            var customertag = await _customerTagService.GetCustomerTagById(customerTagId);
             if (customertag == null)
                 throw new ArgumentException("No customertag found with the specified id");
             if (ModelState.IsValid)
             {
-                _customerTagService.DeleteTagFromCustomer(customerTagId, Id);
+                await _customerTagService.DeleteTagFromCustomer(customerTagId, Id);
                 return new NullJsonResult();
             }
             return ErrorForKendoGridJson(ModelState);
         }
 
         [HttpPost]
-        public IActionResult Delete(string id)
+        public async Task<IActionResult> Delete(string id)
         {
-            var customerTag = _customerTagService.GetCustomerTagById(id);
+            var customerTag = await _customerTagService.GetCustomerTagById(id);
             if (customerTag == null)
                 //No customer role found with the specified id
                 return RedirectToAction("List");
@@ -162,7 +167,7 @@ namespace Grand.Web.Areas.Admin.Controllers
             {
                 if (ModelState.IsValid)
                 {
-                    _customerTagViewModelService.DeleteCustomerTag(customerTag);
+                    await _customerTagViewModelService.DeleteCustomerTag(customerTag);
                     SuccessNotification(_localizationService.GetResource("Admin.Customers.CustomerTags.Deleted"));
                     return RedirectToAction("List");
                 }
@@ -180,63 +185,67 @@ namespace Grand.Web.Areas.Admin.Controllers
         #region Products
 
         [HttpPost]
-        public IActionResult Products(string customerTagId, DataSourceRequest command, [FromServices] IProductService productService)
+        public async Task<IActionResult> Products(string customerTagId, DataSourceRequest command, [FromServices] IProductService productService)
         {
-            var products = _customerTagService.GetCustomerTagProducts(customerTagId);
-
-            var gridModel = new DataSourceResult
+            var products = await _customerTagService.GetCustomerTagProducts(customerTagId);
+            var items = new List<CustomerRoleProductModel>();
+            foreach (var x in products)
             {
-                Data = products.Select(x => new CustomerRoleProductModel
+                items.Add(new CustomerRoleProductModel
                 {
                     Id = x.Id,
-                    Name = productService.GetProductById(x.ProductId)?.Name,
+                    Name = (await productService.GetProductById(x.ProductId))?.Name,
                     ProductId = x.ProductId,
                     DisplayOrder = x.DisplayOrder
-                }),
+                });
+            }
+            var gridModel = new DataSourceResult
+            {
+                Data = items,
                 Total = products.Count()
             };
             return Json(gridModel);
         }
 
         [HttpPost]
-        public IActionResult ProductDelete(string id)
+        public async Task<IActionResult> ProductDelete(string id)
         {
-            var ctp = _customerTagService.GetCustomerTagProductById(id);
+            var ctp = await _customerTagService.GetCustomerTagProductById(id);
             if (ctp == null)
                 throw new ArgumentException("No found the specified id");
             if (ModelState.IsValid)
             {
-                _customerTagService.DeleteCustomerTagProduct(ctp);
+                await _customerTagService.DeleteCustomerTagProduct(ctp);
                 return new NullJsonResult();
             }
             return ErrorForKendoGridJson(ModelState);
         }
 
         [HttpPost]
-        public IActionResult ProductUpdate(CustomerRoleProductModel model)
+        public async Task<IActionResult> ProductUpdate(CustomerRoleProductModel model)
         {
-            var ctp = _customerTagService.GetCustomerTagProductById(model.Id);
+            var ctp = await _customerTagService.GetCustomerTagProductById(model.Id);
             if (ctp == null)
                 throw new ArgumentException("No customer tag product found with the specified id");
             if (ModelState.IsValid)
             {
                 ctp.DisplayOrder = model.DisplayOrder;
-                _customerTagService.UpdateCustomerTagProduct(ctp);
+                await _customerTagService.UpdateCustomerTagProduct(ctp);
                 return new NullJsonResult();
             }
             return ErrorForKendoGridJson(ModelState);
         }
 
-        public IActionResult ProductAddPopup(string customerTagId)
+        public async Task<IActionResult> ProductAddPopup(string customerTagId)
         {
-            var model = _customerTagViewModelService.PrepareProductModel(customerTagId);
+            var model = await _customerTagViewModelService.PrepareProductModel(customerTagId);
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult ProductAddPopupList(DataSourceRequest command, CustomerTagProductModel.AddProductModel model)
+        public async Task<IActionResult> ProductAddPopupList(DataSourceRequest command, CustomerTagProductModel.AddProductModel model)
         {
-            var products = _customerTagViewModelService.PrepareProductModel(model, command.Page, command.PageSize);
+            var products = await _customerTagViewModelService.PrepareProductModel(model, command.Page, command.PageSize);
             var gridModel = new DataSourceResult
             {
                 Data = products.products,
@@ -247,11 +256,11 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         [HttpPost]
         [FormValueRequired("save")]
-        public IActionResult ProductAddPopup(CustomerTagProductModel.AddProductModel model)
+        public async Task<IActionResult> ProductAddPopup(CustomerTagProductModel.AddProductModel model)
         {
             if (model.SelectedProductIds != null)
             {
-                _customerTagViewModelService.InsertProductModel(model);
+                await _customerTagViewModelService.InsertProductModel(model);
             }
 
             //a vendor should have access only to his products
