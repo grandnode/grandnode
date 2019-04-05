6f7c24df84e626d5172ab2255c07174ef88e6be8
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 14:50:39 +0200

    Extend the usage of async methods #438 - Exchange rate provider

--- Grand.Services/Directory/IExchangeRateProvider.cs
+++ Grand.Services/Directory/IExchangeRateProvider.cs
@@ -1,6 +1,7 @@
 using Grand.Core.Domain.Directory;
 using Grand.Core.Plugins;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Directory
 {
@@ -14,6 +15,6 @@ namespace Grand.Services.Directory
         /// </summary>
         /// <param name="exchangeRateCurrencyCode">Exchange rate currency code</param>
         /// <returns>Exchange rates</returns>
-        IList<ExchangeRate> GetCurrencyLiveRates(string exchangeRateCurrencyCode);
+        Task<IList<ExchangeRate>> GetCurrencyLiveRates(string exchangeRateCurrencyCode);
     }
 }
