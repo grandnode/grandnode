9100ce009b48a2bb11f7c3362ac6f2333221c45c
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 14:59:39 +0200

    Extend the usage of async methods #438 - contact attribute service

--- Grand.Services/Messages/IContactAttributeService.cs
+++ Grand.Services/Messages/IContactAttributeService.cs
@@ -1,5 +1,6 @@
 using Grand.Core.Domain.Messages;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Messages
 {
@@ -14,36 +15,34 @@ namespace Grand.Services.Messages
         /// Deletes a contact attribute
         /// </summary>
         /// <param name="contactAttribute">Contact attribute</param>
-        void DeleteContactAttribute(ContactAttribute contactAttribute);
+        Task DeleteContactAttribute(ContactAttribute contactAttribute);
 
         /// <summary>
         /// Gets all Contact attributes
         /// </summary>
         /// <param name="storeId">Store identifier</param>
         /// <returns>Contact attributes</returns>
-        IList<ContactAttribute> GetAllContactAttributes(string storeId = "", bool ignorAcl = false);
+        Task<IList<ContactAttribute>> GetAllContactAttributes(string storeId = "", bool ignorAcl = false);
 
         /// <summary>
         /// Gets a Contact attribute 
         /// </summary>
         /// <param name="contactAttributeId">Contact attribute identifier</param>
         /// <returns>Contact attribute</returns>
-        ContactAttribute GetContactAttributeById(string contactAttributeId);
+        Task<ContactAttribute> GetContactAttributeById(string contactAttributeId);
 
         /// <summary>
         /// Inserts a Contact attribute
         /// </summary>
         /// <param name="contactAttribute">Contact attribute</param>
-        void InsertContactAttribute(ContactAttribute contactAttribute);
+        Task InsertContactAttribute(ContactAttribute contactAttribute);
 
         /// <summary>
         /// Updates the Contact attribute
         /// </summary>
         /// <param name="contactAttribute">Contact attribute</param>
-        void UpdateContactAttribute(ContactAttribute contactAttribute);
+        Task UpdateContactAttribute(ContactAttribute contactAttribute);
 
         #endregion
-
-        
     }
 }
