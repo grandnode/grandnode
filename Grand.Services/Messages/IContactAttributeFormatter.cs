75ae3ab46cd8ebd97eb9fe66d5b3e00e6d274807
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 14:59:05 +0200

    Extend the usage of async methods #438 - contact attrobute formatter

--- Grand.Services/Messages/IContactAttributeFormatter.cs
+++ Grand.Services/Messages/IContactAttributeFormatter.cs
@@ -1,4 +1,5 @@
 using Grand.Core.Domain.Customers;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Messages
 {
@@ -12,7 +13,7 @@ namespace Grand.Services.Messages
         /// </summary>
         /// <param name="attributesXml">Attributes in XML format</param>
         /// <returns>Attributes</returns>
-        string FormatAttributes(string attributesXml);
+        Task<string> FormatAttributes(string attributesXml);
 
         /// <summary>
         /// Formats attributes
@@ -23,7 +24,7 @@ namespace Grand.Services.Messages
         /// <param name="htmlEncode">A value indicating whether to encode (HTML) values</param>
         /// <param name="allowHyperlinks">A value indicating whether to HTML hyperink tags could be rendered (if required)</param>
         /// <returns>Attributes</returns>
-        string FormatAttributes(string attributesXml,
+        Task<string> FormatAttributes(string attributesXml,
             Customer customer, 
             string serapator = "<br />", 
             bool htmlEncode = true,
