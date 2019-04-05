793e33c01b00ac8ee225f0f1f63bb008082c2ad0
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 14:49:40 +0200

    Extend the usage of async methods #438 - customer tag service

--- Grand.Services/Customers/CustomerTagService.cs
+++ Grand.Services/Customers/CustomerTagService.cs
@@ -6,9 +6,11 @@ using Grand.Core.Domain.Customers;
 using Grand.Services.Events;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Customers
 {
@@ -65,29 +67,28 @@ namespace Grand.Services.Customers
         /// Gets all customer for tag id
         /// </summary>
         /// <returns>Customers</returns>
-        public virtual IPagedList<Customer> GetCustomersByTag(string customerTagId = "", int pageIndex = 0, int pageSize = 2147483647)
+        public virtual async Task<IPagedList<Customer>> GetCustomersByTag(string customerTagId = "", int pageIndex = 0, int pageSize = 2147483647)
         {
             var query = from c in _customerRepository.Table
                         where c.CustomerTags.Contains(customerTagId)
                         select c;
-            var customers = new PagedList<Customer>(query, pageIndex, pageSize);
-            return customers;
+            return await Task.FromResult(new PagedList<Customer>(query, pageIndex, pageSize));
         }
 
         /// <summary>
         /// Delete a customer tag
         /// </summary>
         /// <param name="customerTag">Customer tag</param>
-        public virtual void DeleteCustomerTag(CustomerTag customerTag)
+        public virtual async Task DeleteCustomerTag(CustomerTag customerTag)
         {
             if (customerTag == null)
                 throw new ArgumentNullException("productTag");
 
             var builder = Builders<Customer>.Update;
             var updatefilter = builder.Pull(x => x.CustomerTags, customerTag.Id);
-            var result = _customerRepository.Collection.UpdateManyAsync(new BsonDocument(), updatefilter).Result;
+            await _customerRepository.Collection.UpdateManyAsync(new BsonDocument(), updatefilter);
 
-            _customerTagRepository.Delete(customerTag);
+            await _customerTagRepository.DeleteAsync(customerTag);
 
             //event notification
             _eventPublisher.EntityDeleted(customerTag);
@@ -97,10 +98,10 @@ namespace Grand.Services.Customers
         /// Gets all customer tags
         /// </summary>
         /// <returns>Customer tags</returns>
-        public virtual IList<CustomerTag> GetAllCustomerTags()
+        public virtual async Task<IList<CustomerTag>> GetAllCustomerTags()
         {
             var query = _customerTagRepository.Table;
-            return query.ToList();
+            return await query.ToListAsync();
         }
 
         /// <summary>
@@ -108,9 +109,9 @@ namespace Grand.Services.Customers
         /// </summary>
         /// <param name="customerTagId">Customer tag identifier</param>
         /// <returns>Customer tag</returns>
-        public virtual CustomerTag GetCustomerTagById(string customerTagId)
+        public virtual Task<CustomerTag> GetCustomerTagById(string customerTagId)
         {
-            return _customerTagRepository.GetById(customerTagId);
+            return _customerTagRepository.GetByIdAsync(customerTagId);
         }
 
         /// <summary>
@@ -118,13 +119,13 @@ namespace Grand.Services.Customers
         /// </summary>
         /// <param name="name">Customer tag name</param>
         /// <returns>Customer tag</returns>
-        public virtual CustomerTag GetCustomerTagByName(string name)
+        public virtual Task<CustomerTag> GetCustomerTagByName(string name)
         {
             var query = from pt in _customerTagRepository.Table
                         where pt.Name == name
                         select pt;
 
-            return query.FirstOrDefault(); 
+            return query.FirstOrDefaultAsync(); 
         }
 
         /// <summary>
@@ -132,24 +133,24 @@ namespace Grand.Services.Customers
         /// </summary>
         /// <param name="name">Customer tags name</param>
         /// <returns>Customer tags</returns>
-        public virtual IList<CustomerTag> GetCustomerTagsByName(string name)
+        public virtual async Task<IList<CustomerTag>> GetCustomerTagsByName(string name)
         {
             var query = from pt in _customerTagRepository.Table
                         where pt.Name.ToLower().Contains(name.ToLower())
                         select pt;
-            return query.ToList();
+            return await query.ToListAsync();
         }
 
         /// <summary>
         /// Inserts a customer tag
         /// </summary>
         /// <param name="customerTag">Customer tag</param>
-        public virtual void InsertCustomerTag(CustomerTag customerTag)
+        public virtual async Task InsertCustomerTag(CustomerTag customerTag)
         {
             if (customerTag == null)
                 throw new ArgumentNullException("customerTag");
 
-            _customerTagRepository.Insert(customerTag);
+            await _customerTagRepository.InsertAsync(customerTag);
 
             //event notification
             _eventPublisher.EntityInserted(customerTag);
@@ -158,33 +159,33 @@ namespace Grand.Services.Customers
         /// <summary>
         /// Insert tag to a customer
         /// </summary>
-        public virtual void InsertTagToCustomer(string customerTagId, string customerId)
+        public virtual async Task InsertTagToCustomer(string customerTagId, string customerId)
         {
             var updatebuilder = Builders<Customer>.Update;
             var update = updatebuilder.AddToSet(p => p.CustomerTags, customerTagId);
-            _customerRepository.Collection.UpdateOneAsync(new BsonDocument("_id", customerId), update);
+            await _customerRepository.Collection.UpdateOneAsync(new BsonDocument("_id", customerId), update);
         }
 
         /// <summary>
         /// Delete tag from a customer
         /// </summary>
-        public virtual void DeleteTagFromCustomer(string customerTagId, string customerId)
+        public virtual async Task DeleteTagFromCustomer(string customerTagId, string customerId)
         {
             var updatebuilder = Builders<Customer>.Update;
             var update = updatebuilder.Pull(p => p.CustomerTags, customerTagId);
-            _customerRepository.Collection.UpdateOneAsync(new BsonDocument("_id", customerId), update);
+            await _customerRepository.Collection.UpdateOneAsync(new BsonDocument("_id", customerId), update);
         }
 
         /// <summary>
         /// Updates the customer tag
         /// </summary>
         /// <param name="customerTag">Customer tag</param>
-        public virtual void UpdateCustomerTag(CustomerTag customerTag)
+        public virtual async Task UpdateCustomerTag(CustomerTag customerTag)
         {
             if (customerTag == null)
                 throw new ArgumentNullException("customerTag");
 
-            _customerTagRepository.Update(customerTag);
+            await _customerTagRepository.UpdateAsync(customerTag);
 
             //event notification
             _eventPublisher.EntityUpdated(customerTag);
@@ -195,11 +196,13 @@ namespace Grand.Services.Customers
         /// </summary>
         /// <param name="customerTagId">Customer tag identifier</param>
         /// <returns>Number of customers</returns>
-        public virtual int GetCustomerCount(string customerTagId)
+        public virtual async Task<int> GetCustomerCount(string customerTagId)
         {
-            var query = _customerRepository.Table.Where(x => x.CustomerTags.Contains(customerTagId)).GroupBy(p => p, (k, s) => new { Count = s.Count() }).ToList();
-            if (query.Count > 0)
-                return query.FirstOrDefault().Count;
+            var query = await _customerRepository.Table.
+                Where(x => x.CustomerTags.Contains(customerTagId)).
+                GroupBy(p => p, (k, s) => new { Counter = s.Count() }).ToListAsync();
+            if(query.Count > 0)
+                return query.FirstOrDefault().Counter;
             return 0;
         }
 
@@ -211,17 +214,16 @@ namespace Grand.Services.Customers
         /// </summary>
         /// <param name="customerTagId">Customer tag id</param>
         /// <returns>Customer tag products</returns>
-        public virtual IList<CustomerTagProduct> GetCustomerTagProducts(string customerTagId)
+        public virtual async Task<IList<CustomerTagProduct>> GetCustomerTagProducts(string customerTagId)
         {
             string key = string.Format(CUSTOMERTAGPRODUCTS_ROLE_KEY, customerTagId);
-            return _cacheManager.Get(key, () =>
+            return await _cacheManager.Get(key, () =>
             {
                 var query = from cr in _customerTagProductRepository.Table
                             where (cr.CustomerTagId == customerTagId)
                             orderby cr.DisplayOrder
                             select cr;
-                var customerRoles = query.ToList();
-                return customerRoles;
+                return query.ToListAsync();
             });
         }
 
@@ -231,14 +233,13 @@ namespace Grand.Services.Customers
         /// <param name="customerTagId">Customer tag id</param>
         /// <param name="productId">Product id</param>
         /// <returns>Customer tag product</returns>
-        public virtual CustomerTagProduct GetCustomerTagProduct(string customerTagId, string productId)
+        public virtual Task<CustomerTagProduct> GetCustomerTagProduct(string customerTagId, string productId)
         {
             var query = from cr in _customerTagProductRepository.Table
                         where cr.CustomerTagId == customerTagId && cr.ProductId == productId
                         orderby cr.DisplayOrder
                         select cr;
-            var customerRoles = query.ToList();
-            return query.FirstOrDefault();
+            return query.FirstOrDefaultAsync();
         }
 
         /// <summary>
@@ -246,21 +247,21 @@ namespace Grand.Services.Customers
         /// </summary>
         /// <param name="Id">id</param>
         /// <returns>Customer tag product</returns>
-        public virtual CustomerTagProduct GetCustomerTagProductById(string id)
+        public virtual Task<CustomerTagProduct> GetCustomerTagProductById(string id)
         {
-            return _customerTagProductRepository.GetById(id);
+            return _customerTagProductRepository.GetByIdAsync(id);
         }
 
         /// <summary>
         /// Inserts a customer tag product
         /// </summary>
         /// <param name="customerTagProduct">Customer tag product</param>
-        public virtual void InsertCustomerTagProduct(CustomerTagProduct customerTagProduct)
+        public virtual async Task InsertCustomerTagProduct(CustomerTagProduct customerTagProduct)
         {
             if (customerTagProduct == null)
                 throw new ArgumentNullException("customerTagProduct");
 
-            _customerTagProductRepository.Insert(customerTagProduct);
+            await _customerTagProductRepository.InsertAsync(customerTagProduct);
 
             //clear cache
             _cacheManager.RemoveByPattern(string.Format(CUSTOMERTAGPRODUCTS_ROLE_KEY, customerTagProduct.CustomerTagId));
@@ -274,12 +275,12 @@ namespace Grand.Services.Customers
         /// Updates the customer tag product
         /// </summary>
         /// <param name="customerTagProduct">Customer tag product</param>
-        public virtual void UpdateCustomerTagProduct(CustomerTagProduct customerTagProduct)
+        public virtual async Task UpdateCustomerTagProduct(CustomerTagProduct customerTagProduct)
         {
             if (customerTagProduct == null)
                 throw new ArgumentNullException("customerTagProduct");
 
-            _customerTagProductRepository.Update(customerTagProduct);
+            await _customerTagProductRepository.UpdateAsync(customerTagProduct);
 
             //clear cache
             _cacheManager.RemoveByPattern(string.Format(CUSTOMERTAGPRODUCTS_ROLE_KEY, customerTagProduct.CustomerTagId));
@@ -293,12 +294,12 @@ namespace Grand.Services.Customers
         /// Delete a customer tag product
         /// </summary>
         /// <param name="customerTagProduct">Customer tag product</param>
-        public virtual void DeleteCustomerTagProduct(CustomerTagProduct customerTagProduct)
+        public virtual async Task DeleteCustomerTagProduct(CustomerTagProduct customerTagProduct)
         {
             if (customerTagProduct == null)
                 throw new ArgumentNullException("customerTagProduct");
 
-            _customerTagProductRepository.Delete(customerTagProduct);
+            await _customerTagProductRepository.DeleteAsync(customerTagProduct);
 
             //clear cache
             _cacheManager.RemoveByPattern(string.Format(CUSTOMERTAGPRODUCTS_ROLE_KEY, customerTagProduct.CustomerTagId));
