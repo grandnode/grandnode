fc4b0435e23ede6b923b5b555b22826fc1b18698
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 14:58:43 +0200

    Extend the usage of async methods #438 - campaign service

--- Grand.Services/Messages/ICampaignService.cs
+++ Grand.Services/Messages/ICampaignService.cs
@@ -1,6 +1,7 @@
 ﻿using Grand.Core;
 using Grand.Core.Domain.Messages;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Messages
 {
@@ -10,50 +11,50 @@ namespace Grand.Services.Messages
         /// Inserts a campaign
         /// </summary>
         /// <param name="campaign">Campaign</param>        
-        void InsertCampaign(Campaign campaign);
+        Task InsertCampaign(Campaign campaign);
 
         /// <summary>
         /// Inserts a campaign history
         /// </summary>
         /// <param name="campaign">Campaign</param>        
-        void InsertCampaignHistory(CampaignHistory campaignhistory);
+        Task InsertCampaignHistory(CampaignHistory campaignhistory);
         /// <summary>
         /// Updates a campaign
         /// </summary>
         /// <param name="campaign">Campaign</param>
-        void UpdateCampaign(Campaign campaign);
+        Task UpdateCampaign(Campaign campaign);
 
         /// <summary>
         /// Deleted a queued email
         /// </summary>
         /// <param name="campaign">Campaign</param>
-        void DeleteCampaign(Campaign campaign);
+        Task DeleteCampaign(Campaign campaign);
 
         /// <summary>
         /// Gets a campaign by identifier
         /// </summary>
         /// <param name="campaignId">Campaign identifier</param>
         /// <returns>Campaign</returns>
-        Campaign GetCampaignById(string campaignId);
+        Task<Campaign> GetCampaignById(string campaignId);
 
         /// <summary>
         /// Gets all campaigns
         /// </summary>
         /// <returns>Campaigns</returns>
-        IList<Campaign> GetAllCampaigns();
+        Task<IList<Campaign>> GetAllCampaigns();
 
         /// <summary>
         /// Gets campaign history 
         /// </summary>
         /// <returns>Campaigns</returns>
-        IPagedList<CampaignHistory> GetCampaignHistory(Campaign campaign, int pageIndex = 0, int pageSize = int.MaxValue);
+        Task<IPagedList<CampaignHistory>> GetCampaignHistory(Campaign campaign, int pageIndex = 0, int pageSize = int.MaxValue);
 
         /// <summary>
         /// Gets customer subscriptions
         /// </summary>
         /// <param name="Campaign">Campaign</param>
         /// <returns>CampaignCustomerSubscription</returns>
-        IPagedList<NewsLetterSubscription> CustomerSubscriptions(Campaign campaign, int pageIndex = 0, int pageSize = int.MaxValue);
+        Task<IPagedList<NewsLetterSubscription>> CustomerSubscriptions(Campaign campaign, int pageIndex = 0, int pageSize = int.MaxValue);
 
         /// <summary>
         /// Sends a campaign to specified emails
@@ -62,7 +63,7 @@ namespace Grand.Services.Messages
         /// <param name="emailAccount">Email account</param>
         /// <param name="subscriptions">Subscriptions</param>
         /// <returns>Total emails sent</returns>
-        int SendCampaign(Campaign campaign, EmailAccount emailAccount,
+        Task<int> SendCampaign(Campaign campaign, EmailAccount emailAccount,
             IEnumerable<NewsLetterSubscription> subscriptions);
 
         /// <summary>
@@ -71,6 +72,6 @@ namespace Grand.Services.Messages
         /// <param name="campaign">Campaign</param>
         /// <param name="emailAccount">Email account</param>
         /// <param name="email">Email</param>
-        void SendCampaign(Campaign campaign, EmailAccount emailAccount, string email);
+        Task SendCampaign(Campaign campaign, EmailAccount emailAccount, string email);
     }
 }
