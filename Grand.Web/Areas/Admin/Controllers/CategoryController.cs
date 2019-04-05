987dc2b8b331daeeba338e891318f0ef02a71384
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 15:28:43 +0200

    Extend the usage of async methods #438 - admin category controller

--- Grand.Web/Areas/Admin/Controllers/CategoryController.cs
+++ Grand.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Grand.Web.Areas.Admin.Controllers
 {
@@ -69,16 +70,16 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         public IActionResult Index() => RedirectToAction("List");
 
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
-            var model = _categoryViewModelService.PrepareCategoryListModel();
+            var model = await _categoryViewModelService.PrepareCategoryListModel();
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult List(DataSourceRequest command, CategoryListModel model)
+        public async Task<IActionResult> List(DataSourceRequest command, CategoryListModel model)
         {
-            var categories = _categoryViewModelService.PrepareCategoryListModel(model, command.Page, command.PageSize);
+            var categories = await _categoryViewModelService.PrepareCategoryListModel(model, command.Page, command.PageSize);
             var gridModel = new DataSourceResult
             {
                 Data = categories.categoryListModel,
@@ -89,9 +90,9 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         public IActionResult Tree() => View();
 
-        public IActionResult NodeList()
+        public async Task<IActionResult> NodeList()
         {
-            var model = _categoryViewModelService.PrepareCategoryNodeListModel();
+            var model = await _categoryViewModelService.PrepareCategoryNodeListModel();
             return Json(model);
         }
 
@@ -99,45 +100,45 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         #region Create / Edit / Delete
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            var model = _categoryViewModelService.PrepareCategoryModel();
+            var model = await _categoryViewModelService.PrepareCategoryModel();
             //locales
-            AddLocales(_languageService, model.Locales);
+            await AddLocales(_languageService, model.Locales);
 
             return View(model);
         }
 
         [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
-        public IActionResult Create(CategoryModel model, bool continueEditing)
+        public async Task<IActionResult> Create(CategoryModel model, bool continueEditing)
         {
             if (ModelState.IsValid)
             {
-                var category = _categoryViewModelService.InsertCategoryModel(model);
+                var category = await _categoryViewModelService.InsertCategoryModel(model);
                 SuccessNotification(_localizationService.GetResource("Admin.Catalog.Categories.Added"));
                 return continueEditing ? RedirectToAction("Edit", new { id = category.Id }) : RedirectToAction("List");
             }
 
             //If we got this far, something failed, redisplay form
-            model = _categoryViewModelService.PrepareCategoryModel(model, null);
+            model = await _categoryViewModelService.PrepareCategoryModel(model, null);
             //ACL
-            model.PrepareACLModel(null, true, _customerService);
+            await model.PrepareACLModel(null, true, _customerService);
             //Stores
-            model.PrepareStoresMappingModel(null, true, _storeService);
+            await model.PrepareStoresMappingModel(null, true, _storeService);
 
             return View(model);
         }
 
-        public IActionResult Edit(string id)
+        public async Task<IActionResult> Edit(string id)
         {
-            var category = _categoryService.GetCategoryById(id);
+            var category = await _categoryService.GetCategoryById(id);
             if (category == null)
                 //No category found with the specified id
                 return RedirectToAction("List");
 
             var model = category.ToModel();
             //locales
-            AddLocales(_languageService, model.Locales, (locale, languageId) =>
+            await AddLocales(_languageService, model.Locales, (locale, languageId) =>
             {
                 locale.Name = category.GetLocalized(x => x.Name, languageId, false, false);
                 locale.Description = category.GetLocalized(x => x.Description, languageId, false, false);
@@ -146,26 +147,26 @@ namespace Grand.Web.Areas.Admin.Controllers
                 locale.MetaTitle = category.GetLocalized(x => x.MetaTitle, languageId, false, false);
                 locale.SeName = category.GetSeName(languageId, false, false);
             });
-            model = _categoryViewModelService.PrepareCategoryModel(model, category);
+            model = await _categoryViewModelService.PrepareCategoryModel(model, category);
             //ACL
-            model.PrepareACLModel(category, false, _customerService);
+            await model.PrepareACLModel(category, false, _customerService);
             //Stores
-            model.PrepareStoresMappingModel(category, false, _storeService);
+            await model.PrepareStoresMappingModel(category, false, _storeService);
 
             return View(model);
         }
 
         [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
-        public IActionResult Edit(CategoryModel model, bool continueEditing)
+        public async Task<IActionResult> Edit(CategoryModel model, bool continueEditing)
         {
-            var category = _categoryService.GetCategoryById(model.Id);
+            var category = await _categoryService.GetCategoryById(model.Id);
             if (category == null)
                 //No category found with the specified id
                 return RedirectToAction("List");
 
             if (ModelState.IsValid)
             {
-                category = _categoryViewModelService.UpdateCategoryModel(category, model);
+                category = await _categoryViewModelService.UpdateCategoryModel(category, model);
 
                 SuccessNotification(_localizationService.GetResource("Admin.Catalog.Categories.Updated"));
                 if (continueEditing)
@@ -179,26 +180,26 @@ namespace Grand.Web.Areas.Admin.Controllers
             }
 
             //If we got this far, something failed, redisplay form
-            model = _categoryViewModelService.PrepareCategoryModel(model, category);
+            model = await _categoryViewModelService.PrepareCategoryModel(model, category);
             //ACL
-            model.PrepareACLModel(category, true, _customerService);
+            await model.PrepareACLModel(category, true, _customerService);
             //Stores
-            model.PrepareStoresMappingModel(category, true, _storeService);
+            await model.PrepareStoresMappingModel(category, true, _storeService);
 
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult Delete(string id)
+        public async Task<IActionResult> Delete(string id)
         {
-            var category = _categoryService.GetCategoryById(id);
+            var category = await _categoryService.GetCategoryById(id);
             if (category == null)
                 //No category found with the specified id
                 return RedirectToAction("List");
 
             if (ModelState.IsValid)
             {
-                _categoryViewModelService.DeleteCategory(category);
+                await _categoryViewModelService.DeleteCategory(category);
                 SuccessNotification(_localizationService.GetResource("Admin.Catalog.Categories.Deleted"));
             }
             return RedirectToAction("List");
@@ -209,11 +210,11 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         #region Export / Import
 
-        public IActionResult ExportXml()
+        public async Task<IActionResult> ExportXml()
         {
             try
             {
-                var xml = _exportManager.ExportCategoriesToXml();
+                var xml = await _exportManager.ExportCategoriesToXml();
                 return File(Encoding.UTF8.GetBytes(xml), "application/xml", "categories.xml");
             }
             catch (Exception exc)
@@ -223,11 +224,11 @@ namespace Grand.Web.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult ExportXlsx()
+        public async Task<IActionResult> ExportXlsx()
         {
             try
             {
-                var bytes = _exportManager.ExportCategoriesToXlsx(_categoryService.GetAllCategories(showHidden: true));
+                var bytes = _exportManager.ExportCategoriesToXlsx(await _categoryService.GetAllCategories(showHidden: true));
 
                 return File(bytes, "text/xls", "categories.xlsx");
             }
@@ -239,7 +240,7 @@ namespace Grand.Web.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult ImportFromXlsx(IFormFile importexcelfile)
+        public async Task<IActionResult> ImportFromXlsx(IFormFile importexcelfile)
         {
             //a vendor cannot import categories
             if (_workContext.CurrentVendor != null)
@@ -249,7 +250,7 @@ namespace Grand.Web.Areas.Admin.Controllers
             {
                 if (importexcelfile != null && importexcelfile.Length > 0)
                 {
-                    _importManager.ImportCategoryFromXlsx(importexcelfile.OpenReadStream());
+                    await _importManager.ImportCategoryFromXlsx(importexcelfile.OpenReadStream());
                 }
                 else
                 {
@@ -270,9 +271,9 @@ namespace Grand.Web.Areas.Admin.Controllers
         #region Products
 
         [HttpPost]
-        public IActionResult ProductList(DataSourceRequest command, string categoryId)
+        public async Task<IActionResult> ProductList(DataSourceRequest command, string categoryId)
         {
-            var productCategories = _categoryViewModelService.PrepareCategoryProductModel(categoryId, command.Page, command.PageSize);
+            var productCategories = await _categoryViewModelService.PrepareCategoryProductModel(categoryId, command.Page, command.PageSize);
             var gridModel = new DataSourceResult
             {
                 Data = productCategories.categoryProductModels,
@@ -282,37 +283,37 @@ namespace Grand.Web.Areas.Admin.Controllers
             return Json(gridModel);
         }
 
-        public IActionResult ProductUpdate(CategoryModel.CategoryProductModel model)
+        public async Task<IActionResult> ProductUpdate(CategoryModel.CategoryProductModel model)
         {
             if (ModelState.IsValid)
             {
-                _categoryViewModelService.UpdateProductCategoryModel(model);
+                await _categoryViewModelService.UpdateProductCategoryModel(model);
                 return new NullJsonResult();
             }
             return ErrorForKendoGridJson(ModelState);
         }
 
-        public IActionResult ProductDelete(string id, string productId)
+        public async Task<IActionResult> ProductDelete(string id, string productId)
         {
             if (ModelState.IsValid)
             {
-                _categoryViewModelService.DeleteProductCategoryModel(id, productId);
+                await _categoryViewModelService.DeleteProductCategoryModel(id, productId);
                 return new NullJsonResult();
             }
             return ErrorForKendoGridJson(ModelState);
         }
 
-        public IActionResult ProductAddPopup(string categoryId)
+        public async Task<IActionResult> ProductAddPopup(string categoryId)
         {
-            var model = _categoryViewModelService.PrepareAddCategoryProductModel();
+            var model = await _categoryViewModelService.PrepareAddCategoryProductModel();
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult ProductAddPopupList(DataSourceRequest command, CategoryModel.AddCategoryProductModel model)
+        public async Task<IActionResult> ProductAddPopupList(DataSourceRequest command, CategoryModel.AddCategoryProductModel model)
         {
             var gridModel = new DataSourceResult();
-            var products = _categoryViewModelService.PrepareProductModel(model, command.Page, command.PageSize);
+            var products = await _categoryViewModelService.PrepareProductModel(model, command.Page, command.PageSize);
             gridModel.Data = products.products.ToList();
             gridModel.Total = products.totalCount;
 
@@ -321,11 +322,11 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         [HttpPost]
         [FormValueRequired("save")]
-        public IActionResult ProductAddPopup(CategoryModel.AddCategoryProductModel model)
+        public async Task<IActionResult> ProductAddPopup(CategoryModel.AddCategoryProductModel model)
         {
             if (model.SelectedProductIds != null)
             {
-                _categoryViewModelService.InsertCategoryProductModel(model);
+                await _categoryViewModelService.InsertCategoryProductModel(model);
             }
             ViewBag.RefreshPage = true;
             return View(model);
@@ -336,9 +337,9 @@ namespace Grand.Web.Areas.Admin.Controllers
         #region Activity log
 
         [HttpPost]
-        public IActionResult ListActivityLog(DataSourceRequest command, string categoryId)
+        public async Task<IActionResult> ListActivityLog(DataSourceRequest command, string categoryId)
         {
-            var activityLog = _categoryViewModelService.PrepareActivityLogModel(categoryId, command.Page, command.PageSize);
+            var activityLog = await _categoryViewModelService.PrepareActivityLogModel(categoryId, command.Page, command.PageSize);
             var gridModel = new DataSourceResult
             {
                 Data = activityLog.activityLogModel,
