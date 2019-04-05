b151d3aa2615cf8e398560f89f05295d79da4ee1
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 15:30:35 +0200

    Extend the usage of async methods #438 - admin customer action type controller

--- Grand.Web/Areas/Admin/Controllers/CustomerActionTypeController.cs
+++ Grand.Web/Areas/Admin/Controllers/CustomerActionTypeController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Grand.Web.Areas.Admin.Controllers
 {
@@ -34,25 +35,25 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         #region Action types
 
-        public IActionResult ListTypes()
+        public async Task<IActionResult> ListTypes()
         {
-            var model = _customerActionService.GetCustomerActionType()
+            var model = (await _customerActionService.GetCustomerActionType())
                 .Select(x => x.ToModel())
                 .ToList();
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult SaveTypes(IFormCollection form)
+        public async Task<IActionResult> SaveTypes(IFormCollection form)
         {
             string formKey = "checkbox_action_types";
             var checkedActionTypes = form[formKey].ToString() != null ? form[formKey].ToString().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x).ToList() : new List<string>();
 
-            var activityTypes = _customerActionService.GetCustomerActionType();
+            var activityTypes = await _customerActionService.GetCustomerActionType();
             foreach (var actionType in activityTypes)
             {
                 actionType.Enabled = checkedActionTypes.Contains(actionType.Id);
-                _customerActionService.UpdateCustomerActionType(actionType);
+                await _customerActionService.UpdateCustomerActionType(actionType);
             }
             SuccessNotification(_localizationService.GetResource("Admin.Customers.ActionType.Updated"));
             return RedirectToAction("ListTypes");
