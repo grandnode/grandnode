47181f63fe8e8243ad6b27199469475dbb8d80dd
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 15:18:51 +0200

    Extend the usage of async methods #438 - customer api service

--- Grand.Api/Interfaces/ICustomerApiService.cs
+++ Grand.Api/Interfaces/ICustomerApiService.cs
@@ -1,19 +1,20 @@
 ﻿using Grand.Api.DTOs.Customers;
 using MongoDB.Driver.Linq;
+using System.Threading.Tasks;
 
 namespace Grand.Api.Interfaces
 {
     public interface ICustomerApiService
     {
-        CustomerDto GetByEmail(string email);
-        CustomerDto InsertOrUpdateCustomer(CustomerDto model);
-        CustomerDto InsertCustomer(CustomerDto model);
-        CustomerDto UpdateCustomer(CustomerDto model);
-        void DeleteCustomer(CustomerDto model);
-        AddressDto InsertAddress(CustomerDto customer, AddressDto model);
-        AddressDto UpdateAddress(CustomerDto customer, AddressDto model);
-        void DeleteAddress(CustomerDto customer, AddressDto model);
-        VendorDto GetVendorById(string id);
+        Task<CustomerDto> GetByEmail(string email);
+        Task<CustomerDto> InsertOrUpdateCustomer(CustomerDto model);
+        Task<CustomerDto> InsertCustomer(CustomerDto model);
+        Task<CustomerDto> UpdateCustomer(CustomerDto model);
+        Task DeleteCustomer(CustomerDto model);
+        Task<AddressDto> InsertAddress(CustomerDto customer, AddressDto model);
+        Task<AddressDto> UpdateAddress(CustomerDto customer, AddressDto model);
+        Task DeleteAddress(CustomerDto customer, AddressDto model);
+        Task<VendorDto> GetVendorById(string id);
         IMongoQueryable<VendorDto> GetVendors();
     }
 }
