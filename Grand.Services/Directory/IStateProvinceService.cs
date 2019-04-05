bf5d034cf91544d8d4fe87e0e167cfa77357bda0
KrzysztofPajak <krzysiek@nop4you.com>
2019-04-05 14:51:06 +0200

    Extend the usage of async methods #438 - state province service

--- Grand.Services/Directory/IStateProvinceService.cs
+++ Grand.Services/Directory/IStateProvinceService.cs
@@ -1,5 +1,6 @@
 using Grand.Core.Domain.Directory;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Grand.Services.Directory
 {
@@ -12,48 +13,41 @@ namespace Grand.Services.Directory
         /// Deletes a state/province
         /// </summary>
         /// <param name="stateProvince">The state/province</param>
-        void DeleteStateProvince(StateProvince stateProvince);
+        Task DeleteStateProvince(StateProvince stateProvince);
 
         /// <summary>
         /// Gets a state/province
         /// </summary>
         /// <param name="stateProvinceId">The state/province identifier</param>
         /// <returns>State/province</returns>
-        StateProvince GetStateProvinceById(string stateProvinceId);
+        Task<StateProvince> GetStateProvinceById(string stateProvinceId);
 
-        /// <summary>
-        /// Gets a state/province 
-        /// </summary>
-        /// <param name="abbreviation">The state/province abbreviation</param>
-        /// <returns>State/province</returns>
-        StateProvince GetStateProvinceByAbbreviation(string abbreviation);
-
-        /// <summary>
+               /// <summary>
         /// Gets a state/province collection by country identifier
         /// </summary>
         /// <param name="countryId">Country identifier</param>
         /// <param name="languageId">Language identifier. It's used to sort states by localized names (if specified); pass 0 to skip it</param>
         /// <param name="showHidden">A value indicating whether to show hidden records</param>
         /// <returns>States</returns>
-        IList<StateProvince> GetStateProvincesByCountryId(string countryId, string languageId = "", bool showHidden = false);
+        Task<IList<StateProvince>> GetStateProvincesByCountryId(string countryId, string languageId = "", bool showHidden = false);
 
         /// <summary>
         /// Gets all states/provinces
         /// </summary>
         /// <param name="showHidden">A value indicating whether to show hidden records</param>
         /// <returns>State/province collection</returns>
-        IList<StateProvince> GetStateProvinces(bool showHidden = false);
+        Task<IList<StateProvince>> GetStateProvinces(bool showHidden = false);
 
         /// <summary>
         /// Inserts a state/province
         /// </summary>
         /// <param name="stateProvince">State/province</param>
-        void InsertStateProvince(StateProvince stateProvince);
+        Task InsertStateProvince(StateProvince stateProvince);
 
         /// <summary>
         /// Updates a state/province
         /// </summary>
         /// <param name="stateProvince">State/province</param>
-        void UpdateStateProvince(StateProvince stateProvince);
+        Task UpdateStateProvince(StateProvince stateProvince);
     }
 }
