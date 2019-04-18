53bbc5340f1f54d86f39c74fce6435a94be77f33
benys <kserwus@gmail.com>
2019-04-18 21:56:28 +0200

    Added NBP exchange provider

--- Plugins/Grand.Plugin.ExchangeRate.McExchange/McExchangeRateProvider.cs
+++ Plugins/Grand.Plugin.ExchangeRate.McExchange/McExchangeRateProvider.cs
@@ -1,25 +1,27 @@
-﻿using Grand.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grand.Core;
 using Grand.Core.Plugins;
 using Grand.Services.Directory;
 using Grand.Services.Localization;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Net;
-using System.Threading.Tasks;
-using System.Xml;
 
-namespace Grand.Plugin.ExchangeRate.EcbExchange
+namespace Grand.Plugin.ExchangeRate.McExchange
 {
-    public class EcbExchangeRateProvider : BasePlugin, IExchangeRateProvider
+    public class McExchangeRateProvider : BasePlugin, IExchangeRateProvider
     {
         private readonly ILocalizationService _localizationService;
         private readonly IServiceProvider _serviceProvider;
 
-        public EcbExchangeRateProvider(ILocalizationService localizationService, IServiceProvider serviceProvider)
+        private readonly IDictionary<string, IRateProvider> _providers = new Dictionary<string, IRateProvider> {
+            {"eur", new EcbExchange()},
+            {"pln", new NbpExchange()},
+        };
+
+        public McExchangeRateProvider(ILocalizationService localizationService, IServiceProvider serviceProvider)
         {
-            this._localizationService = localizationService;
-            this._serviceProvider = serviceProvider;
+            _localizationService = localizationService;
+            _serviceProvider = serviceProvider;
         }
 
         /// <summary>
@@ -27,42 +29,19 @@ namespace Grand.Plugin.ExchangeRate.EcbExchange
         /// </summary>
         /// <param name="exchangeRateCurrencyCode">Exchange rate currency code</param>
         /// <returns>Exchange rates</returns>
-        public async Task<IList<Core.Domain.Directory.ExchangeRate>> GetCurrencyLiveRates(string exchangeRateCurrencyCode)
+        public Task<IList<Core.Domain.Directory.ExchangeRate>> GetCurrencyLiveRates(string exchangeRateCurrencyCode)
         {
-            if (String.IsNullOrEmpty(exchangeRateCurrencyCode) ||
-                exchangeRateCurrencyCode.ToLower() != "eur")
-                throw new GrandException(_localizationService.GetResource("Plugins.ExchangeRate.EcbExchange.SetCurrencyToEURO"));
+            if(string.IsNullOrEmpty(exchangeRateCurrencyCode))
+                throw new ArgumentNullException(nameof(exchangeRateCurrencyCode));
 
-            var exchangeRates = new List<Grand.Core.Domain.Directory.ExchangeRate>();
+            exchangeRateCurrencyCode = exchangeRateCurrencyCode.ToLowerInvariant();
 
-            var request = WebRequest.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml") as HttpWebRequest;
-            using (WebResponse response = request.GetResponse())
+            if (_providers.TryGetValue(exchangeRateCurrencyCode, out var provider))
             {
-                var document = new XmlDocument();
-                document.Load(response.GetResponseStream());
-                var nsmgr = new XmlNamespaceManager(document.NameTable);
-                nsmgr.AddNamespace("ns", "http://www.ecb.int/vocabulary/2002-08-01/eurofxref");
-                nsmgr.AddNamespace("gesmes", "http://www.gesmes.org/xml/2002-08-01");
-
-                var node = document.SelectSingleNode("gesmes:Envelope/ns:Cube/ns:Cube", nsmgr);
-                var updateDate = DateTime.ParseExact(node.Attributes["time"].Value, "yyyy-MM-dd", null);
-
-                var provider = new NumberFormatInfo();
-                provider.NumberDecimalSeparator = ".";
-                provider.NumberGroupSeparator = "";
-                foreach (XmlNode node2 in node.ChildNodes)
-                {
-                    exchangeRates.Add(new Core.Domain.Directory.ExchangeRate()
-                    {
-                        CurrencyCode = node2.Attributes["currency"].Value,
-                        Rate = decimal.Parse(node2.Attributes["rate"].Value, provider),
-                        UpdatedOn = updateDate
-                    }
-                    );
-                }
+                return provider.GetCurrencyLiveRates();
             }
 
-            return await Task.FromResult(exchangeRates);
+            throw new GrandException(_localizationService.GetResource("Plugins.ExchangeRate.EcbExchange.SetCurrencyToEURO"));
         }
 
         public override async Task Install()
