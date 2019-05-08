34929f846aa4ff633c6a909c7c847b8e26df16e1
KrzysztofPajak <krzysiek@nop4you.com>
2019-05-08 20:58:30 +0200

    Move html extensions DatePickerDropDowns to the tag helper with View

---
+++ Grand.Framework/Mvc/Models/DatePickerDropDownsModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Grand.Framework.Mvc.Models
+{
+    public partial class DatePickerDropDownsModel : BaseGrandEntityModel
+    {
+        public DatePickerDropDownsModel()
+        {
+            SelectListDay = new List<SelectListItem>();
+            SelectListMonth = new List<SelectListItem>();
+            SelectListYear = new List<SelectListItem>();
+        }
+        public string Attribute { get; set; }
+
+        public string Day { get; set; }
+
+        public IList<SelectListItem> SelectListDay { get; set; }
+
+        public string Month { get; set; }
+        public IList<SelectListItem> SelectListMonth { get; set; }
+
+        public string Year { get; set; }
+        public IList<SelectListItem> SelectListYear { get; set; }
+
+        public int Begin_Year { get; set; }
+
+        public int End_Year { get; set; }
+
+        public int SelectedDay { get; set; }
+
+        public int SelectedMonth { get; set; }
+
+        public int SelectedYear { get; set; }
+    }
+}
