e7079e4c5fbf873c44ac729b3b5c37cd2832b127
KrzysztofPajak <krzysiek@nop4you.com>
2019-05-23 19:08:35 +0200

    Register BrainTree Payment plugin in DI

---
+++ Plugins/Grand.Plugin.Payments.BrainTree/DependencyRegistrar.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Grand.Core.Configuration;
+using Grand.Core.Infrastructure;
+using Grand.Core.Infrastructure.DependencyManagement;
+
+namespace Grand.Plugin.Payments.BrainTree
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, GrandConfig config)
+        {
+            builder.RegisterType<BrainTreePaymentProcessor>().InstancePerLifetimeScope();
+        }
+
+        public int Order {
+            get { return 10; }
+        }
+    }
+
+}
