a9c750a44ce9a013a68ef31eb6eab14d5732391d
KrzysztofPajak <krzysiek@nop4you.com>
2019-07-30 13:02:46 +0200

    Move push notifications routing to separate file

---
+++ Grand.Web/Areas/Admin/Infrastructure/RouteProvider.cs
@@ -0,0 +1,52 @@
+﻿using Grand.Framework.Mvc.Routing;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Grand.Web.Areas.Admin.Infrastructure
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            //push notifications
+            routeBuilder.MapRoute(
+               "PushNotifications.Send",
+               "Admin/PushNotifications/Send",
+            new { controller = "PushNotifications", action = "Send" });
+
+            routeBuilder.MapRoute(
+                "PushNotifications.Messages",
+                "Admin/PushNotifications/Messages",
+            new { controller = "PushNotifications", action = "Messages" });
+
+            routeBuilder.MapRoute(
+               "PushNotifications.Receivers",
+               "Admin/PushNotifications/Receivers",
+            new { controller = "PushNotifications", action = "Receivers" });
+
+            routeBuilder.MapRoute(
+                "PushNotifications.DeleteReceiver",
+                "Admin/PushNotifications/DeleteReceiver",
+                new { controller = "PushNotifications", action = "DeleteReceiver" });
+
+            routeBuilder.MapRoute(
+                "PushNotifications.Configure",
+                "Admin/PushNotifications/Configure",
+                new { controller = "PushNotifications", action = "Configure" });
+
+            routeBuilder.MapRoute(
+                "PushNotifications.PushMessagesList",
+                "Admin/PushNotifications/PushMessagesList",
+            new { controller = "PushNotifications", action = "PushMessagesList" });
+
+            routeBuilder.MapRoute(
+                "PushNotifications.PushReceiversList",
+                "Admin/PushNotifications/PushReceiversList",
+            new { controller = "PushNotifications", action = "PushReceiversList" });
+
+        }
+        public int Priority => 0;
+
+
+    }
+}
--- Grand.Web/Infrastructure/RouteProvider.cs
+++ Grand.Web/Infrastructure/RouteProvider.cs
@@ -659,41 +659,7 @@ namespace Grand.Web.Infrastructure
             routeBuilder.MapLocalizedRoute("PageNotFound", "page-not-found",
                             new { controller = "Common", action = "PageNotFound" });
 
-            //push notifications
-            routeBuilder.MapRoute(
-               "PushNotifications.Send",
-               "Admin/PushNotifications/Send",
-            new { controller = "PushNotifications", action = "Send" });
-
-            routeBuilder.MapRoute(
-                "PushNotifications.Messages",
-                "Admin/PushNotifications/Messages",
-            new { controller = "PushNotifications", action = "Messages" });
-
-            routeBuilder.MapRoute(
-               "PushNotifications.Receivers",
-               "Admin/PushNotifications/Receivers",
-            new { controller = "PushNotifications", action = "Receivers" });
-
-            routeBuilder.MapRoute(
-                "PushNotifications.DeleteReceiver",
-                "Admin/PushNotifications/DeleteReceiver",
-                new { controller = "PushNotifications", action = "DeleteReceiver" });
-
-            routeBuilder.MapRoute(
-                "PushNotifications.Configure",
-                "Admin/PushNotifications/Configure",
-                new { controller = "PushNotifications", action = "Configure" });
-
-            routeBuilder.MapRoute(
-                "PushNotifications.PushMessagesList",
-                "Admin/PushNotifications/PushMessagesList",
-            new { controller = "PushNotifications", action = "PushMessagesList" });
-
-            routeBuilder.MapRoute(
-                "PushNotifications.PushReceiversList",
-                "Admin/PushNotifications/PushReceiversList",
-            new { controller = "PushNotifications", action = "PushReceiversList" });
+            
 
             //lets encrypt
             routeBuilder.MapRoute("well-known", ".well-known/acme-challenge/{fileName}", 
