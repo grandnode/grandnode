93a108d9382937f5b894f3a6cf0f9b65f52c8fa3
KrzysztofPajak <krzysiek@nop4you.com>
2019-08-02 14:50:15 +0200

    Plugin WidgetSlider - there is no need to have 2 files with dependency register

--- Plugins/Grand.Plugin.Widgets.Slider/DependencyRegistrar.cs
+++ Plugins/Grand.Plugin.Widgets.Slider/DependencyRegistrar.cs
@@ -2,6 +2,9 @@
 using Grand.Core.Configuration;
 using Grand.Core.Infrastructure;
 using Grand.Core.Infrastructure.DependencyManagement;
+using Grand.Plugin.Widgets.Slider.Domain;
+using Grand.Plugin.Widgets.Slider.Services;
+using MongoDB.Bson.Serialization;
 
 namespace Grand.Plugin.Widgets.Slider
 {
@@ -10,6 +13,12 @@ namespace Grand.Plugin.Widgets.Slider
         public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, GrandConfig config)
         {
             builder.RegisterType<SliderPlugin>().InstancePerLifetimeScope();
+            BsonClassMap.RegisterClassMap<PictureSlider>(cm =>
+            {
+                cm.AutoMap();
+                cm.UnmapMember(c => c.SliderType);
+            });
+            builder.RegisterType<SliderService>().As<ISliderService>().InstancePerLifetimeScope();
         }
 
         public int Order
