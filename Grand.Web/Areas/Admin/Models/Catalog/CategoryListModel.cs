da2aea7c4a7ee93c2a3f51a787af3ac96694553b
KrzysztofPajak <krzysiek@nop4you.com>
2019-08-12 13:54:00 +0200

    Remove unused using

--- Grand.Web/Areas/Admin/Models/Catalog/CategoryListModel.cs
+++ Grand.Web/Areas/Admin/Models/Catalog/CategoryListModel.cs
@@ -1,6 +1,4 @@
-﻿using Grand.Framework;
-using Grand.Framework.Mvc;
-using Grand.Framework.Mvc.ModelBinding;
+﻿using Grand.Framework.Mvc.ModelBinding;
 using Grand.Framework.Mvc.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
@@ -15,7 +13,7 @@ namespace Grand.Web.Areas.Admin.Models.Catalog
         }
 
         [GrandResourceDisplayName("Admin.Catalog.Categories.List.SearchCategoryName")]
-        
+
         public string SearchCategoryName { get; set; }
 
         [GrandResourceDisplayName("Admin.Catalog.Categories.List.SearchStore")]
