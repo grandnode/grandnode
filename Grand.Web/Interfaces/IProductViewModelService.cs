4105575f5f175f4d8733121e11b5648701bc5e55
KrzysztofPajak <krzysiek@nop4you.com>
2019-12-09 20:57:55 +0100

    Refactoring ProductViewModelService

--- Grand.Web/Interfaces/IProductViewModelService.cs
+++ Grand.Web/Interfaces/IProductViewModelService.cs
@@ -1,6 +1,7 @@
 ﻿using Grand.Core.Domain.Catalog;
 using Grand.Core.Domain.Orders;
 using Grand.Web.Models.Catalog;
+using Grand.Web.Models.Media;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -21,6 +22,18 @@ namespace Grand.Web.Interfaces
         Task<string> PrepareProductTemplateViewPath(string productTemplateId);
         Task<ProductDetailsModel> PrepareProductDetailsPage(Product product,
             ShoppingCartItem updatecartitem = null, bool isAssociatedProduct = false);
+
+        Task<VendorBriefInfoModel> PrepareVendorBriefInfoModel(Product product);
+        Task<ProductDetailsModel.ProductBreadcrumbModel> PrepareProductBreadcrumbModel(Product product);
+        Task<List<ProductTagModel>> PrepareProductTagModel(Product product);
+        Task<(PictureModel defaultPictureModel, List<PictureModel> pictureModels)> PrepareProductPictureModel(Product product, int defaultPictureSize, bool isAssociatedProduct, string name);
+        Task<ProductDetailsModel.ProductPriceModel> PrepareProductPriceModel(Product product);
+        Task<ProductDetailsModel.AddToCartModel> PrepareAddToCartModel(Product product, ShoppingCartItem updatecartitem = null);
+        ProductDetailsModel.GiftCardModel PrepareGiftCardModel(Product product, ShoppingCartItem updatecartitem = null);
+        Task<IList<ProductDetailsModel.ProductAttributeModel>> PrepareProductAttributeModel(Product product, int defaultPictureSize, ShoppingCartItem updatecartitem = null);
+        Task<IList<ProductDetailsModel.TierPriceModel>> PrepareProductTierPriceModel(Product product);
+        Task<IList<ProductDetailsModel.ProductBundleModel>> PrepareProductBundleModel(Product product);
+        Task PrepareProductReservation(ProductDetailsModel model, Product product);
         Task PrepareProductReviewsModel(ProductReviewsModel model, Product product, int size = 0);
         Task<ProductReview> InsertProductReview(Product product, ProductReviewsModel model);
 
