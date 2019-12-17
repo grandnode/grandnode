9c758b758c29a95884c109fe2873c43d8def12db
Leonardo Ferreira <index.xp@gmail.com>
2019-12-17 19:04:12 -0300

    ReImplemented T Get<T>(cacheManager, key, acquire)

--- Grand.Core/Caching/CacheExtensions.cs
+++ Grand.Core/Caching/CacheExtensions.cs
@@ -46,5 +46,41 @@ namespace Grand.Core.Caching
                 await cacheManager.SetAsync(key, result, cacheTime);
             return result;
         }
+
+        /// <summary>
+        /// Get a cached item. If it's not in the cache yet, then load and cache it
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="cacheManager">Cache manager</param>
+        /// <param name="key">Cache key</param>
+        /// <param name="cacheTime">Cache time in minutes (0 - do not cache)</param>
+        /// <param name="acquire">Function to load item if it's not in the cache yet</param>
+        /// <returns>Cached item</returns>
+        public static T Get<T>(this ICacheManager cacheManager, string key, int cacheTime, Func<T> acquire)
+        {
+            var value = cacheManager.TryGetValue<T>(key);
+            if (value.FromCache == true)
+                return value.Result;
+
+            var result = acquire();
+            if (cacheTime > 0)
+                cacheManager.Set(key, result, cacheTime);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get a cached item. If it's not in the cache yet, then load and cache it
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="cacheManager">Cache manager</param>
+        /// <param name="key">Cache key</param>
+        /// <param name="cacheTime">Cache time in minutes (0 - do not cache)</param>
+        /// <param name="acquire">Function to load item if it's not in the cache yet</param>
+        /// <returns>Cached item</returns>
+        public static T Get<T>(this ICacheManager cacheManager, string key, Func<T> acquire)
+        {
+            return Get(cacheManager, key, DefaultCacheTimeMinutes, acquire);
+        }
     }
 }
