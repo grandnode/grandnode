fc7fd5c3194826a6ae3dbc4f12fc3132953ebf27
Daniel Żuławski <sirdan269@gmail.com>
2019-12-20 15:14:02 +0100

    Revert "Slider - changed widget zone and fixed some issues"
    This reverts commit 948bf0d6e5fd7fe157c59258c64a39b06ce13f9e.

--- Plugins/Grand.Plugin.Widgets.Slider/SliderDefaults.cs
+++ Plugins/Grand.Plugin.Widgets.Slider/SliderDefaults.cs
@@ -3,7 +3,7 @@ namespace Grand.Plugin.Widgets.Slider
 {
     public static class SliderDefaults
     {
-        public static string WidgetZoneHomePage => "content_before";
+        public static string WidgetZoneHomePage => "home_page_top";
         public static string WidgetZoneCategoryPage => "home_page_category";
         public static string WidgetZoneManufacturerPage => "home_page_manufacturer";
 
