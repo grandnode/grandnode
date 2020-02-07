d1f8a4895c8bc9b628fff3f864d8616460c469da
KrzysztofPajak <krzysiek@nop4you.com>
2020-02-07 10:09:12 +0100

    Typo fix

--- Grand.Web/Infrastructure/Cache/ModelCacheEventConsumer.cs
+++ Grand.Web/Infrastructure/Cache/ModelCacheEventConsumer.cs
@@ -485,8 +485,8 @@ namespace Grand.Web.Infrastructure.Cache
         /// {0} : current product id
         /// {1} : current store ID
         /// </remarks>
-        public const string PRODUCTS_ALSO_PURCHASED_IDS_KEY = "Grand.pres.alsopuchased-{0}-{1}";
-        public const string PRODUCTS_ALSO_PURCHASED_IDS_PATTERN_KEY = "Grand.pres.alsopuchased";
+        public const string PRODUCTS_ALSO_PURCHASED_IDS_KEY = "Grand.pres.alsopurchased-{0}-{1}";
+        public const string PRODUCTS_ALSO_PURCHASED_IDS_PATTERN_KEY = "Grand.pres.alsopurchased";
 
         /// <summary>
         /// Key for "related" product identifiers displayed on the product details page
