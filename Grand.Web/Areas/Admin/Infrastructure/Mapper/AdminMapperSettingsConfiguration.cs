ec1bf37620747b1743d6fd1db7dba42363644331
TTomu <wozniaktomasz@gmail.com>
2020-03-02 12:17:48 +0100

    Minor changes - refactoring AdminMapper - add ShippingSettingsProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperSettingsConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperSettingsConfiguration.cs
@@ -20,27 +20,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
         public AdminMapperSettingsConfiguration()
         {
 
-            CreateMap<ShippingSettings, ShippingSettingsModel>()
-                .ForMember(dest => dest.ShippingOriginAddress, mo => mo.Ignore())
-                .ForMember(dest => dest.ActiveStoreScopeConfiguration, mo => mo.Ignore())
-                .ForMember(dest => dest.AllowPickUpInStore_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.ShipToSameAddress_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.UseWarehouseLocation_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.NotifyCustomerAboutShippingFromMultipleLocations_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.FreeShippingOverXEnabled_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.FreeShippingOverXValue_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.FreeShippingOverXIncludingTax_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.EstimateShippingEnabled_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.DisplayShipmentEventsToCustomers_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.DisplayShipmentEventsToStoreOwner_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.BypassShippingMethodSelectionIfOnlyOne_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.ShippingOriginAddress_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.GenericAttributes, mo => mo.Ignore());
-            CreateMap<ShippingSettingsModel, ShippingSettings>()
-                .ForMember(dest => dest.ActiveShippingRateComputationMethodSystemNames, mo => mo.Ignore())
-                .ForMember(dest => dest.ReturnValidOptionsIfThereAreAny, mo => mo.Ignore())
-                .ForMember(dest => dest.UseCubeRootMethod, mo => mo.Ignore());
-
             CreateMap<CatalogSettings, CatalogSettingsModel>()
                 .ForMember(dest => dest.ActiveStoreScopeConfiguration, mo => mo.Ignore())
                 .ForMember(dest => dest.AllowViewUnpublishedProductPage_OverrideForStore, mo => mo.Ignore())
