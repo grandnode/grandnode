27a2d07e5f39887ee9f53675277de925643057a7
TTomu <wozniaktomasz@gmail.com>
2020-03-02 09:29:43 +0100

    Minor changes - refactoring AdminMapper - add InteractiveFormProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -66,31 +66,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
         public AdminMapperModelConfiguration()
         {
             
-            //InteractiveForm
-            CreateMap<InteractiveForm, InteractiveFormModel>()
-                .ForMember(dest => dest.Locales, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableEmailAccounts, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableTokens, mo => mo.Ignore());
-
-            CreateMap<InteractiveFormModel, InteractiveForm>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.FormAttributes, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.MapFrom(x => x.Locales.ToLocalizedProperty()));
-
-            CreateMap<InteractiveForm.FormAttribute, InteractiveFormAttributeModel>()
-                .ForMember(dest => dest.Locales, mo => mo.Ignore());
-
-            CreateMap<InteractiveFormAttributeModel, InteractiveForm.FormAttribute>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.MapFrom(x => x.Locales.ToLocalizedProperty()));
-
-            CreateMap<InteractiveForm.FormAttributeValue, InteractiveFormAttributeValueModel>()
-                .ForMember(dest => dest.Locales, mo => mo.Ignore());
-
-            CreateMap<InteractiveFormAttributeValueModel, InteractiveForm.FormAttributeValue>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.MapFrom(x => x.Locales.ToLocalizedProperty()));
-
             //campaign
             CreateMap<Campaign, CampaignModel>()
                 .ForMember(dest => dest.CreatedOn, mo => mo.Ignore())
