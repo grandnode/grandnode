d625ebe336036012229bf9f7e6ca7366d87bac88
TTomu <wozniaktomasz@gmail.com>
2020-03-02 09:34:50 +0100

    Minor changes - refactoring AdminMapper - add CategoryProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -66,31 +66,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
         public AdminMapperModelConfiguration()
         {
             
-            //category
-            CreateMap<Category, CategoryModel>()
-                .ForMember(dest => dest.Locales, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableCategoryTemplates, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.Ignore())
-                .ForMember(dest => dest.Breadcrumb, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableCategories, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableDiscounts, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableSortOptions, mo => mo.Ignore())
-                .ForMember(dest => dest.SelectedDiscountIds, mo => mo.Ignore())
-                .ForMember(dest => dest.SeName, mo => mo.MapFrom(src => src.GetSeName("", true, false)))
-                .ForMember(dest => dest.AvailableCustomerRoles, mo => mo.Ignore())
-                .ForMember(dest => dest.SelectedCustomerRoleIds, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableStores, mo => mo.Ignore())
-                .ForMember(dest => dest.SelectedStoreIds, mo => mo.Ignore());
-
-            CreateMap<CategoryModel, Category>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.Ignore())
-                .ForMember(dest => dest.CreatedOnUtc, mo => mo.Ignore())
-                .ForMember(dest => dest.UpdatedOnUtc, mo => mo.Ignore())
-                .ForMember(dest => dest.Stores, mo => mo.MapFrom(x => x.SelectedStoreIds != null ? x.SelectedStoreIds.ToList() : new List<string>()))
-                .ForMember(dest => dest.CustomerRoles, mo => mo.MapFrom(x => x.SelectedCustomerRoleIds != null ? x.SelectedCustomerRoleIds.ToList() : new List<string>()))
-                .ForMember(dest => dest.AppliedDiscounts, mo => mo.Ignore());
-
             //manufacturer
             CreateMap<Manufacturer, ManufacturerModel>()
                 .ForMember(dest => dest.Locales, mo => mo.Ignore())
