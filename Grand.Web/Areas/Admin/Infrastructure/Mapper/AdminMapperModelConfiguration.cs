5c71ff72c278d5ff6f8f552c47c28c0e59d90d06
TTomu <wozniaktomasz@gmail.com>
2020-03-02 11:28:20 +0100

    Minor changes - refactoring AdminMapper - add CheckoutAttributeProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -65,32 +65,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
     {
         public AdminMapperModelConfiguration()
         {
-            //checkout attributes
-            CreateMap<CheckoutAttribute, CheckoutAttributeModel>()
-                .ForMember(dest => dest.AvailableTaxCategories, mo => mo.Ignore())
-                .ForMember(dest => dest.AttributeControlTypeName, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableStores, mo => mo.Ignore())
-                .ForMember(dest => dest.SelectedStoreIds, mo => mo.Ignore())
-                .ForMember(dest => dest.AvailableCustomerRoles, mo => mo.Ignore())
-                .ForMember(dest => dest.SelectedCustomerRoleIds, mo => mo.Ignore())
-                .ForMember(dest => dest.ConditionAllowed, mo => mo.Ignore())
-                .ForMember(dest => dest.ConditionModel, mo => mo.Ignore());
-            CreateMap<CheckoutAttributeModel, CheckoutAttribute>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.MapFrom(x => x.Locales.ToLocalizedProperty()))
-                .ForMember(dest => dest.AttributeControlType, mo => mo.Ignore())
-                .ForMember(dest => dest.ConditionAttributeXml, mo => mo.Ignore())
-                .ForMember(dest => dest.Stores, mo => mo.MapFrom(x => x.SelectedStoreIds != null ? x.SelectedStoreIds.ToList() : new List<string>()))
-                .ForMember(dest => dest.CustomerRoles, mo => mo.MapFrom(x => x.SelectedCustomerRoleIds != null ? x.SelectedCustomerRoleIds.ToList() : new List<string>()))
-                .ForMember(dest => dest.CheckoutAttributeValues, mo => mo.Ignore());
-
-            CreateMap<CheckoutAttributeValue, CheckoutAttributeValueModel>()
-               .ForMember(dest => dest.Locales, mo => mo.Ignore());
-            CreateMap<CheckoutAttributeValueModel, CheckoutAttributeValue>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.Locales, mo => mo.MapFrom(x => x.Locales.ToLocalizedProperty()));
-
             //contact attributes
             CreateMap<ContactAttribute, ContactAttributeModel>()
                 .ForMember(dest => dest.Locales, mo => mo.Ignore())
