24a4ff00ee646b40a010b650c6ccbb650f5c0867
TTomu <wozniaktomasz@gmail.com>
2020-03-02 12:21:00 +0100

    Minor changes - refactoring AdminMapper - add RewardPointsSettingsProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperSettingsConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperSettingsConfiguration.cs
@@ -20,20 +20,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
         public AdminMapperSettingsConfiguration()
         {
 
-            CreateMap<RewardPointsSettings, RewardPointsSettingsModel>()
-                .ForMember(dest => dest.PrimaryStoreCurrencyCode, mo => mo.Ignore())
-                .ForMember(dest => dest.ActiveStoreScopeConfiguration, mo => mo.Ignore())
-                .ForMember(dest => dest.Enabled_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.ExchangeRate_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.MinimumRewardPointsToUse_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.PointsForRegistration_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.PointsForPurchases_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.PointsForPurchases_Awarded_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.PointsForPurchases_Canceled_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.DisplayHowMuchWillBeEarned_OverrideForStore, mo => mo.Ignore())
-                .ForMember(dest => dest.GenericAttributes, mo => mo.Ignore());
-            CreateMap<RewardPointsSettingsModel, RewardPointsSettings>();
-
             CreateMap<OrderSettings, OrderSettingsModel>()
                 .ForMember(dest => dest.GiftCards_Activated_OrderStatuses, mo => mo.Ignore())
                 .ForMember(dest => dest.PrimaryStoreCurrencyCode, mo => mo.Ignore())
