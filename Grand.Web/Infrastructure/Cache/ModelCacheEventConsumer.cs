81acaf33447ec570dc987f1f85d90773d6591118
TTomu <wozniaktomasz@gmail.com>
2020-03-02 13:09:48 +0100

    Minor changes - refactoring ModelCacheEvent - add ProductNotificatioHandler

--- Grand.Web/Infrastructure/Cache/ModelCacheEventConsumer.cs
+++ Grand.Web/Infrastructure/Cache/ModelCacheEventConsumer.cs
@@ -18,10 +18,7 @@ namespace Grand.Web.Infrastructure.Cache
     /// </summary>
     public partial class ModelCacheEventConsumer :
         
-        //products
-        INotificationHandler<EntityInserted<Product>>,
-        INotificationHandler<EntityUpdated<Product>>,
-        INotificationHandler<EntityDeleted<Product>>,
+        
         //related product
         INotificationHandler<EntityInserted<RelatedProduct>>,
         INotificationHandler<EntityUpdated<RelatedProduct>>,
@@ -108,29 +105,6 @@ namespace Grand.Web.Infrastructure.Cache
             _cacheManager = cacheManager;
         }
 
-        //products
-        public async Task Handle(EntityInserted<Product> eventMessage, CancellationToken cancellationToken)
-        {
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.SITEMAP_PATTERN_KEY);
-        }
-        public async Task Handle(EntityUpdated<Product> eventMessage, CancellationToken cancellationToken)
-        {
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.HOMEPAGE_BESTSELLERS_IDS_PATTERN_KEY);
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.PRODUCTS_ALSO_PURCHASED_IDS_PATTERN_KEY);
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.SITEMAP_PATTERN_KEY);
-
-            await _cacheManager.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.Id));
-            await _cacheManager.RemoveAsync(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.Id));
-        }
-        public async Task Handle(EntityDeleted<Product> eventMessage, CancellationToken cancellationToken)
-        {
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.HOMEPAGE_BESTSELLERS_IDS_PATTERN_KEY);
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.PRODUCTS_ALSO_PURCHASED_IDS_PATTERN_KEY);
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.SITEMAP_PATTERN_KEY);
-
-            await _cacheManager.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.Id));
-            await _cacheManager.RemoveAsync(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.Id));
-        }
 
         //product tags
         public async Task Handle(EntityInserted<ProductTag> eventMessage, CancellationToken cancellationToken)
