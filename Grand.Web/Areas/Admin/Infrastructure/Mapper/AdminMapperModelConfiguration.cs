b92fd04c1b861d53de50bcffe099bc8aa390dd36
TTomu <wozniaktomasz@gmail.com>
2020-03-02 11:37:50 +0100

    Minor changes - refactoring AdminMapper - add GiftCardProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -65,20 +65,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
     {
         public AdminMapperModelConfiguration()
         {
-            //gift cards
-            CreateMap<GiftCard, GiftCardModel>()
-                .ForMember(dest => dest.PurchasedWithOrderId, mo => mo.Ignore())
-                .ForMember(dest => dest.AmountStr, mo => mo.Ignore())
-                .ForMember(dest => dest.RemainingAmountStr, mo => mo.Ignore())
-                .ForMember(dest => dest.CreatedOn, mo => mo.Ignore())
-                .ForMember(dest => dest.PrimaryStoreCurrencyCode, mo => mo.Ignore());
-            CreateMap<GiftCardModel, GiftCard>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.GiftCardType, mo => mo.Ignore())
-                .ForMember(dest => dest.GiftCardUsageHistory, mo => mo.Ignore())
-                .ForMember(dest => dest.PurchasedWithOrderItem, mo => mo.Ignore())
-                .ForMember(dest => dest.IsRecipientNotified, mo => mo.Ignore())
-                .ForMember(dest => dest.CreatedOnUtc, mo => mo.Ignore());
             //stores
             CreateMap<Store, StoreModel>()
                 .ForMember(dest => dest.AvailableLanguages, mo => mo.Ignore())
