c3569ad84dd25fb7e44bf858925217055ae6a5a3
KrzysztofPajak <krzysiek@nop4you.com>
2020-03-02 13:06:25 +0100

    Fix test

--- Tests/Grand.Services.Tests/Seo/SeoExtensionsTests.cs
+++ Tests/Grand.Services.Tests/Seo/SeoExtensionsTests.cs
@@ -8,12 +8,13 @@ namespace Grand.Services.Seo.Tests
         [TestMethod()]
         public void SeoExtensionsTest()
         {
+            var charConversions = "ä:a;ö:o;ü:u;т:t;е:e;с:s;т:t";
 
             //german letters with diacritics
-            Assert.AreEqual("testaou", SeoExtensions.GetSeName("testäöü", true, false));
+            Assert.AreEqual("testaou", SeoExtensions.GetSeName("testäöü", true, false, charConversions));
             Assert.AreEqual("test", SeoExtensions.GetSeName("testäöü", false, false));
             //russian letters           
-            Assert.AreEqual("testtest", SeoExtensions.GetSeName("testтест", true, false));
+            Assert.AreEqual("testtest", SeoExtensions.GetSeName("testтест", true, false, charConversions));
             Assert.AreEqual("test", SeoExtensions.GetSeName("testтест", false, false));
 
             Assert.AreEqual("test", SeoExtensions.GetSeName("testтест", false, false));
