af500ca4c2513743a4ee55ef5c3f5b15f825ea48
TTomu <wozniaktomasz@gmail.com>
2020-03-02 09:24:34 +0100

    Minor changes - refactoring AdminMapper - add QueuedEmailProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -65,25 +65,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
     {
         public AdminMapperModelConfiguration()
         {
-            //queued email
-            CreateMap<QueuedEmail, QueuedEmailModel>()
-                .ForMember(dest => dest.CreatedOn, mo => mo.Ignore())
-                .ForMember(dest => dest.PriorityName, mo => mo.Ignore())
-                .ForMember(dest => dest.DontSendBeforeDate, mo => mo.Ignore())
-                .ForMember(dest => dest.SendImmediately, mo => mo.Ignore())
-                .ForMember(dest => dest.SentOn, mo => mo.Ignore());
-
-            CreateMap<QueuedEmailModel, QueuedEmail>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.Priority, dt => dt.Ignore())
-                .ForMember(dest => dest.PriorityId, dt => dt.Ignore())
-                .ForMember(dest => dest.CreatedOnUtc, dt => dt.Ignore())
-                .ForMember(dest => dest.DontSendBeforeDateUtc, mo => mo.Ignore())
-                .ForMember(dest => dest.SentOnUtc, mo => mo.Ignore())
-                .ForMember(dest => dest.EmailAccountId, mo => mo.Ignore())
-                .ForMember(dest => dest.AttachmentFilePath, mo => mo.Ignore())
-                .ForMember(dest => dest.AttachmentFileName, mo => mo.Ignore());
-
             //contact form
             CreateMap<ContactUs, ContactFormModel>()
                 .ForMember(dest => dest.CreatedOn, mo => mo.Ignore());
