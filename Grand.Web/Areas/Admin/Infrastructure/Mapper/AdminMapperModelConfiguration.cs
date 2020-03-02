e771b2b4780358b0c54f3edb1cc6704b170e6d94
TTomu <wozniaktomasz@gmail.com>
2020-03-02 11:11:48 +0100

    Minor changes - refactoring AdminMapper - add CustomerReminderModelProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -65,23 +65,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
     {
         public AdminMapperModelConfiguration()
         {
-            //Customer reminder
-            CreateMap<CustomerReminderModel, CustomerReminder>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore());
-
-            CreateMap<CustomerReminder, CustomerReminderModel>();
-
-            CreateMap<CustomerReminder.ReminderLevel, CustomerReminderModel.ReminderLevelModel>()
-                .ForMember(dest => dest.EmailAccounts, mo => mo.Ignore());
-            CreateMap<CustomerReminderModel.ReminderLevelModel, CustomerReminder.ReminderLevel>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore());
-
-            CreateMap<CustomerReminder.ReminderCondition, CustomerReminderModel.ConditionModel>()
-                .ForMember(dest => dest.ConditionType, mo => mo.Ignore());
-            CreateMap<CustomerReminderModel.ConditionModel, CustomerReminder.ReminderCondition>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.ConditionType, mo => mo.Ignore());
-
             //user api
             CreateMap<UserApi, UserApiModel>()
                 .ForMember(dest => dest.Password, mo => mo.Ignore());
