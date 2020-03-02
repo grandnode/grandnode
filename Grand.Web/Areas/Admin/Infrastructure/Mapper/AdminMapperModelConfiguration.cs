4f1af37b0d836796a72bfb192c244a82bb2821cd
TTomu <wozniaktomasz@gmail.com>
2020-03-02 10:45:18 +0100

    Minor changes - refactoring AdminMapper - add ForumGroupProfile

--- Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperModelConfiguration.cs
@@ -65,30 +65,6 @@ namespace Grand.Web.Areas.Admin.Infrastructure.Mapper
     {
         public AdminMapperModelConfiguration()
         {
-            //forums
-            CreateMap<ForumGroup, ForumGroupModel>()
-                .ForMember(dest => dest.CreatedOn, mo => mo.Ignore());
-
-            CreateMap<ForumGroupModel, ForumGroup>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.CreatedOnUtc, mo => mo.Ignore())
-                .ForMember(dest => dest.UpdatedOnUtc, mo => mo.Ignore());
-
-            CreateMap<Forum, ForumModel>()
-                .ForMember(dest => dest.CreatedOn, mo => mo.Ignore())
-                .ForMember(dest => dest.ForumGroups, mo => mo.Ignore());
-
-            CreateMap<ForumModel, Forum>()
-                .ForMember(dest => dest.Id, mo => mo.Ignore())
-                .ForMember(dest => dest.NumTopics, mo => mo.Ignore())
-                .ForMember(dest => dest.NumPosts, mo => mo.Ignore())
-                .ForMember(dest => dest.LastTopicId, mo => mo.Ignore())
-                .ForMember(dest => dest.LastPostId, mo => mo.Ignore())
-                .ForMember(dest => dest.LastPostCustomerId, mo => mo.Ignore())
-                .ForMember(dest => dest.LastPostTime, mo => mo.Ignore())
-                .ForMember(dest => dest.CreatedOnUtc, mo => mo.Ignore())
-                .ForMember(dest => dest.UpdatedOnUtc, mo => mo.Ignore());
-
             //knowledgebase
             CreateMap<KnowledgebaseCategory, KnowledgebaseCategoryModel>()
                 .ForMember(dest => dest.Locales, mo => mo.Ignore())
