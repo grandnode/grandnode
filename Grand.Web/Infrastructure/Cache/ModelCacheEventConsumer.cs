7bc80b61da1a1c19f4601f61c901333f4f6000db
TTomu <wozniaktomasz@gmail.com>
2020-03-02 13:48:08 +0100

    Minor changes - refactoring ModelCacheEvent - add CategoryTemplateNotificatioHandler

--- Grand.Web/Infrastructure/Cache/ModelCacheEventConsumer.cs
+++ Grand.Web/Infrastructure/Cache/ModelCacheEventConsumer.cs
@@ -13,15 +13,7 @@ using System.Threading.Tasks;
 
 namespace Grand.Web.Infrastructure.Cache
 {
-    /// <summary>
-    /// Model cache event consumer (used for caching of presentation layer models)
-    /// </summary>
     public partial class ModelCacheEventConsumer :
-        
-        //templates
-        INotificationHandler<EntityInserted<CategoryTemplate>>,
-        INotificationHandler<EntityUpdated<CategoryTemplate>>,
-        INotificationHandler<EntityDeleted<CategoryTemplate>>,
         INotificationHandler<EntityInserted<ManufacturerTemplate>>,
         INotificationHandler<EntityUpdated<ManufacturerTemplate>>,
         INotificationHandler<EntityDeleted<ManufacturerTemplate>>,
@@ -31,11 +23,7 @@ namespace Grand.Web.Infrastructure.Cache
         INotificationHandler<EntityInserted<TopicTemplate>>,
         INotificationHandler<EntityUpdated<TopicTemplate>>,
         INotificationHandler<EntityDeleted<TopicTemplate>>
-        
-        
     {
-
-
         private readonly ICacheManager _cacheManager;
 
         public ModelCacheEventConsumer(ICacheManager cacheManager)
@@ -43,21 +31,6 @@ namespace Grand.Web.Infrastructure.Cache
             _cacheManager = cacheManager;
         }
 
-
-
-        //templates
-        public async Task Handle(EntityInserted<CategoryTemplate> eventMessage, CancellationToken cancellationToken)
-        {
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.CATEGORY_TEMPLATE_PATTERN_KEY);
-        }
-        public async Task Handle(EntityUpdated<CategoryTemplate> eventMessage, CancellationToken cancellationToken)
-        {
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.CATEGORY_TEMPLATE_PATTERN_KEY);
-        }
-        public async Task Handle(EntityDeleted<CategoryTemplate> eventMessage, CancellationToken cancellationToken)
-        {
-            await _cacheManager.RemoveByPrefix(ModelCacheEventConst.CATEGORY_TEMPLATE_PATTERN_KEY);
-        }
         public async Task Handle(EntityInserted<ManufacturerTemplate> eventMessage, CancellationToken cancellationToken)
         {
             await _cacheManager.RemoveByPrefix(ModelCacheEventConst.MANUFACTURER_TEMPLATE_PATTERN_KEY);
