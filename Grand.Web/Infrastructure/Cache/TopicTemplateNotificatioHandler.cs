4a74e97dcd36ea5fbdba080f773f1907428ebe8c
TTomu <wozniaktomasz@gmail.com>
2020-03-02 13:55:00 +0100

    Minor changes - refactoring ModelCacheEvent - add TopicTemplateNotificatioHandler

--- Grand.Web/Infrastructure/Cache/TopicTemplateNotificatioHandler.cs
+++ Grand.Web/Infrastructure/Cache/TopicTemplateNotificatioHandler.cs
@@ -1,11 +1,5 @@
 ﻿using Grand.Core.Caching;
-using Grand.Core.Domain.Blogs;
-using Grand.Core.Domain.Catalog;
-using Grand.Core.Domain.News;
-using Grand.Core.Domain.Orders;
-using Grand.Core.Domain.Polls;
 using Grand.Core.Domain.Topics;
-using Grand.Core.Domain.Vendors;
 using Grand.Core.Events;
 using MediatR;
 using System.Threading;
@@ -13,20 +7,18 @@ using System.Threading.Tasks;
 
 namespace Grand.Web.Infrastructure.Cache
 {
-    public partial class ModelCacheEventConsumer :
+    public partial class TopicTemplateNotificatioHandler :
         INotificationHandler<EntityInserted<TopicTemplate>>,
         INotificationHandler<EntityUpdated<TopicTemplate>>,
         INotificationHandler<EntityDeleted<TopicTemplate>>
     {
         private readonly ICacheManager _cacheManager;
 
-        public ModelCacheEventConsumer(ICacheManager cacheManager)
+        public TopicTemplateNotificatioHandler(ICacheManager cacheManager)
         {
             _cacheManager = cacheManager;
         }
 
-        
-        
         public async Task Handle(EntityInserted<TopicTemplate> eventMessage, CancellationToken cancellationToken)
         {
             await _cacheManager.RemoveByPrefix(ModelCacheEventConst.TOPIC_TEMPLATE_PATTERN_KEY);
@@ -39,7 +31,6 @@ namespace Grand.Web.Infrastructure.Cache
         {
             await _cacheManager.RemoveByPrefix(ModelCacheEventConst.TOPIC_TEMPLATE_PATTERN_KEY);
         }
-
 
     }
 }
