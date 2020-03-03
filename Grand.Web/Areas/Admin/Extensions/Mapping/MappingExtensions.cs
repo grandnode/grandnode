0405097e80566cb2ff49a443d83d2ef1ab3db9e5
TTomu <wozniaktomasz@gmail.com>
2020-03-03 10:50:17 +0100

    Minor changes - refactoring Admin - MappingExtensions - ProductsMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,51 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-
-        #region Products
-
-        public static ProductModel ToModel(this Product entity, IDateTimeHelper dateTimeHelper)
-        {
-            var product = entity.MapTo<Product, ProductModel>();
-            product.MarkAsNewStartDateTime = entity.MarkAsNewStartDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            product.MarkAsNewEndDateTime = entity.MarkAsNewEndDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            product.AvailableStartDateTime = entity.AvailableStartDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            product.AvailableEndDateTime = entity.AvailableEndDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            product.PreOrderAvailabilityStartDateTime = entity.PreOrderAvailabilityStartDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            return product;
-
-        }
-
-        public static Product ToEntity(this ProductModel model, IDateTimeHelper dateTimeHelper)
-        {
-            var product = model.MapTo<ProductModel, Product>();
-            product.MarkAsNewStartDateTimeUtc = model.MarkAsNewStartDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.MarkAsNewEndDateTimeUtc = model.MarkAsNewEndDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.AvailableStartDateTimeUtc = model.AvailableStartDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.AvailableEndDateTimeUtc = model.AvailableEndDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.PreOrderAvailabilityStartDateTimeUtc = model.PreOrderAvailabilityStartDateTime.ConvertToUtcTime(dateTimeHelper);
-
-            return product;
-        }
-
-        public static Product ToEntity(this ProductModel model, Product destination, IDateTimeHelper dateTimeHelper)
-        {
-            var product = model.MapTo(destination);
-            product.MarkAsNewStartDateTimeUtc = model.MarkAsNewStartDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.MarkAsNewEndDateTimeUtc = model.MarkAsNewEndDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.AvailableStartDateTimeUtc = model.AvailableStartDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.AvailableEndDateTimeUtc = model.AvailableEndDateTime.ConvertToUtcTime(dateTimeHelper);
-            product.PreOrderAvailabilityStartDateTimeUtc = model.PreOrderAvailabilityStartDateTime.ConvertToUtcTime(dateTimeHelper);
-            return product;
-        }
-
-        public static ProductAttributeValue ToEntity(this PredefinedProductAttributeValue model)
-        {
-            return model.MapTo<PredefinedProductAttributeValue, ProductAttributeValue>();
-        }
-
-        #endregion
-
         #region Product attributes
 
         public static ProductAttributeModel ToModel(this ProductAttribute entity)
