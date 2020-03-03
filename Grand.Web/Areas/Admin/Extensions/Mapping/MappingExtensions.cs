d0bf843962f7d80135d744c7e2af2322ad3880ea
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:48:22 +0100

    Minor changes - refactoring Admin - MappingExtensions - NewsItemMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,35 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region News
-
-        //news items
-        public static NewsItemModel ToModel(this NewsItem entity, IDateTimeHelper dateTimeHelper)
-        {
-            var newsitem = entity.MapTo<NewsItem, NewsItemModel>();
-            newsitem.StartDate = entity.StartDateUtc.ConvertToUserTime(dateTimeHelper);
-            newsitem.EndDate = entity.EndDateUtc.ConvertToUserTime(dateTimeHelper);
-            return newsitem;
-        }
-
-        public static NewsItem ToEntity(this NewsItemModel model, IDateTimeHelper dateTimeHelper)
-        {
-            var newsitem = model.MapTo<NewsItemModel, NewsItem>();
-            newsitem.StartDateUtc = model.StartDate.ConvertToUtcTime(dateTimeHelper);
-            newsitem.EndDateUtc = model.EndDate.ConvertToUtcTime(dateTimeHelper);
-            return newsitem;
-        }
-
-        public static NewsItem ToEntity(this NewsItemModel model, NewsItem destination, IDateTimeHelper dateTimeHelper)
-        {
-            var newsitem = model.MapTo(destination);
-            newsitem.StartDateUtc = model.StartDate.ConvertToUtcTime(dateTimeHelper);
-            newsitem.EndDateUtc = model.EndDate.ConvertToUtcTime(dateTimeHelper);
-            return newsitem;
-        }
-
-        #endregion
-
         #region Polls
 
         //poll items
