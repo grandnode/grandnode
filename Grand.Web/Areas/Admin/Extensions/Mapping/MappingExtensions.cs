bced37d39cb8e5b71206c7e6334d1db363233bd9
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:17:20 +0100

    Minor changes - refactoring Admin - MappingExtensions - InteractiveFormMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,57 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Interactive form
-
-        public static InteractiveFormModel ToModel(this InteractiveForm entity)
-        {
-            return entity.MapTo<InteractiveForm, InteractiveFormModel>();
-        }
-
-        public static InteractiveForm ToEntity(this InteractiveFormModel model)
-        {
-            return model.MapTo<InteractiveFormModel, InteractiveForm>();
-        }
-
-        public static InteractiveForm ToEntity(this InteractiveFormModel model, InteractiveForm destination)
-        {
-            return model.MapTo(destination);
-        }
-
-
-        public static InteractiveFormAttributeModel ToModel(this InteractiveForm.FormAttribute entity)
-        {
-            return entity.MapTo<InteractiveForm.FormAttribute, InteractiveFormAttributeModel>();
-        }
-
-        public static InteractiveForm.FormAttribute ToEntity(this InteractiveFormAttributeModel model)
-        {
-            return model.MapTo<InteractiveFormAttributeModel, InteractiveForm.FormAttribute>();
-        }
-
-        public static InteractiveForm.FormAttribute ToEntity(this InteractiveFormAttributeModel model, InteractiveForm.FormAttribute destination)
-        {
-            return model.MapTo(destination);
-        }
-
-
-        public static InteractiveFormAttributeValueModel ToModel(this InteractiveForm.FormAttributeValue entity)
-        {
-            return entity.MapTo<InteractiveForm.FormAttributeValue, InteractiveFormAttributeValueModel>();
-        }
-
-        public static InteractiveForm.FormAttributeValue ToEntity(this InteractiveFormAttributeValueModel model)
-        {
-            return model.MapTo<InteractiveFormAttributeValueModel, InteractiveForm.FormAttributeValue>();
-        }
-
-        public static InteractiveForm.FormAttributeValue ToEntity(this InteractiveFormAttributeValueModel model, InteractiveForm.FormAttributeValue destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        #endregion
-
         #region Documents
 
         public static DocumentModel ToModel(this Document entity)
