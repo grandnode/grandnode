e37d2204dae9d7e8929ead7e31af072fa9b3a5b6
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:49:17 +0100

    Minor changes - refactoring Admin - MappingExtensions - PollMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,54 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Polls
-
-        //poll items
-        public static PollModel ToModel(this Poll entity, IDateTimeHelper dateTimeHelper)
-        {
-            var poll = entity.MapTo<Poll, PollModel>();
-            poll.StartDate = entity.StartDateUtc.ConvertToUserTime(dateTimeHelper);
-            poll.EndDate = entity.EndDateUtc.ConvertToUserTime(dateTimeHelper);
-            return poll;
-        }
-        
-        public static Poll ToEntity(this PollModel model, IDateTimeHelper dateTimeHelper)
-        {
-            var poll = model.MapTo<PollModel, Poll>();
-            poll.StartDateUtc = model.StartDate.ConvertToUtcTime(dateTimeHelper);
-            poll.EndDateUtc = model.EndDate.ConvertToUtcTime(dateTimeHelper);
-            return poll;
-
-        }
-
-        public static Poll ToEntity(this PollModel model, Poll destination, IDateTimeHelper dateTimeHelper)
-        {
-            var poll = model.MapTo(destination);
-            poll.StartDateUtc = model.StartDate.ConvertToUtcTime(dateTimeHelper);
-            poll.EndDateUtc = model.EndDate.ConvertToUtcTime(dateTimeHelper);
-            return poll;
-
-        }
-
-        //poll answer
-        public static PollAnswerModel ToModel(this PollAnswer entity)
-        {
-            return entity.MapTo<PollAnswer, PollAnswerModel>();
-        }
-
-        public static PollAnswer ToEntity(this PollAnswerModel model)
-        {
-            return model.MapTo<PollAnswerModel, PollAnswer>();
-        }
-
-        public static PollAnswer ToEntity(this PollAnswerModel model, PollAnswer destination)
-        {
-            return model.MapTo(destination);
-        }
-
-
-        #endregion
-
         #region Customer roles
 
         //customer roles
