2d479e415c58e226de59d44a9ede7d058ffa216a
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:52:10 +0100

    Minor changes - refactoring Admin - MappingExtensions - CustomerActionMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,50 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Customer Action
-
-        //customer action
-        public static CustomerActionModel ToModel(this CustomerAction entity, IDateTimeHelper dateTimeHelper)
-        {
-            var action = entity.MapTo<CustomerAction, CustomerActionModel>();
-            action.StartDateTime = entity.StartDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            action.EndDateTime = entity.EndDateTimeUtc.ConvertToUserTime(dateTimeHelper);
-            return action;
-        }
-
-        public static CustomerAction ToEntity(this CustomerActionModel model, IDateTimeHelper dateTimeHelper)
-        {
-            var action = model.MapTo<CustomerActionModel, CustomerAction>();
-            action.StartDateTimeUtc = model.StartDateTime.ConvertToUtcTime(dateTimeHelper);
-            action.EndDateTimeUtc = model.EndDateTime.ConvertToUtcTime(dateTimeHelper);
-            return action;
-        }
-
-        public static CustomerAction ToEntity(this CustomerActionModel model, CustomerAction destination, IDateTimeHelper dateTimeHelper)
-        {
-            var action = model.MapTo(destination);
-            action.StartDateTimeUtc = model.StartDateTime.ConvertToUtcTime(dateTimeHelper);
-            action.EndDateTimeUtc = model.EndDateTime.ConvertToUtcTime(dateTimeHelper);
-            return action;
-        }
-
-        public static CustomerActionConditionModel ToModel(this CustomerAction.ActionCondition entity)
-        {
-            return entity.MapTo<CustomerAction.ActionCondition, CustomerActionConditionModel>();
-        }
-
-        public static CustomerAction.ActionCondition ToEntity(this CustomerActionConditionModel model)
-        {
-            return model.MapTo<CustomerActionConditionModel, CustomerAction.ActionCondition>();
-        }
-
-        public static CustomerAction.ActionCondition ToEntity(this CustomerActionConditionModel model, CustomerAction.ActionCondition destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        #endregion
-
         #region Customer Action Type
 
         public static CustomerActionTypeModel ToModel(this CustomerActionType entity)
