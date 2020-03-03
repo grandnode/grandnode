b052fbd52059031e1ca84bc5fd44c656fcfbd4d0
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:46:17 +0100

    Minor changes - refactoring Admin - MappingExtensions - BlogPostMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,35 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Blog
-
-        //blog posts
-        public static BlogPostModel ToModel(this BlogPost entity, IDateTimeHelper dateTimeHelper)
-        {
-            var blogpost = entity.MapTo<BlogPost, BlogPostModel>();
-            blogpost.StartDate = entity.StartDateUtc.ConvertToUserTime(dateTimeHelper);
-            blogpost.EndDate = entity.EndDateUtc.ConvertToUserTime(dateTimeHelper);
-            return blogpost;
-        }
-
-        public static BlogPost ToEntity(this BlogPostModel model, IDateTimeHelper dateTimeHelper)
-        {
-            var blogpost = model.MapTo<BlogPostModel, BlogPost>();
-            blogpost.StartDateUtc = model.StartDate.ConvertToUtcTime(dateTimeHelper);
-            blogpost.EndDateUtc = model.EndDate.ConvertToUtcTime(dateTimeHelper);
-            return blogpost;
-        }
-
-        public static BlogPost ToEntity(this BlogPostModel model, BlogPost destination, IDateTimeHelper dateTimeHelper)
-        {
-            var blogpost = model.MapTo(destination);
-            blogpost.StartDateUtc = model.StartDate.ConvertToUtcTime(dateTimeHelper);
-            blogpost.EndDateUtc = model.EndDate.ConvertToUtcTime(dateTimeHelper);
-            return blogpost;
-        }
-
-        #endregion
-
         #region Blog categories
 
         public static BlogCategoryModel ToModel(this BlogCategory entity)
