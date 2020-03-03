77bb783df7dd65422f105e76389929267fcdff03
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:54:55 +0100

    Minor changes - refactoring Admin - MappingExtensions - UserApiMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,25 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region User api
-
-        public static UserApiModel ToModel(this UserApi entity)
-        {
-            return entity.MapTo<UserApi, UserApiModel>();
-        }
-
-        public static UserApi ToEntity(this UserApiModel model)
-        {
-            return model.MapTo<UserApiModel, UserApi>();
-        }
-
-        public static UserApi ToEntity(this UserApiModel model, UserApi destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        #endregion
-
         #region Gift Cards
 
         public static GiftCardModel ToModel(this GiftCard entity)
