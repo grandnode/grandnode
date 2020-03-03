fbada8e1b43776ec39a453d6b95ed479c20ebbb0
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:08:54 +0100

    Minor changes - refactoring Admin - MappingExtensions - CustomerAttributeMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,41 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Customer attributes
-
-        //customer attributes
-        public static CustomerAttributeModel ToModel(this CustomerAttribute entity)
-        {
-            return entity.MapTo<CustomerAttribute, CustomerAttributeModel>();
-        }
-
-        public static CustomerAttribute ToEntity(this CustomerAttributeModel model)
-        {
-            return model.MapTo<CustomerAttributeModel, CustomerAttribute>();
-        }
-
-        public static CustomerAttribute ToEntity(this CustomerAttributeModel model, CustomerAttribute destination)
-        {
-            return model.MapTo(destination);
-        }
-        //customer attributes value
-        public static CustomerAttributeValueModel ToModel(this CustomerAttributeValue entity)
-        {
-            return entity.MapTo<CustomerAttributeValue, CustomerAttributeValueModel>();
-        }
-
-        public static CustomerAttributeValue ToEntity(this CustomerAttributeValueModel model)
-        {
-            return model.MapTo<CustomerAttributeValueModel, CustomerAttributeValue>();
-        }
-
-        public static CustomerAttributeValue ToEntity(this CustomerAttributeValueModel model, CustomerAttributeValue destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        #endregion
-
         #region Address attributes
 
         //attributes
