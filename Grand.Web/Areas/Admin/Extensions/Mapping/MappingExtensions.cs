c50e6287dcb9e75436ca9428ef416d60975c98df
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:21:10 +0100

    Minor changes - refactoring Admin - MappingExtensions - LogMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,39 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Log
-
-        public static LogModel ToModel(this Log entity)
-        {
-            return entity.MapTo<Log, LogModel>();
-        }
-
-        public static Log ToEntity(this LogModel model)
-        {
-            return model.MapTo<LogModel, Log>();
-        }
-
-        public static Log ToEntity(this LogModel model, Log destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        public static ActivityLogTypeModel ToModel(this ActivityLogType entity)
-        {
-            return entity.MapTo<ActivityLogType, ActivityLogTypeModel>();
-        }
-
-        public static ActivityLogModel ToModel(this ActivityLog entity)
-        {
-            return entity.MapTo<ActivityLog, ActivityLogModel>();
-        }
-
-        public static ActivityStatsModel ToModel(this ActivityStats entity)
-        {
-            return entity.MapTo<ActivityStats, ActivityStatsModel>();
-        }
-        #endregion
-
         #region Currencies
 
         public static CurrencyModel ToModel(this Currency entity)
