9f49ce10642fa77efbeeeaab2bfe46011dced1d3
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:56:40 +0100

    Minor changes - refactoring Admin - MappingExtensions - CountryMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,41 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Countries / states
-
-        public static CountryModel ToModel(this Country entity)
-        {
-            return entity.MapTo<Country, CountryModel>();
-        }
-
-        public static Country ToEntity(this CountryModel model)
-        {
-            return model.MapTo<CountryModel, Country>();
-        }
-
-        public static Country ToEntity(this CountryModel model, Country destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        public static StateProvinceModel ToModel(this StateProvince entity)
-        {
-            return entity.MapTo<StateProvince, StateProvinceModel>();
-        }
-
-        public static StateProvince ToEntity(this StateProvinceModel model)
-        {
-            return model.MapTo<StateProvinceModel, StateProvince>();
-        }
-
-        public static StateProvince ToEntity(this StateProvinceModel model, StateProvince destination)
-        {
-            return model.MapTo(destination);
-        }
-
-
-        #endregion
-
         #region Settings
 
         public static TaxSettingsModel ToModel(this TaxSettings entity)
