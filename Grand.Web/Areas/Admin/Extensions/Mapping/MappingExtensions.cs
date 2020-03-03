50b0e2d5cb070f2e75eee411a2f8b731463a069c
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:09:48 +0100

    Minor changes - refactoring Admin - MappingExtensions - AddressAttributeMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,41 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Address attributes
-
-        //attributes
-        public static AddressAttributeModel ToModel(this AddressAttribute entity)
-        {
-            return entity.MapTo<AddressAttribute, AddressAttributeModel>();
-        }
-
-        public static AddressAttribute ToEntity(this AddressAttributeModel model)
-        {
-            return model.MapTo<AddressAttributeModel, AddressAttribute>();
-        }
-
-        public static AddressAttribute ToEntity(this AddressAttributeModel model, AddressAttribute destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        //attributes value
-        public static AddressAttributeValueModel ToModel(this AddressAttributeValue entity)
-        {
-            return entity.MapTo<AddressAttributeValue, AddressAttributeValueModel>();
-        }
-        public static AddressAttributeValue ToEntity(this AddressAttributeValueModel model)
-        {
-            return model.MapTo<AddressAttributeValueModel, AddressAttributeValue>();
-        }
-
-        public static AddressAttributeValue ToEntity(this AddressAttributeValueModel model, AddressAttributeValue destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        #endregion
-
         #region Languages
 
         public static LanguageModel ToModel(this Language entity)
