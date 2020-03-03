4ff175acf813ec6fc110b17f81d172f6f20f018e
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:18:22 +0100

    Minor changes - refactoring Admin - MappingExtensions - DocumentMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,40 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region Documents
-
-        public static DocumentModel ToModel(this Document entity)
-        {
-            return entity.MapTo<Document, DocumentModel>();
-        }
-
-        public static Document ToEntity(this DocumentModel model)
-        {
-            return model.MapTo<DocumentModel, Document>();
-        }
-
-        public static Document ToEntity(this DocumentModel model, Document destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        public static DocumentTypeModel ToModel(this DocumentType entity)
-        {
-            return entity.MapTo<DocumentType, DocumentTypeModel>();
-        }
-
-        public static DocumentType ToEntity(this DocumentTypeModel model)
-        {
-            return model.MapTo<DocumentTypeModel, DocumentType>();
-        }
-
-        public static DocumentType ToEntity(this DocumentTypeModel model, DocumentType destination)
-        {
-            return model.MapTo(destination);
-        }
-
-        #endregion
-
         #region Campaigns
 
         public static CampaignModel ToModel(this Campaign entity)
