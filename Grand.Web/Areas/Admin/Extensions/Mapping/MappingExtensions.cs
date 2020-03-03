ee26d91ca75a01435398fe0eee5243027d629449
TTomu <wozniaktomasz@gmail.com>
2020-03-03 11:05:26 +0100

    Minor changes - refactoring Admin - MappingExtensions - TierPriceMappingExtensions

--- Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/MappingExtensions.cs
@@ -74,34 +74,6 @@ namespace Grand.Web.Areas.Admin.Extensions
             return AutoMapperConfiguration.Mapper.Map(source, destination);
         }
 
-        #region TierPrices
-        //attributes
-        public static ProductModel.TierPriceModel ToModel(this TierPrice entity, IDateTimeHelper dateTimeHelper)
-        {
-            var tierprice = entity.MapTo<TierPrice, ProductModel.TierPriceModel>();
-            tierprice.StartDateTime = tierprice.StartDateTime.ConvertToUserTime(dateTimeHelper);
-            tierprice.EndDateTime = tierprice.EndDateTime.ConvertToUserTime(dateTimeHelper);
-            return tierprice;
-        }
-
-        public static TierPrice ToEntity(this ProductModel.TierPriceModel model, IDateTimeHelper dateTimeHelper)
-        {
-            var tierprice = model.MapTo<ProductModel.TierPriceModel, TierPrice>();
-            tierprice.StartDateTimeUtc = tierprice.StartDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
-            tierprice.EndDateTimeUtc = tierprice.EndDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
-            return tierprice;
-        }
-
-        public static TierPrice ToEntity(this ProductModel.TierPriceModel model, TierPrice destination, IDateTimeHelper dateTimeHelper)
-        {
-            var tierprice = model.MapTo(destination);
-            tierprice.StartDateTimeUtc = tierprice.StartDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
-            tierprice.EndDateTimeUtc = tierprice.EndDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
-            return tierprice;
-        }
-
-        #endregion
-
         #region Checkout attributes
 
         //attributes
---
+++ Grand.Web/Areas/Admin/Extensions/Mapping/TierPriceMappingExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Grand.Core.Domain.Catalog;
+using Grand.Services.Helpers;
+using Grand.Web.Areas.Admin.Models.Catalog;
+
+namespace Grand.Web.Areas.Admin.Extensions
+{
+    public static class TierPriceMappingExtensions
+    {
+        public static ProductModel.TierPriceModel ToModel(this TierPrice entity, IDateTimeHelper dateTimeHelper)
+        {
+            var tierprice = entity.MapTo<TierPrice, ProductModel.TierPriceModel>();
+            tierprice.StartDateTime = tierprice.StartDateTime.ConvertToUserTime(dateTimeHelper);
+            tierprice.EndDateTime = tierprice.EndDateTime.ConvertToUserTime(dateTimeHelper);
+            return tierprice;
+        }
+
+        public static TierPrice ToEntity(this ProductModel.TierPriceModel model, IDateTimeHelper dateTimeHelper)
+        {
+            var tierprice = model.MapTo<ProductModel.TierPriceModel, TierPrice>();
+            tierprice.StartDateTimeUtc = tierprice.StartDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
+            tierprice.EndDateTimeUtc = tierprice.EndDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
+            return tierprice;
+        }
+
+        public static TierPrice ToEntity(this ProductModel.TierPriceModel model, TierPrice destination, IDateTimeHelper dateTimeHelper)
+        {
+            var tierprice = model.MapTo(destination);
+            tierprice.StartDateTimeUtc = tierprice.StartDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
+            tierprice.EndDateTimeUtc = tierprice.EndDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
+            return tierprice;
+        }
+    }
+}
