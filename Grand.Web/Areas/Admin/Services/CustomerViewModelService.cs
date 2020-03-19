6d08f3bcfbda3926b7216927fcb26f88cfe89620
TTomu <wozniaktomasz@gmail.com>
2020-03-19 13:43:17 +0100

    Fix for #359

--- Grand.Web/Areas/Admin/Services/CustomerViewModelService.cs
+++ Grand.Web/Areas/Admin/Services/CustomerViewModelService.cs
@@ -1068,6 +1068,20 @@ namespace Grand.Web.Areas.Admin.Services
             //activity log
             await _customerActivityService.InsertActivity("DeleteCustomer", customer.Id, _localizationService.GetResource("ActivityLog.DeleteCustomer"), customer.Id);
         }
+
+        public virtual async Task DeleteSelected(IList<string> selectedIds)
+        {
+            var customers = new List<Customer>();
+            customers.AddRange(await _customerService.GetCustomersByIds(selectedIds.ToArray()));
+            for (var i = 0; i < customers.Count; i++)
+            {
+                var customer = customers[i];
+                await _customerService.DeleteCustomer(customer);
+                //activity log
+                await _customerActivityService.InsertActivity("DeleteCustomer", customer.Id, _localizationService.GetResource("ActivityLog.DeleteCustomer"), customer.Id);
+            }
+        }
+
         public async Task SendEmail(Customer customer, CustomerModel.SendEmailModel model)
         {
             var emailAccount = await _emailAccountService.GetEmailAccountById(_emailAccountSettings.DefaultEmailAccountId);
