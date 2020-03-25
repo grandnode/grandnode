eb9b2cd5fbe1bc3cb2c157b0ad49d83fbe447e09
KrzysztofPajak <krzysiek@nop4you.com>
2020-03-25 09:10:19 +0100

    Typo fix

--- Grand.Services/Stores/StoreMappingService.cs
+++ Grand.Services/Stores/StoreMappingService.cs
@@ -1,5 +1,4 @@
 using Grand.Core;
-using Grand.Core.Caching;
 using Grand.Core.Domain.Catalog;
 using Grand.Core.Domain.Stores;
 using System;
@@ -11,19 +10,6 @@ namespace Grand.Services.Stores
     /// </summary>
     public partial class StoreMappingService : IStoreMappingService
     {
-        #region Constants
-
-        /// <summary>
-        /// Key for caching
-        /// </summary>
-        /// <remarks>
-        /// {0} : entity ID
-        /// {1} : entity name
-        /// </remarks>
-        private const string STOREMAPPING_BY_ENTITYID_NAME_KEY = "Grand.storemapping.entityid-name-{0}-{1}";
-
-        #endregion
-
         #region Fields
 
         private readonly IStoreContext _storeContext;
@@ -36,7 +22,6 @@ namespace Grand.Services.Stores
         /// <summary>
         /// Ctor
         /// </summary>
-        /// <param name="cacheManager">Cache manager</param>
         /// <param name="storeContext">Store context</param>
         /// <param name="catalogSettings">Catalog settings</param>
         public StoreMappingService(IStoreContext storeContext,
