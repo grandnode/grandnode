16c155999dbf2fb69e869aa75ab82812c51095a0
KrzysztofPajak <krzysiek@nop4you.com>
2020-03-31 12:06:34 +0200

    Minor fix send quantity below store owner notification

--- Grand.Services/Commands/Handlers/Catalog/SendQuantityBelowStoreOwnerNotificationCommandHandler.cs
+++ Grand.Services/Commands/Handlers/Catalog/SendQuantityBelowStoreOwnerNotificationCommandHandler.cs
@@ -22,7 +22,7 @@ namespace Grand.Services.Commands.Handlers.Catalog
 
         public async Task<bool> Handle(SendQuantityBelowStoreOwnerNotificationCommand request, CancellationToken cancellationToken)
         {
-            if (request.ProductAttributeCombination != null)
+            if (request.ProductAttributeCombination == null)
                 await _workflowMessageService.SendQuantityBelowStoreOwnerNotification(request.Customer, request.Product, _localizationSettings.DefaultAdminLanguageId);
             else
                 await _workflowMessageService.SendQuantityBelowStoreOwnerNotification(request.Customer, request.Product, request.ProductAttributeCombination, _localizationSettings.DefaultAdminLanguageId);
