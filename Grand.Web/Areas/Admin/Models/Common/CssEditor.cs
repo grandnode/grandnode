4e1aff1524aceac337b5752ea80ff4afeb83c2e2
KrzysztofPajak <krzysiek@nop4you.com>
2020-04-17 09:30:48 +0200

    Admin panel - allow to edit custom style

---
+++ Grand.Web/Areas/Admin/Models/Common/CssEditor.cs
@@ -0,0 +1,10 @@
+﻿
+using Grand.Framework.Mvc.ModelBinding;
+
+namespace Grand.Web.Areas.Admin.Models.Common
+{
+    public partial class CssEditor
+    {
+        public string Content { get; set; }
+    }
+}
