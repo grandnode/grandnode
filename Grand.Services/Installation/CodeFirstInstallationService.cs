369b380c23389cd41212bf2a487df721e88c30a4
KrzysztofPajak <krzysiek@nop4you.com>
2020-05-18 10:52:29 +0200

    Installation - change default active shipping rate method system name to "Shipping.FixedRate"

--- Grand.Services/Installation/CodeFirstInstallationService.cs
+++ Grand.Services/Installation/CodeFirstInstallationService.cs
@@ -5226,7 +5226,7 @@ namespace Grand.Services.Installation
             });
 
             await _settingService.SaveSetting(new ShippingSettings {
-                ActiveShippingRateComputationMethodSystemNames = new List<string> { "Shipping.ByWeight" },
+                ActiveShippingRateComputationMethodSystemNames = new List<string> { "Shipping.FixedRate" },
                 ShipToSameAddress = false,
                 AllowPickUpInStore = true,
                 UseWarehouseLocation = false,
