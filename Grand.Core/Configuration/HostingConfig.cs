f28203fdfeb1f3f6a7ab0d09793d4af1bd68da69
KrzysztofPajak <krzysiek@nop4you.com>
2020-06-29 11:15:17 +0200

    Improve Security - implement host filtering, restricting the hostnames that your app responds to, is recommended whenever you're running in production for security reasons.

--- Grand.Core/Configuration/HostingConfig.cs
+++ Grand.Core/Configuration/HostingConfig.cs
@@ -21,5 +21,10 @@
         /// Gets or sets a value indicating whether to use Forwards proxied headers onto current request
         /// </summary>
         public bool UseForwardedHeaders { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value for allowedHosts, is used for host filtering to bind your app to specific hostnames
+        /// </summary>
+        public string AllowedHosts { get; set; }
     }
 }
