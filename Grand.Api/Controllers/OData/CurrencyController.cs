d14f1d6dd56258997ac5b78f5c22275001a3ab96
PatrykPorabik <patryk@porabik.pl>
2020-07-09 09:12:50 +0200

    API - Prepare SwaggerOperation for Currency Controller

--- Grand.Api/Controllers/OData/CurrencyController.cs
+++ Grand.Api/Controllers/OData/CurrencyController.cs
@@ -5,6 +5,7 @@ using MediatR;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +22,7 @@ namespace Grand.Api.Controllers.OData
             _permissionService = permissionService;
         }
 
+        [SwaggerOperation(summary: "Get entity from Currency")]
         [HttpGet("{key}")]
         public async Task<IActionResult> Get(string key)
         {
@@ -34,6 +36,7 @@ namespace Grand.Api.Controllers.OData
             return Ok(currency.FirstOrDefault());
         }
 
+        [SwaggerOperation(summary: "Get entities from Currency")]
         [HttpGet]
         [EnableQuery(HandleNullPropagation = HandleNullPropagationOption.False)]
         public async Task<IActionResult> Get()
