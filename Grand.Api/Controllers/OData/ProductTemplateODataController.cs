400f1532980122fcddf9bfae468aba00da1d3aae
PatrykPorabik <patryk@porabik.pl>
2020-07-09 10:20:24 +0200

    API - Prepare SwaggerOperation for ProductTemplate Controller

--- Grand.Api/Controllers/OData/ProductTemplateODataController.cs
+++ Grand.Api/Controllers/OData/ProductTemplateODataController.cs
@@ -4,22 +4,24 @@ using MediatR;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Grand.Api.Controllers.OData
 {
-    public partial class ProductTemplateController : BaseODataController
+    public partial class ProductTemplateODataController : BaseODataController
     {
         private readonly IMediator _mediator;
         private readonly IPermissionService _permissionService;
 
-        public ProductTemplateController(IMediator mediator, IPermissionService permissionService)
+        public ProductTemplateODataController(IMediator mediator, IPermissionService permissionService)
         {
             _mediator = mediator;
             _permissionService = permissionService;
         }
 
+        [SwaggerOperation(summary: "Get entity from ProductTemplate")]
         [HttpGet("{key}")]
         public async Task<IActionResult> Get(string key)
         {
@@ -34,6 +36,7 @@ namespace Grand.Api.Controllers.OData
 
         }
 
+        [SwaggerOperation(summary: "Get entities from ProductTemplate")]
         [HttpGet]
         [EnableQuery(HandleNullPropagation = HandleNullPropagationOption.False)]
         public async Task<IActionResult> Get()
