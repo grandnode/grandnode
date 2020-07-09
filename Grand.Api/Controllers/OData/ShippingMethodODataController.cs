b341641d4b7f99fc6fabff830de24f29bc85ef25
PatrykPorabik <patryk@porabik.pl>
2020-07-09 10:22:17 +0200

    API - Prepare SwaggerOperation for ShippingMethod Controller

--- Grand.Api/Controllers/OData/ShippingMethodODataController.cs
+++ Grand.Api/Controllers/OData/ShippingMethodODataController.cs
@@ -5,22 +5,24 @@ using MediatR;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Grand.Api.Controllers.OData
 {
-    public partial class ShippingMethodController : BaseODataController
+    public partial class ShippingMethodODataController : BaseODataController
     {
         private readonly IMediator _mediator;
         private readonly IPermissionService _permissionService;
 
-        public ShippingMethodController(IMediator mediator, IPermissionService permissionService)
+        public ShippingMethodODataController(IMediator mediator, IPermissionService permissionService)
         {
             _mediator = mediator;
             _permissionService = permissionService;
         }
 
+        [SwaggerOperation(summary: "Get entity from ShippingMethod")]
         [HttpGet("{key}")]
         public async Task<IActionResult> Get(string key)
         {
@@ -34,6 +36,7 @@ namespace Grand.Api.Controllers.OData
             return Ok(shipping.FirstOrDefault());
         }
 
+        [SwaggerOperation(summary: "Get entities from ShippingMethod")]
         [HttpGet]
         [EnableQuery(HandleNullPropagation = HandleNullPropagationOption.False)]
         public async Task<IActionResult> Get()
