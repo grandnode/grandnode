da83fe6863aa9a40367d3e33401a28385d6ac88c
PatrykPorabik <patryk@porabik.pl>
2020-07-23 08:23:16 +0200

    Start/End Date for Tier Prices are not saved on product #889

--- Grand.Web/Areas/Admin/Extensions/Mapping/TierPriceMappingExtensions.cs
+++ Grand.Web/Areas/Admin/Extensions/Mapping/TierPriceMappingExtensions.cs
@@ -9,24 +9,24 @@ namespace Grand.Web.Areas.Admin.Extensions
         public static ProductModel.TierPriceModel ToModel(this TierPrice entity, IDateTimeHelper dateTimeHelper)
         {
             var tierprice = entity.MapTo<TierPrice, ProductModel.TierPriceModel>();
-            tierprice.StartDateTime = tierprice.StartDateTime.ConvertToUserTime(dateTimeHelper);
-            tierprice.EndDateTime = tierprice.EndDateTime.ConvertToUserTime(dateTimeHelper);
+            tierprice.StartDateTime = entity.StartDateTimeUtc.ConvertToUserTime(dateTimeHelper);
+            tierprice.EndDateTime = entity.EndDateTimeUtc.ConvertToUserTime(dateTimeHelper);
             return tierprice;
         }
 
         public static TierPrice ToEntity(this ProductModel.TierPriceModel model, IDateTimeHelper dateTimeHelper)
         {
             var tierprice = model.MapTo<ProductModel.TierPriceModel, TierPrice>();
-            tierprice.StartDateTimeUtc = tierprice.StartDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
-            tierprice.EndDateTimeUtc = tierprice.EndDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
+            tierprice.StartDateTimeUtc = model.StartDateTime.ConvertToUtcTime(dateTimeHelper);
+            tierprice.EndDateTimeUtc = model.EndDateTime.ConvertToUtcTime(dateTimeHelper);
             return tierprice;
         }
 
         public static TierPrice ToEntity(this ProductModel.TierPriceModel model, TierPrice destination, IDateTimeHelper dateTimeHelper)
         {
             var tierprice = model.MapTo(destination);
-            tierprice.StartDateTimeUtc = tierprice.StartDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
-            tierprice.EndDateTimeUtc = tierprice.EndDateTimeUtc.ConvertToUtcTime(dateTimeHelper);
+            tierprice.StartDateTimeUtc = model.StartDateTime.ConvertToUtcTime(dateTimeHelper);
+            tierprice.EndDateTimeUtc = model.EndDateTime.ConvertToUtcTime(dateTimeHelper);
             return tierprice;
         }
     }
