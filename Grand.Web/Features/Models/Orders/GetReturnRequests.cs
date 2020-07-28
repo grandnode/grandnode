ca99a6581bab2c48126a95672f048cd3c6af8947
KrzysztofPajak <krzysiek@nop4you.com>
2020-07-28 08:53:38 +0200

    Remove unused using

--- Grand.Web/Features/Models/Orders/GetReturnRequests.cs
+++ Grand.Web/Features/Models/Orders/GetReturnRequests.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Grand.Web.Models.Orders;
+﻿using Grand.Web.Models.Orders;
 using MediatR;
 
 namespace Grand.Web.Features.Models.Orders
 {
     public class GetReturnRequests : IRequest<CustomerReturnRequestsModel>
-    { 
+    {
     }
 }
