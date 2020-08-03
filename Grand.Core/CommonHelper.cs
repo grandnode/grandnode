6613e78757d179f631d0ee89f1f571634a377044
KrzysztofPajak <krzysiek@nop4you.com>
2020-08-03 15:06:17 +0200

    Move action DeleteDirectory from CommonHelper to the PluginController

--- Grand.Core/CommonHelper.cs
+++ Grand.Core/CommonHelper.cs
@@ -251,35 +251,5 @@ namespace Grand.Core
         /// </summary>
         public static int CookieAuthExpires { get; set; }
 
-        /// <summary>
-        ///  Depth-first recursive delete, with handling for descendant directories open in Windows Explorer.
-        /// </summary>
-        /// <param name="path">Directory path</param>
-        public static void DeleteDirectory(string path)
-        {
-            if (string.IsNullOrEmpty(path))
-                throw new ArgumentNullException(path);
-
-            //find more info about directory deletion
-            //and why we use this approach at https://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true
-
-            foreach (var directory in Directory.GetDirectories(path))
-            {
-                DeleteDirectory(directory);
-            }
-
-            try
-            {
-                Directory.Delete(path, true);
-            }
-            catch (IOException)
-            {
-                Directory.Delete(path, true);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Directory.Delete(path, true);
-            }
-        }
     }
 }
