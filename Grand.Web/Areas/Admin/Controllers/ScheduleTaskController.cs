8b0d259eee871fac96c697fdc6853d0261d1d915
KrzysztofPajak <krzysiek@nop4you.com>
2020-08-06 09:38:01 +0200

    Admin panel - Schedule task list - convert DateTime fields to user time

--- Grand.Web/Areas/Admin/Controllers/ScheduleTaskController.cs
+++ Grand.Web/Areas/Admin/Controllers/ScheduleTaskController.cs
@@ -1,12 +1,14 @@
 ﻿using Grand.Domain.Tasks;
 using Grand.Framework.Kendoui;
 using Grand.Framework.Security.Authorization;
+using Grand.Services.Helpers;
 using Grand.Services.Localization;
 using Grand.Services.Security;
 using Grand.Services.Tasks;
 using Grand.Web.Areas.Admin.Models.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using NPOI.OpenXmlFormats.Dml;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,16 +22,19 @@ namespace Grand.Web.Areas.Admin.Controllers
 
         private readonly IScheduleTaskService _scheduleTaskService;
         private readonly ILocalizationService _localizationService;
+        private readonly IDateTimeHelper _dateTimeHelper;
 
         #endregion
 
         #region Constructors
         public ScheduleTaskController(
             IScheduleTaskService scheduleTaskService,
-            ILocalizationService localizationService)
+            ILocalizationService localizationService,
+            IDateTimeHelper dateTimeHelper)
         {
             _scheduleTaskService = scheduleTaskService;
             _localizationService = localizationService;
+            _dateTimeHelper = dateTimeHelper;
         }
         #endregion
 
@@ -44,9 +49,9 @@ namespace Grand.Web.Areas.Admin.Controllers
                 Type = task.Type,
                 Enabled = task.Enabled,
                 StopOnError = task.StopOnError,
-                LastStartUtc = task.LastStartUtc,
-                LastEndUtc = task.LastNonSuccessEndUtc,
-                LastSuccessUtc = task.LastSuccessUtc,
+                LastStartUtc = task.LastStartUtc.HasValue ? _dateTimeHelper.ConvertToUserTime(task.LastStartUtc.Value, DateTimeKind.Utc) : default(DateTime?),
+                LastEndUtc = task.LastNonSuccessEndUtc.HasValue ? _dateTimeHelper.ConvertToUserTime(task.LastNonSuccessEndUtc.Value, DateTimeKind.Utc) : default(DateTime?),
+                LastSuccessUtc = task.LastSuccessUtc.HasValue ? _dateTimeHelper.ConvertToUserTime(task.LastSuccessUtc.Value, DateTimeKind.Utc) : default(DateTime?),
                 TimeInterval = task.TimeInterval,
             };
             return model;
