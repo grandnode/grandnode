df737a110766fb17527f1576db451517c653441e
KrzysztofPajak <krzysiek@nop4you.com>
2020-08-07 09:20:42 +0200

    Remove IHtmlHelper extension and prepare TagHelper - Html

---
+++ Grand.Framework/TagHelpers/HtmlTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Grand.Core;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Grand.Framework.TagHelpers
+{
+    [HtmlTargetElement("html", Attributes = ForAttributeName)]
+    public class HtmlTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "use-lang";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public bool UseLanguage { set; get; }
+
+        private readonly IWorkContext _workContext;
+
+        public HtmlTagHelper(IWorkContext workContext)
+        {
+            _workContext = workContext;
+        }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (UseLanguage)
+            {
+                output.Attributes.Add("lang", _workContext.WorkingLanguage.UniqueSeoCode);
+                if(_workContext.WorkingLanguage.Rtl)
+                    output.Attributes.Add("dir", "rtl");
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
