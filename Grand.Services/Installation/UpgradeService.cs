e108b4348b27ad95cb75d1582565a42326cb3ec7
KrzysztofPajak <krzysiek@nop4you.com>
2020-08-26 11:45:29 +0200

    Add support for the upgrade to version 4.80 Advanced Permissions #913

--- Grand.Services/Installation/UpgradeService.cs
+++ Grand.Services/Installation/UpgradeService.cs
@@ -1013,6 +1013,22 @@ namespace Grand.Services.Installation
 
             #endregion
 
+            #region Update permissions - Actions
+
+            IPermissionProvider provider = new StandardPermissionProvider();
+            var permissions = provider.GetPermissions();
+            var permissionService = _serviceProvider.GetRequiredService<IPermissionService>();
+            foreach (var permission in permissions)
+            {
+                var p = await permissionService.GetPermissionRecordBySystemName(permission.SystemName);
+                if (p != null)
+                {
+                    p.Actions = permission.Actions;
+                    await permissionService.UpdatePermissionRecord(p);
+                }
+            }
+
+            #endregion
         }
 
         private async Task InstallStringResources(string filenames)
