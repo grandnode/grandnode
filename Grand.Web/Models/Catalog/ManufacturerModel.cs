c9f045f1151c4485ceb8c232a7da03eca22b1837
PatrykPorabik <patryk@porabik.pl>
2020-09-04 10:53:33 +0200

    Second manufacturer description on the bottom of the page #919

--- Grand.Web/Models/Catalog/ManufacturerModel.cs
+++ Grand.Web/Models/Catalog/ManufacturerModel.cs
@@ -16,6 +16,7 @@ namespace Grand.Web.Models.Catalog
 
         public string Name { get; set; }
         public string Description { get; set; }
+        public string BottomDescription { get; set; }
         public string MetaKeywords { get; set; }
         public string MetaDescription { get; set; }
         public string MetaTitle { get; set; }
