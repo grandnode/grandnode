d36b9089c3769360ddee4f6c0cad2a8e7e353e81
KrzysztofPajak <krzysiek@nop4you.com>
2020-09-06 16:18:29 +0200

    Refactoring - AppDomainTypeFinder

--- Grand.Core/Infrastructure/AppDomainTypeFinder.cs
+++ Grand.Core/Infrastructure/AppDomainTypeFinder.cs
@@ -2,61 +2,18 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
-using System.Text.RegularExpressions;
 
 namespace Grand.Core.Infrastructure
 {
     /// <summary>
     /// A class that finds types needed by Grand by looping assemblies in the 
-    /// currently executing AppDomain. Only assemblies whose names matches
-    /// certain patterns are investigated and an optional list of assemblies
-    /// referenced by <see cref="AssemblyNames"/> are always investigated.
+    /// currently executing AppDomain. 
     /// </summary>
     public class AppDomainTypeFinder : ITypeFinder
     {
-        #region Fields
-
-        private bool loadAppDomainAssemblies = true;
-        private string assemblySkipLoadingPattern = "^System|^mscorlib|^Microsoft|^Autofac|^AutoMapper|^Wkhtmltopdf|^FluentValidation|^Serilog|^Swashbuckle|^MiniProfiler|^MongoDB|^MvcContrib|^Newtonsoft|^nunit|^dotliquid|^netstandard|^Google.Apis|^AWSSDK|^Braintree|^MediatR|^WebMarkupMin|^HealthChecks|^WebEssentials|^NWebsec|^NetEscapades|^Wangkanai|^dotnet-bundle|^MailKit|^MimeKit|^SkiaSharp|^StackExchange.Redis|^Google.Authenticator|^QRCoder";
-        private string assemblyRestrictToLoadingPattern = ".*";
-        private IList<string> assemblyNames = new List<string>();
-
-        #endregion
-
-        #region Properties
-       
-        /// <summary>Gets or sets wether Nop should iterate assemblies in the app domain when loading Nop types. Loading patterns are applied when loading these assemblies.</summary>
-        public bool LoadAppDomainAssemblies
-        {
-            get { return loadAppDomainAssemblies; }
-            set { loadAppDomainAssemblies = value; }
-        }
-
-        /// <summary>Gets or sets assemblies loaded a startup in addition to those loaded in the AppDomain.</summary>
-        public IList<string> AssemblyNames
-        {
-            get { return assemblyNames; }
-            set { assemblyNames = value; }
-        }
-
-        /// <summary>Gets the pattern for dlls that we know don't need to be investigated.</summary>
-        public string AssemblySkipLoadingPattern
-        {
-            get { return assemblySkipLoadingPattern; }
-            set { assemblySkipLoadingPattern = value; }
-        }
-
-        /// <summary>Gets or sets the pattern for dll that will be investigated. For ease of use this defaults to match all but to increase performance you might want to configure a pattern that includes assemblies and your own.</summary>
-        /// <remarks>If you change this so that Nop assemblies arn't investigated (e.g. by not including something like "^Nop|..." you may break core functionality.</remarks>
-        public string AssemblyRestrictToLoadingPattern
-        {
-            get { return assemblyRestrictToLoadingPattern; }
-            set { assemblyRestrictToLoadingPattern = value; }
-        }
-
-        #endregion
 
         #region Methods
 
@@ -72,7 +29,7 @@ namespace Grand.Core.Infrastructure
 
         public IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true)
         {
-            return FindClassesOfType(typeof (T), assemblies, onlyConcreteClasses);
+            return FindClassesOfType(typeof(T), assemblies, onlyConcreteClasses);
         }
 
         public IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true)
@@ -158,9 +115,7 @@ namespace Grand.Core.Infrastructure
             var addedAssemblyNames = new List<string>();
             var assemblies = new List<Assembly>();
 
-            if (LoadAppDomainAssemblies)
-                AddAssembliesInAppDomain(addedAssemblyNames, assemblies);
-            AddConfiguredAssemblies(addedAssemblyNames, assemblies);
+            AddAssembliesInAppDomain(addedAssemblyNames, assemblies);
 
             return assemblies;
         }
@@ -176,9 +131,13 @@ namespace Grand.Core.Infrastructure
         /// <param name="assemblies"></param>
         private void AddAssembliesInAppDomain(List<string> addedAssemblyNames, List<Assembly> assemblies)
         {
+            Assembly currentAssem = Assembly.GetExecutingAssembly();
             foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                if (Matches(assembly.FullName))
+                var product = assembly.GetCustomAttribute<AssemblyProductAttribute>();
+                var referencedAssemblies = assembly.GetReferencedAssemblies().ToList();
+                if (referencedAssemblies.Where(x => x.FullName == currentAssem.FullName).Any()
+                    || product?.Product == "grandnode")
                 {
                     if (!addedAssemblyNames.Contains(assembly.FullName))
                     {
@@ -187,56 +146,6 @@ namespace Grand.Core.Infrastructure
                     }
                 }
             }
-        }
-
-        /// <summary>
-        /// Adds specificly configured assemblies.
-        /// </summary>
-        /// <param name="addedAssemblyNames"></param>
-        /// <param name="assemblies"></param>
-        protected virtual void AddConfiguredAssemblies(List<string> addedAssemblyNames, List<Assembly> assemblies)
-        {
-            foreach (string assemblyName in AssemblyNames)
-            {
-                Assembly assembly = Assembly.Load(new AssemblyName(assemblyName));
-                if (!addedAssemblyNames.Contains(assembly.FullName))
-                {
-                    assemblies.Add(assembly);
-                    addedAssemblyNames.Add(assembly.FullName);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Check if a dll is one of the shipped dlls that we know don't need to be investigated.
-        /// </summary>
-        /// <param name="assemblyFullName">
-        /// The name of the assembly to check.
-        /// </param>
-        /// <returns>
-        /// True if the assembly should be loaded into Grand.
-        /// </returns>
-        public virtual bool Matches(string assemblyFullName)
-        {
-            return !Matches(assemblyFullName, AssemblySkipLoadingPattern)
-                   && Matches(assemblyFullName, AssemblyRestrictToLoadingPattern);
-        }
-
-        /// <summary>
-        /// Check if a dll is one of the shipped dlls that we know don't need to be investigated.
-        /// </summary>
-        /// <param name="assemblyFullName">
-        /// The assembly name to match.
-        /// </param>
-        /// <param name="pattern">
-        /// The regular expression pattern to match against the assembly name.
-        /// </param>
-        /// <returns>
-        /// True if the pattern matches the assembly name.
-        /// </returns>
-        protected virtual bool Matches(string assemblyFullName, string pattern)
-        {
-            return Regex.IsMatch(assemblyFullName, pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
         }
 
         /// <summary>
