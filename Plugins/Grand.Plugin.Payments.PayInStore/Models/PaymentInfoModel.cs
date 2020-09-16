e30a70f60db07744f3e756d28db92719e9c94072
PatrykPorabik <patryk@porabik.pl>
2020-09-16 13:17:48 +0200

    Move some plugins to the marketplace

--- Plugins/Grand.Plugin.Payments.PayInStore/Models/PaymentInfoModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Grand.Core.Models;
-
-namespace Grand.Plugin.Payments.PayInStore.Models
-{
-    public class PaymentInfoModel : BaseModel
-    {
-        public string DescriptionText { get; set; }
-    }
-}
