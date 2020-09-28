cc7a26a19f338671a6c8b86e479437abd3f59d08
KrzysztofPajak <krzysiek@nop4you.com>
2020-09-28 09:34:57 +0200

    Delete customer also from External Authentication table when deleting customer #949

---
+++ Grand.Services/Events/CustomerDeletedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using Grand.Core.Events;
+using Grand.Domain.Customers;
+using Grand.Domain.Data;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Services.Events
+{
+    public class CustomerDeletedEventHandler : INotificationHandler<EntityDeleted<Customer>>
+    {
+        private readonly IRepository<ExternalAuthenticationRecord> _externalAuthenticationRepository;
+        public CustomerDeletedEventHandler(IRepository<ExternalAuthenticationRecord> externalAuthenticationRepository)
+        {
+            _externalAuthenticationRepository = externalAuthenticationRepository;
+        }
+        public async Task Handle(EntityDeleted<Customer> notification, CancellationToken cancellationToken)
+        {
+            var externalrecords = _externalAuthenticationRepository.Table.Where(x => x.CustomerId == notification.Entity.Id).ToList();
+            foreach (var item in externalrecords)
+            {
+                await _externalAuthenticationRepository.DeleteAsync(item);
+            }
+        }
+    }
+}
