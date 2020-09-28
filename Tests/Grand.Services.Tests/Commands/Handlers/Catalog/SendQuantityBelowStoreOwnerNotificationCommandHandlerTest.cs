2bff583493ac1d0be796215c1d1724b2e212bac4
Dmitriy Borowskiy <d.borowskij@gmail.com>
2020-09-28 13:06:21 +0300

    #472 unit tests  -  WorkflowMessageServiceTest - SendNotification Method (#957)
    * merge dev
    
    * DocumentServiceTests
    
    * Test DocumentType Service
    
    * DeleteMeasureUnitOnProductCommandHandlerTest
    
    * SendNotificationsToSubscribersCommandHandlerTest
    
    * SendNotificationsToSubscribersCommandHandlerTest
    
    * SendOutBidCustomerNotificationCommandHandlerTest
    
    * WorkflowMessageServiceTest
    
    * SendQuantityBelowStoreOwnerNotificationCommandHandlerTest

---
+++ Tests/Grand.Services.Tests/Commands/Handlers/Catalog/SendQuantityBelowStoreOwnerNotificationCommandHandlerTest.cs
@@ -0,0 +1,58 @@
+﻿using Grand.Domain.Catalog;
+using Grand.Domain.Localization;
+using Grand.Services.Commands.Handlers.Catalog;
+using Grand.Services.Commands.Models.Catalog;
+using Grand.Services.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+
+namespace Grand.Services.Tests.Commands.Handlers.Catalog
+{
+    [TestClass()]
+    public class SendQuantityBelowStoreOwnerNotificationCommandHandlerTest
+    {
+        private Mock<IWorkflowMessageService> _workflowMessageServiceMock;
+        private Mock<LocalizationSettings> _localizationSettingsMock;
+        private SendQuantityBelowStoreOwnerNotificationCommandHandler _sendQuantityBelowStoreOwnerNotificationCommandHandler;
+        private SendQuantityBelowStoreOwnerNotificationCommand _request;
+        private SendQuantityBelowStoreOwnerNotificationCommand _requestWithoutPAC;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            _workflowMessageServiceMock = new Mock<IWorkflowMessageService>();
+            _localizationSettingsMock = new Mock<LocalizationSettings>();
+            _sendQuantityBelowStoreOwnerNotificationCommandHandler = new SendQuantityBelowStoreOwnerNotificationCommandHandler(_workflowMessageServiceMock.Object, _localizationSettingsMock.Object);
+            _request = new SendQuantityBelowStoreOwnerNotificationCommand 
+            { 
+                Product = new Product(), 
+                ProductAttributeCombination = new ProductAttributeCombination()
+            };
+            _requestWithoutPAC = new SendQuantityBelowStoreOwnerNotificationCommand {
+                Product = new Product()
+            };
+        }
+
+        [TestMethod()]
+        public async Task ProductAttributeCombinationNotNull()
+        {
+            await _sendQuantityBelowStoreOwnerNotificationCommandHandler.Handle(_request, default);
+            _workflowMessageServiceMock.Verify(x => x.SendQuantityBelowStoreOwnerNotification(It.IsAny<Product>(), It.IsAny<ProductAttributeCombination>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [TestMethod()]
+        public async Task ProductAttributeCombinationIsNull()
+        {
+            await _sendQuantityBelowStoreOwnerNotificationCommandHandler.Handle(_requestWithoutPAC, default);
+            _workflowMessageServiceMock.Verify(x => x.SendQuantityBelowStoreOwnerNotification(It.IsAny<Product>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [TestMethod()]
+        public async Task HandleResultReturnsTrue()
+        {
+            var result = await _sendQuantityBelowStoreOwnerNotificationCommandHandler.Handle(_request, default);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Tests/Grand.Services.Tests/Messages/WorkflowMessageServiceTest.cs
@@ -0,0 +1,117 @@
+﻿using Grand.Domain.Catalog;
+using Grand.Domain.Common;
+using Grand.Domain.Customers;
+using Grand.Domain.Localization;
+using Grand.Domain.Messages;
+using Grand.Domain.Stores;
+using Grand.Domain.Vendors;
+using Grand.Services.Localization;
+using Grand.Services.Messages;
+using Grand.Services.Queries.Models.Customers;
+using Grand.Services.Stores;
+using MediatR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grand.Services.Tests.Messages
+{
+    [TestClass()]
+    public class WorkflowMessageServiceTest
+    {
+        private Mock<IMessageTemplateService> _messageTemplateServiceMock;
+        private Mock<IQueuedEmailService> _queuedEmailServiceMock;
+        private Mock<ILanguageService> _languageServiceMock;
+        private Mock<IEmailAccountService> _emailAccountServiceMock;
+        private Mock<IMessageTokenProvider> _messageTokenProviderMock;
+        private Mock<IStoreService> _storeServiceMock;
+        private Mock<IMediator> _mediatorMock;
+        private Mock<EmailAccountSettings> _emailAccountSettingsMock;
+        private Mock<CommonSettings> _commonSetting;
+        private WorkflowMessageService _workflowMessageService;
+        private Product _product;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            _messageTemplateServiceMock = new Mock<IMessageTemplateService>();
+            _messageTemplateServiceMock.Setup(x => x.GetMessageTemplateByName(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(new MessageTemplate { IsActive = true }));
+
+            _queuedEmailServiceMock = new Mock<IQueuedEmailService>();
+            
+            _languageServiceMock = new Mock<ILanguageService>();
+            _languageServiceMock.Setup(x => x.GetLanguageById(It.IsAny<string>())).Returns(Task.FromResult(new Language()));
+            _languageServiceMock.Setup(x => x.GetAllLanguages(It.IsAny<bool>(), It.IsAny<string>())).Returns(Task.FromResult(
+                new List<Language> { new Language { Name = "English" }, new Language { Name = "Polish" }} as IList<Language>));
+            
+            _emailAccountServiceMock = new Mock<IEmailAccountService>();
+            _emailAccountServiceMock.Setup(x => x.GetAllEmailAccounts())
+                .Returns(Task.FromResult(new List<EmailAccount> { new EmailAccount { Email = "sdfsdf@mail.com" } } as IList<EmailAccount>));
+            
+            _messageTokenProviderMock = new Mock<IMessageTokenProvider>();
+            
+            _storeServiceMock = new Mock<IStoreService>();
+            _storeServiceMock.Setup(x => x.GetStoreById(It.IsAny<string>())).Returns(Task.FromResult(new Store()));
+            _storeServiceMock.Setup(x => x.GetAllStores()).Returns(Task.FromResult(new List<Store>() as IList<Store>));
+
+            _mediatorMock = new Mock<IMediator>();
+            _mediatorMock.Setup(x => x.Send(It.IsAny<GetCustomerByIdQuery>(), default))
+                .Returns(Task.FromResult(new Customer { Email = "sdfsdf@mail.com" }));
+            
+            _emailAccountSettingsMock = new Mock<EmailAccountSettings>();
+            _commonSetting = new Mock<CommonSettings>();
+            
+            _workflowMessageService = new WorkflowMessageService(
+                _messageTemplateServiceMock.Object,
+                _queuedEmailServiceMock.Object,
+                _languageServiceMock.Object,
+                _emailAccountServiceMock.Object,
+                _messageTokenProviderMock.Object,
+                _storeServiceMock.Object,
+                _mediatorMock.Object,
+                _emailAccountSettingsMock.Object,
+                _commonSetting.Object);
+            _product = new Product();
+
+        }
+
+        [TestMethod()]
+        public async Task CallInsertQueuedEmailMethod()
+        {
+            var result = await _workflowMessageService.SendOutBidCustomerNotification(new Product(), "123", new Bid());
+            _queuedEmailServiceMock.Verify(x => x.InsertQueuedEmail(It.IsAny<QueuedEmail>()), Times.Once());
+        }
+
+        [TestMethod()]
+        public async Task SendOutBidCustomerNotificationMethodReturnCorrectResult()
+        {
+            var result = await _workflowMessageService.SendOutBidCustomerNotification(new Product(), "123", new Bid());
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod()]
+        public async Task SendNewVendorAccountApplyStoreOwnerNotificationRetunsCorrectResult()
+        {
+            var result = await _workflowMessageService.SendNewVendorAccountApplyStoreOwnerNotification(new Customer(), new Vendor(), new Store(), "123");
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod()]
+        public async Task SendNotificationMethodToEmailIsNull()
+        {
+            var result = await _workflowMessageService.SendNotification(messageTemplate: new MessageTemplate(), 
+                emailAccount: new EmailAccount(), 
+                languageId: "123", 
+                liquidObject: new LiquidObject(),
+                toName: "The God",
+                toEmailAddress: null);
+            
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
