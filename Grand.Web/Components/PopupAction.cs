8916a385421e1b94cf516b2aac94762b5a73856c
KrzysztofPajak <krzysiek@nop4you.com>
2020-10-19 09:10:51 +0200

    Add new notification - PopupRenderEvent

--- Grand.Web/Components/PopupAction.cs
+++ Grand.Web/Components/PopupAction.cs
@@ -1,7 +1,9 @@
 ﻿using Grand.Core;
 using Grand.Framework.Components;
 using Grand.Services.Messages;
+using Grand.Web.Events;
 using Grand.Web.Models.Common;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -11,11 +13,13 @@ namespace Grand.Web.ViewComponents
     {
         private readonly IPopupService _popupService;
         private readonly IWorkContext _workContext;
+        private readonly IMediator _mediator;
 
-        public PopupActionViewComponent(IPopupService popupService, IWorkContext workContext)
+        public PopupActionViewComponent(IPopupService popupService, IWorkContext workContext, IMediator mediator)
         {
             _popupService = popupService;
             _workContext = workContext;
+            _mediator = mediator;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
@@ -32,6 +36,9 @@ namespace Grand.Web.ViewComponents
             model.CustomerActionId = result.CustomerActionId;
 
             await _popupService.MovepopupToArchive(result.Id, _workContext.CurrentCustomer.Id);
+
+            //notification 
+            await _mediator.Publish(new PopupRenderEvent(_workContext.CurrentCustomer, result));
 
             return View(model);
         }
---
+++ Grand.Web/Events/PopupRenderEvent.cs
@@ -0,0 +1,17 @@
+﻿using Grand.Domain.Customers;
+using Grand.Domain.Messages;
+using MediatR;
+
+namespace Grand.Web.Events
+{
+    public class PopupRenderEvent : INotification
+    {
+        public Customer Customer { get; private set; }
+        public PopupActive PopupActive { get; private set; }
+        public PopupRenderEvent(Customer customer, PopupActive popupActive)
+        {
+            Customer = customer;
+            PopupActive = popupActive;
+        }
+    }
+}
