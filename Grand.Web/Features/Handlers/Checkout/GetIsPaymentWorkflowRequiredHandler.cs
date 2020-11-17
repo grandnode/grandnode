9851ee63b420d11db3c86d484acb785eb0f53bd8
KrzysztofPajak <krzysiek@grandnode.com>
2020-11-17 13:53:40 +0100

    Add hidden settings ShowPaymentMethodIfCartIsZero

--- Grand.Web/Features/Handlers/Checkout/GetIsPaymentWorkflowRequiredHandler.cs
+++ Grand.Web/Features/Handlers/Checkout/GetIsPaymentWorkflowRequiredHandler.cs
@@ -1,4 +1,5 @@
-﻿using Grand.Services.Orders;
+﻿using Grand.Domain.Payments;
+using Grand.Services.Orders;
 using Grand.Web.Features.Models.Checkout;
 using MediatR;
 using System.Threading;
@@ -9,17 +10,19 @@ namespace Grand.Web.Features.Handlers.Checkout
     public class GetIsPaymentWorkflowRequiredHandler : IRequestHandler<GetIsPaymentWorkflowRequired, bool>
     {
         private readonly IOrderTotalCalculationService _orderTotalCalculationService;
+        private readonly PaymentSettings _paymentSettings;
 
-        public GetIsPaymentWorkflowRequiredHandler(IOrderTotalCalculationService orderTotalCalculationService)
+        public GetIsPaymentWorkflowRequiredHandler(IOrderTotalCalculationService orderTotalCalculationService, PaymentSettings paymentSettings)
         {
             _orderTotalCalculationService = orderTotalCalculationService;
+            _paymentSettings = paymentSettings;
         }
         public async Task<bool> Handle(GetIsPaymentWorkflowRequired request, CancellationToken cancellationToken)
         {
             bool result = true;
             //check whether order total equals zero
             decimal? shoppingCartTotalBase = (await _orderTotalCalculationService.GetShoppingCartTotal(request.Cart, useRewardPoints: request.UseRewardPoints)).shoppingCartTotal;
-            if (shoppingCartTotalBase.HasValue && shoppingCartTotalBase.Value == decimal.Zero)
+            if (shoppingCartTotalBase.HasValue && shoppingCartTotalBase.Value == decimal.Zero && !_paymentSettings.ShowPaymentMethodIfCartIsZero)
                 result = false;
             return result;
         }
