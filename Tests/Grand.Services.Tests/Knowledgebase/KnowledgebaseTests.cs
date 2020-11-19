c8fef56e3666f17c22e41a054043241c414fb9bf
TTomu <wozniaktomasz@gmail.com>
2020-11-19 08:46:52 +0100

    Update knowlegebase tests

--- Tests/Grand.Services.Tests/Knowledgebase/KnowledgebaseTests.cs
+++ Tests/Grand.Services.Tests/Knowledgebase/KnowledgebaseTests.cs
@@ -18,6 +18,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grand.Services.Security;
 
 namespace Grand.Services.Tests.Knowledgebase
 {
@@ -33,6 +34,7 @@ namespace Grand.Services.Tests.Knowledgebase
         private IStoreContext _storeContext;
         private CommonSettings _commonSettings;
         private CatalogSettings _catalogSettings;
+        private readonly IPermissionService _permissionService;
 
         [TestInitialize()]
         public void TestInitialize()
@@ -69,7 +71,7 @@ namespace Grand.Services.Tests.Knowledgebase
             
 
             _knowledgebaseService = new KnowledgebaseService(_categoryRepository, _articleRepository, _eventPublisher, _commonSettings, _catalogSettings,
-                _workContext, _cacheManager, _storeContext, _articleCommentRepository);
+                _workContext, _cacheManager, _storeContext, _articleCommentRepository, _permissionService);
         }
 
         [TestMethod()]
