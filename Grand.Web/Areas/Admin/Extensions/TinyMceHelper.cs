57030b5a1c93a2d8980ae39003e94a736c686818
talhakaanozkan <talhakaanozkan@gmail.com>
2020-11-19 12:28:41 +0300

    Update TinyMceHelper.cs
    Updated javascript files paths

--- Grand.Web/Areas/Admin/Extensions/TinyMceHelper.cs
+++ Grand.Web/Areas/Admin/Extensions/TinyMceHelper.cs
@@ -16,34 +16,34 @@ namespace Grand.Web.Areas.Admin.Extensions
         public static string GetTinyMceLanguage(IWorkContext workContext, IWebHostEnvironment webHostEnvironment)
         {
             //GrandNode supports TinyMCE's localization for 10 languages:
-            //Chinese, Spanish, Arabic, Portuguese, Russian, German, French, Italian, Dutch and English out-of-the-box.
+            //Chinese, Spanish, Arabic, Portuguese, Russian, German, French, Italian, Dutch, Turkish and English out-of-the-box.
             //Additional languages can be downloaded from the website TinyMCE(https://www.tinymce.com/download/language-packages/)
 
             var languageCulture = workContext.WorkingLanguage.LanguageCulture;
 
             var langFile = string.Format("{0}.js", languageCulture);
-            var path = Path.Combine(webHostEnvironment.WebRootPath, "lib\\tinymce\\langs");
-            var fileExists = File.Exists(string.Format("{0}{1}", path, langFile));
+            var path = Path.Combine(webHostEnvironment.WebRootPath, "content\\tinymce\\langs");
+            var fileExists = File.Exists(string.Format("{0}{1}{2}", path, "/", langFile));
 
             if (!fileExists)
             {
                 languageCulture = languageCulture.Replace('-', '_');
                 langFile = string.Format("{0}.js", languageCulture);
-                fileExists = File.Exists(string.Format("{0}{1}", path, langFile));
+                fileExists = File.Exists(string.Format("{0}{1}{2}", path, "/", langFile));
             }
 
             if (!fileExists)
             {
                 languageCulture = languageCulture.Replace('-', '_');
                 langFile = string.Format("{0}.js", languageCulture);
-                fileExists = File.Exists(string.Format("{0}{1}", path, langFile));
+                fileExists = File.Exists(string.Format("{0}{1}{2}", path, "/", langFile));
             }
 
             if (!fileExists)
             {
                 languageCulture = languageCulture.Split('_', '-')[0];
                 langFile = string.Format("{0}.js", languageCulture);
-                fileExists = File.Exists(string.Format("{0}{1}", path, langFile));
+                fileExists = File.Exists(string.Format("{0}{1}{2}", path, "/", langFile));
             }
 
             return fileExists ? languageCulture : string.Empty;
