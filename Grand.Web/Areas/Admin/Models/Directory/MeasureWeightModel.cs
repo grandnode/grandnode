4d1cc80c9e84714f8f5452b115e4f3d7a38f2424
KrzysztofPajak <krzysiek@grandnode.com>
2020-12-17 14:31:54 +0100

    Minor changes

--- Grand.Web/Areas/Admin/Models/Directory/MeasureWeightModel.cs
+++ Grand.Web/Areas/Admin/Models/Directory/MeasureWeightModel.cs
@@ -6,11 +6,9 @@ namespace Grand.Web.Areas.Admin.Models.Directory
     public partial class MeasureWeightModel : BaseEntityModel
     {
         [GrandResourceDisplayName("Admin.Configuration.Measures.Weights.Fields.Name")]
-
         public string Name { get; set; }
 
         [GrandResourceDisplayName("Admin.Configuration.Measures.Weights.Fields.SystemKeyword")]
-
         public string SystemKeyword { get; set; }
 
         [GrandResourceDisplayName("Admin.Configuration.Measures.Weights.Fields.Ratio")]
