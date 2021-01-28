46ef74bc4148dfadb1844c19e6e08e8b3dd5ef0c
Mehmet Akif Çakar <iletisim@mehmetakifcakar.com>
2021-01-28 10:30:55 +0300

    Remove the category system name from Search Engine Friendly Names during the category deletion (#1066)
    * Removed UrlActions right after the deletion of category
    
    * Revert "Removed UrlActions right after the deletion of category"
    
    This reverts commit d181bc06b27a28f58764712148934adf7e1a0849.
    
    * Removed UrlActions right after the deletion of category
    
    Co-authored-by: mikando <makif_cakar@yahoo.com>

---
+++ Grand.Services/Events/CategoryDeletedEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using Grand.Domain.Data;
+using Grand.Domain.Catalog;
+using Grand.Domain.Seo;
+using Grand.Core.Events;
+using MediatR;
+using MongoDB.Driver;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Grand.Services.Events
+{
+    public class CategoryDeletedEventHandler : INotificationHandler<EntityDeleted<Category>>
+    {
+        private readonly IRepository<UrlRecord> _urlRecordRepository;
+
+        public CategoryDeletedEventHandler(
+            IRepository<UrlRecord> urlRecordRepository)
+        {
+            _urlRecordRepository = urlRecordRepository;
+        }
+
+        public async Task Handle(EntityDeleted<Category> notification, CancellationToken cancellationToken)
+        {
+            //delete url
+            var filters = Builders<UrlRecord>.Filter;
+            var filter = filters.Eq(x => x.EntityId, notification.Entity.Id);
+            filter = filter & filters.Eq(x => x.EntityName, "Category");
+            await _urlRecordRepository.Collection.DeleteManyAsync(filter);
+        }
+    }
+}
